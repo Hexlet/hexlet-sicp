8f513815f45822e9b11265e644ff32e30b7ff9b0
Ilya Kolesnikov <ivkol@mail.ru>
2019-08-31 08:48:38 +0300

    chapters table in materialized path

---
+++ database/migrations/2019_08_31_054447_create_chapters.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChapters extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chapters', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('number');
+            $table->string('name');
+            $table->string('book');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chapters');
+    }
+}
