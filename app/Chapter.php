a2cf3b022a853590468e87a1f2bf019da515c911
vikzh <vikt.anmail@gmail.com>
2019-09-01 13:18:27 +0300

    model fix

--- app/Chapter.php
+++ app/Chapter.php
@@ -9,4 +9,14 @@ class Chapter extends Model
     protected $fillable = [
         'number', 'name', 'description', 'parent_id'
     ];
+
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
+
+    public function parentCategory()
+    {
+        return $this->hasMany(self::class, 'id', 'parent_id');
+    }
 }
--- database/migrations/2019_09_01_095418_create_chapters_table.php
+++ database/migrations/2019_09_01_095418_create_chapters_table.php
@@ -18,7 +18,7 @@ class CreateChapterTable extends Migration
             $table->string('number')->unique();
             $table->string('name');
             $table->text('description')->nullable();
-            $table->bigInteger('parent_id')->nullable();
+            $table->unsignedInteger('parent_id')->nullable();
             $table->timestamps();
         });
     }
