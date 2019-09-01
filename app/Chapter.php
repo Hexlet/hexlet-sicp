e7a0be928b660fc1d21107b6422a7209d6aacf02
feycot <feycot@gmail.com>
2019-09-01 13:02:08 +0500

    clear Chapter code

--- app/Chapter.php
+++ app/Chapter.php
@@ -6,18 +6,4 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chapter extends Model
 {
-    public function getParent()
-    {
-        return $this->where('number', 'like', substr($this->number, 0, -2))->first();
-    }
-
-    public function getChildren()
-    {
-        return $this->where('number', 'like', "{$this->number}._")->get();
-    }
-
-    public function getNameAttribute()
-    {
-        return __(sprintf('sicp.chapters.%s', $this->number));
-    }
 }
