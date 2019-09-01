3502a530ef48e649e48749931a5b624985899bd3
vikzh <vikt.anmail@gmail.com>
2019-09-01 12:58:24 +0300

    migration added

---
+++ database/migrations/2019_09_01_095418_create_chapters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChapterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chapter', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('number')->unique();
+            $table->string('name');
+            $table->text('description');
+            $table->bigInteger(parent_id)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chapter');
+    }
+}
