4af6757b231a1bb4463f41693c37f5071eb83dbd
Alexey Lebedev <alexey.lebedev@fogstream.ru>
2019-09-03 11:08:32 +1000

    added register template, add basic registred template en\ru locale

---
+++ resources/lang/en/register.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title' => 'Registration',
+    'emailPlaceholder' => 'E-mail',
+    'namePlaceholder' => 'Name',
+    'passwordPlaceholder' => 'Password',
+    'passwordConfirmationPlaceholder' => 'Confirm password',
+    'registerButton' => 'Register',
+    'accountExists' => 'Account already exists?',
+    'logIn' => 'Log in',
+    'or' => 'or'
+];
