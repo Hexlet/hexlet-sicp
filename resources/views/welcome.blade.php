f503d045800fd9098b40facd14abbf462011f6a8
feycot <feycot@gmail.com>
2019-09-14 22:25:11 +0500

    replace welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,17 +2,10 @@
 
 @section('content')
     <div class="title m-b-md">
-        Laravel
+        Hexlet SICP
     </div>
 
     <div class="links">
-        <a href="https://laravel.com/docs">Docs</a>
-        <a href="https://laracasts.com">Laracasts</a>
-        <a href="https://laravel-news.com">News</a>
-        <a href="https://blog.laravel.com">Blog</a>
-        <a href="https://nova.laravel.com">Nova</a>
-        <a href="https://forge.laravel.com">Forge</a>
-        <a href="https://github.com/laravel/laravel">GitHub</a>
-        <a href="{{ route('oauth.github') }}">login with github</a>
+        <p>Осилятор СИКП — сервис, в котором есть рейтинг тех, кто проходит сикп, и каждый отмечает что он прошел.</p>
     </div>
 @endsection
