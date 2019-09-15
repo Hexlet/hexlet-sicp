52c01b7263945c18ae5c2c7362d34525c8aaf920
feycot <feycot@gmail.com>
2019-09-15 20:22:04 +0500

    add link to profile for current user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,11 +55,15 @@
                                     </a>
 
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('users.show', Auth::user()->name) }}">
+                                                {{ __('Profile') }}
+                                        </a>
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                                                  onclick="event.preventDefault();
                                                                  document.getElementById('logout-form').submit();">
                                             {{ __('Logout') }}
                                         </a>
+
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                             @csrf
