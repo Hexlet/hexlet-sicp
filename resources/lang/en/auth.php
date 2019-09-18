fa576bcede7aa2729ca299edc7c64d608774114b
feycot <feycot@gmail.com>
2019-09-18 22:51:35 +0500

    add with_github text

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -15,5 +15,5 @@ return [
 
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
+    'with_github' => 'Log in with Github'
 ];
--- resources/views/components/social_login.blade.php
+++ resources/views/components/social_login.blade.php
@@ -3,6 +3,7 @@
             <div class="card text-white">
                 <div class="card-body text-center">
                     <a href="{{ route('oauth.github') }}" class="btn btn-outline-secondary">
+                        {{ __('auth.with_github') }}
                         <img src="{{ url('icons/octoface.svg') }}"/>
                     </a>
                 </div>
