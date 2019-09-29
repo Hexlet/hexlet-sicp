c4e7ae66bb7556475f8e048d259afb02aee2c0d4
feycot <feycot@gmail.com>
2019-09-29 23:25:31 +0500

    add unique index for path column of chapters

---
+++ database/migrations/2019_09_29_182332_alter_chapters_add_unique_path.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterChaptersAddUniquePath extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('chapters', function (Blueprint $table) {
+            $table->unique('path');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('chapters', function (Blueprint $table) {
+            $table->dropUnique('path');
+        });
+    }
+}
