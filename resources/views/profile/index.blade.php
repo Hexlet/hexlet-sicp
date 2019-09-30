24c686130410598357c7ed8739b08586aceaf75d
feycot <feycot@gmail.com>
2019-09-30 22:51:46 +0500

    add profile index page

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row my-4">
+        <div class="col-12 col-md-3">
+            <div class="position-sticky sticky-top pt-4 mb-4">
+                <h1 class="h2 mb-2">
+                    {{ $user->name }}
+                </h1>
+            </div>
+        </div>
+        <div class="col-12 col-md-9 my-4">
+            <h1>{{ __('sicp.title') }}</h1>
+            <span>{{ __('sicp.authors') }}</span>
+
+            <ul class="list-group">
+                {!! Form::open()->route('users.chapters.store', [$user]) !!}
+                @foreach($chapters as $chapter)
+                    @php
+                        $is_read = $chapters->contains($chapter);
+                    @endphp
+
+                    <li class="list-group-item {{ getChapterHeaderTag($chapter) }}">
+                        <div class="form-check">
+                            @if($chapter->can_read)
+                                <input type="checkbox" name="chapters_id[]" class="form-check-input"  id="{{ $chapter->id }}" value="{{ $chapter->id }}" {{ haveRead($user, $chapter) ? 'checked' : '' }}>
+                            @endif
+
+                            <label for="{{ $chapter->id }}" class="form-check-label">
+                                {{ $chapter->path }} {{ getChapterName($chapter->path) }}
+                            </label>
+                        </div>
+                    </li>
+
+                @endforeach
+
+                <div class="form-group my-2">
+                    {!! Form::submit(__('Save')) !!}
+                </div>
+
+                {!! Form::close() !!}
+            </ul>
+        </div>
+    </div>
+@endsection
