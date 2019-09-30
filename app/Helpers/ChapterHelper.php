cf881f210a8d82c119eb9a4695de47e0e57afb94
feycot <feycot@gmail.com>
2019-09-30 22:37:49 +0500

    add haveRead helper function

--- app/Helpers/ChapterHelper.php
+++ app/Helpers/ChapterHelper.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\User;
+
 if (!function_exists('getChapterName')) {
     function getChapterName(string $chapter): string
     {
         return  __('sicp.chapters')[$chapter];
+    }
+}
+
+if (!function_exists('haveRead')) {
+    function haveRead(App\User $user, App\Chapter $chapter)
+    {
+        return $user->chapters->contains($chapter);
     }
 }
