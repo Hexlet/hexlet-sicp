1e03ca9ecb2aeb57d459544402780e844551022e
feycot <feycot@gmail.com>
2019-10-01 22:29:30 +0500

    update users.show readable chapters list

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -12,34 +12,12 @@
         <div class="col-12 col-md-9 my-4">
             <h3>{{ __('sicp.title') }}</h3>
             <span>{{ __('sicp.authors') }}</span>
-
             <ul class="list-group">
-                {!! Form::open()->route('users.chapters.store', [$user->id]) !!}
                 @foreach($chapters as $chapter)
-                    @php
-                        $is_read = isset($readChaptersById[$chapter->id]);
-                    @endphp
-
-                    <li class="list-group-item">
-                        <div class="form-check">
-                            @if($chapter->can_read)
-                                <input type="checkbox" name="chapters_id[]" class="form-check-input"  id="{{ $chapter->id }}" value="{{ $chapter->id }}" {{ $is_read ? 'checked' : '' }}>
-                            @endif
-
-                            <label for="{{ $chapter->id }}" class="form-check-label">
-                                {{ $chapter->path }} {{ getChapterName($chapter->path) }}
-                            </label>
-                        </div>
+                <li class="list-group-item {{ haveRead($user, $chapter) ? 'list-group-item-success' : '' }}">
+                        {{ $chapter->path }} {{ getChapterName($chapter->path) }}
                     </li>
-
                 @endforeach
-
-                <div class="form-group my-2">
-                    {!! Form::submit(__('Save')) !!}
-                </div>
-
-                {!! Form::close() !!}
-            </ul>
         </div>
     </div>
 @endsection
