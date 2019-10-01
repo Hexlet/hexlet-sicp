f4b23792314db60e319b8a349f407f063f65f596
feycot <feycot@gmail.com>
2019-10-01 21:24:19 +0500

    fix users.show view template name

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
             ];
         });
 
-        return view('user.index', [
+        return view('user.show', [
             'user' => $user,
             'chapters' => $chapters,
             'readChaptersById' => $readChaptersById,
