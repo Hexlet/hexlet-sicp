f598c5f09d0467054df198db3fad1aad48f804c9
feycot <feycot@gmail.com>
2019-10-02 20:41:10 +0500

    fix h tags

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -4,40 +4,32 @@
     <div class="row my-4">
         <div class="col-12 col-md-3">
             <div class="position-sticky sticky-top pt-4 mb-4">
-                <h1 class="h2 mb-2">
-                    {{ $user->name }}
-                </h1>
+                <p class="h2 mb-2">{{ $user->name }}</p>
             </div>
         </div>
         <div class="col-12 col-md-9 my-4">
-            <h1>{{ __('sicp.title') }}</h1>
-            <span>{{ __('sicp.authors') }}</span>
-
+            <h1 class="h3">{{ __('sicp.title') }}</h1>
+            <h2 class="h4">by {{ __('sicp.authors') }}</h4>
             <ul class="list-group">
                 {!! Form::open()->route('users.chapters.store', [$user]) !!}
                 @foreach($chapters as $chapter)
                     @php
                         $is_read = $chapters->contains($chapter);
                     @endphp
-
                     <li class="list-group-item {{ getChapterHeaderTag($chapter) }}">
                         <div class="form-check">
                             @if($chapter->can_read)
                                 <input type="checkbox" name="chapters_id[]" class="form-check-input"  id="{{ $chapter->id }}" value="{{ $chapter->id }}" {{ haveRead($user, $chapter) ? 'checked' : '' }}>
                             @endif
-
                             <label for="{{ $chapter->id }}" class="form-check-label">
                                 {{ $chapter->path }} {{ getChapterName($chapter->path) }}
                             </label>
                         </div>
                     </li>
-
                 @endforeach
-
                 <div class="form-group my-2">
                     {!! Form::submit(__('Save')) !!}
                 </div>
-
                 {!! Form::close() !!}
             </ul>
         </div>
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -4,14 +4,14 @@
     <div class="row my-4">
         <div class="col-12 col-md-3">
             <div class="position-sticky sticky-top pt-4 mb-4">
-                <h1 class="h2 mb-2">
+                <p class="h2 mb-2">
                     {{ $user->name }}
-                </h1>
+                </p>
             </div>
         </div>
         <div class="col-12 col-md-9 my-4">
-            <h3>{{ __('sicp.title') }}</h3>
-            <span>{{ __('sicp.authors') }}</span>
+            <h1 class="h3">{{ __('sicp.title') }}</h1>
+            <h2 class="h4">by {{ __('sicp.authors') }}</h4>
             <div class="progress">
                     <div class="progress-bar bg-success"
                         role="progressbar" style="width: {{ getReadChapterPercent($user->readChapters, $chapters) }}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
@@ -20,7 +20,11 @@
             <ul class="list-group">
                 @foreach($chapters as $chapter)
                 <li class="list-group-item {{ haveRead($user, $chapter) ? 'list-group-item-success' : '' }}">
-                        {{ $chapter->path }} {{ getChapterName($chapter->path) }}
+                    @if(!$chapter->can_read)
+                    <h3 class="h4">{{ $chapter->path }} {{ getChapterName($chapter->path) }}</h3>
+                    @else
+                    <h4 class="h6">{{ $chapter->path }} {{ getChapterName($chapter->path) }}</h4>
+                    @endif
                     </li>
                 @endforeach
         </div>
