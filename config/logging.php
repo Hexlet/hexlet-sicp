21c6274680d97c1852221ea1a0aea24c1c3f646b
feycot <feycot@gmail.com>
2019-10-03 22:31:59 +0500

    add rollbar

--- config/logging.php
+++ config/logging.php
@@ -34,6 +34,12 @@ return [
     */
 
     'channels' => [
+        'rollbar' => [
+            'driver' => 'monolog',
+            'handler' => \Rollbar\Laravel\MonologHandler::class,
+            'access_token' => env('ROLLBAR_TOKEN'),
+            'level' => 'debug',
+        ],
         'stack' => [
             'driver' => 'stack',
             'channels' => ['daily'],
