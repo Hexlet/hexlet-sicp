f85b4f0973de41215eb2832ed19855470494b204
feycot <feycot@gmail.com>
2019-10-22 22:57:29 +0500

    clear code

--- app/Http/Controllers/ChapterController.php
+++ app/Http/Controllers/ChapterController.php
@@ -8,11 +8,6 @@ use Symfony\Component\Yaml\Yaml;
 
 class ChapterController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $treeStructure = Yaml::parseFile(database_path('chapters.yml'));
@@ -21,68 +16,30 @@ class ChapterController extends Controller
         return view('chapter.index', ['chapters' => $chapters]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
-    {
-        return NotFoundHttpException();
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-
-
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Chapter  $chapter
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Chapter $chapter)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Chapter  $chapter
-     * @return \Illuminate\Http\Response
-     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Chapter $chapter)
+    {
+        //
+    }
     public function edit(Chapter $chapter)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Chapter  $chapter
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Chapter $chapter)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Chapter  $chapter
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Chapter $chapter)
     {
         //
