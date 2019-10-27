15dc813cc73e311b8cf5c52bdbfb86288d9db2e0
feycot <feycot@gmail.com>
2019-10-27 19:22:18 +0500

    add lang files

--- resources/lang/en/welcome.php
+++ resources/lang/en/welcome.php
@@ -2,6 +2,22 @@
 
 return [
     'what_is_here' => 'What is here',
+    'about_sicp' => 'SICP - a book on computer science (computer science).
+                 Perception of computers as abstract machines used to manipulate data.
+                 Despite the fact that the book was first published in 1979, it still has not lost its relevance and
+                 will not lose in the future. SICP is always in the top of the best books on teaching programming in
+                 for decades.',
+
+    'description' => 'SICP conqueror, in which there is a rating of those who pass the SICP, and everyone notes that he passed.',
     'features' => 'Features',
-    'coming_soon' => 'Coming soon'
+    'coming_soon' => 'Coming soon',
+    'features_list' => [
+        'Mark chapters as read',
+        'See rating, who read how much',
+    ],
+    'coming_soon_list' => [
+        'Discuss chapters and tasks',
+        'Bind repository with resolved exercises',
+        'Track user activity',
+    ],
 ];
