aa7f604bf9de83929ec1a1271565ac89618e6298
Santa Klaus <egorich2004@mail.ru>
2019-10-28 15:19:16 +0100

    fix comment

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -8,23 +8,12 @@ use Auth;
 
 class AccountController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $user = Auth::user();
         return view('account.index', compact('user'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(User $user)
     {
         $user = Auth::user();
