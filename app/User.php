d00748eda396ae67077f91ef43b82d9c9c34b921
Caius Durling <georgyzhur@gmail.com>
2019-11-26 07:48:14 +0100

    fix test

--- app/User.php
+++ app/User.php
@@ -48,7 +48,6 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function readChapters()
     {
-        var_dump();
         return $this->hasMany(ReadChapter::class);
     }
 }
