4ae305a21f91ad7981ef3d931793c273f78c60ef
Georgij Zsuravljov <georgyzhur@gmail.com>
2019-11-28 08:57:20 +0100

    fix code style

--- resources/views/chapter/list.blade.php
+++ resources/views/chapter/list.blade.php
@@ -2,7 +2,7 @@
     @foreach ($chapters as $chapter)
         <li>
             <a href="{{ route('chapters.show', $chapter) }}">{{ getChapterName($chapter->path) }}</a>
-            @includeWhen(count($chapter->children), 'chapter.list', ['chapters' => $chapter->children])
+            @includeWhen($chapter->children->isNotEmpty(), 'chapter.list', ['chapters' => $chapter->children])
         </li>
     @endforeach
 </ol>
