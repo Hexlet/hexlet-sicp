980891e21bfdfb5dfd78b555eeae32a9f6da0275
Georgij Zsuravljov <georgyzhur@gmail.com>
2019-11-30 07:13:00 +0100

    fix code style

--- resources/views/chapter/list.blade.php
+++ resources/views/chapter/list.blade.php
@@ -1,7 +1,7 @@
 <ol>
     @foreach ($chapters[$parent] as $chapter)
         <li>
-            <a href="{{ route('chapters.show', $chapter->id) }}">{{ getChapterName($chapter->path) }}</a>
+            <a href="{{ route('chapters.show', $chapter) }}">{{ getChapterName($chapter->path) }}</a>
             @includeWhen(isset($chapters[$chapter->id]), 'chapter.list', ['chapters' => $chapters, 'parent' => $chapter->id])
         </li>
     @endforeach
