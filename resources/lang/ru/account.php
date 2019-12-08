a9c10e85990a56334bbc95cf5bb7bc51daf598dd
Georgij Zsuravljov <georgyzhur@gmail.com>
2019-12-08 16:00:50 +0100

    fix delele

--- resources/lang/ru/account.php
+++ resources/lang/ru/account.php
@@ -3,7 +3,7 @@ return [
     'settings' => 'Настройка',
     'account' => 'Аккаунт',
     'profile' => 'Профиль',
-    'delele_account' => 'Удалить Аккаунт',
+    'delete_account' => 'Удалить Аккаунт',
     'are_you_sure' => 'Вы уверены, что хотите удалить свой аккаунт ?',
     'your_account_deleted' => 'Ваш аккаунт успешно удален!',
     'name_updated' => 'Ваше имя успешно изменено!',
