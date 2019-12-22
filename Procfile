293aa8cbf734cbaa1525151679bd599187111f62
feycot <feycot@gmail.com>
2019-12-23 00:07:02 +0500

    add command

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: vendor/bin/heroku-php-apache2 public/
-release: php artisan migrate --force
+release: php artisan migrate --force && php artisan sitemap:generate
 frontend: npm run watch
---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+
+class GenerateSitemap extends Command
+{
+    protected $signature = 'sitemap:generate';
+    protected $description = 'Generate the sitemap.';
+
+    public function handle()
+    {
+        SitemapGenerator::create(config('app.url'))->writeToFile(public_path('sitemap.xml'));
+    }
+}
