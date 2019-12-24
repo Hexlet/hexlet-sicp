c4f2d7069527cab59e3d37a6c54e0a0a041391fb
feycot <feycot@gmail.com>
2019-12-24 23:00:48 +0500

    fix tests

--- tests/Feature/Http/Controllers/SitemapControllerTest.php
+++ tests/Feature/Http/Controllers/SitemapControllerTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature\Http\Controllers;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Github\Api\Gists;
+use GrahamCampbell\GitHub\GitHubManager;
 use Tests\TestCase;
+
+use function GuzzleHttp\json_decode;
 
 class SitemapControllerTest extends TestCase
 {
@@ -15,8 +17,32 @@ class SitemapControllerTest extends TestCase
      */
     public function testIndex()
     {
+        $gists = $this->createMock(Gists::class);
+        $gists->method('show')->willReturn(
+            $this->getFixture()
+        );
+
+        $github = $this->getMockBuilder(GitHubManager::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['gists'])
+            ->getMock();
+        $github->method('gists')->willReturn($gists);
+
+        $this->app->instance(GitHubManager::class, $github);
+
         $response = $this->get('/sitemap.xml');
 
         $response->assertStatus(200);
+    }
+
+    private function getFixture()
+    {
+        return [
+            "files" => [
+                "sitemap.xml" => [
+                    "content" => "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<urlset xmlns=\"http:\/\/www.sitemaps.org\/schemas\/sitemap\/0.9\" xmlns:xhtml=\"http:\/\/www.w3.org\/1999\/xhtml\">\n<\/urlset>"
+                ]
+            ]
+        ];
     }
 }
