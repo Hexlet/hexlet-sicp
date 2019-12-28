e52f0ebeee360b643d8125b3d1ae65b13b1cd373
Sergey Karpuk <sergeykarpuk@yandex.by>
2019-12-29 00:26:46 +0300

    style(seed): fix lint

--- database/seeds/ExercisesTableSeeder.php
+++ database/seeds/ExercisesTableSeeder.php
@@ -16,14 +16,13 @@ class ExercisesTableSeeder extends Seeder
     {
         $exercises =  Yaml::parseFile(database_path('exercises.yml'));
         DB::beginTransaction();
-        $createExercises = function ($exercises)
-        {
+        $createExercises = function ($exercises) {
             foreach ($exercises as ['chapter_path' => $chapter_path, 'children' => $children]) {
                 // ['chapter_path' => '1.1.2', 'children' => [1.21, 1.22, 1.23]]
 
                 $chapter = App\Chapter::where('path', $chapter_path)->value('id');
 
-                array_map (function ($exercise) use ($chapter) {
+                array_map(function ($exercise) use ($chapter) {
                     $exerciseModel = new Exercise();
                     $exerciseModel->chapter_id = $chapter;
                     $exerciseModel->path = $exercise;
