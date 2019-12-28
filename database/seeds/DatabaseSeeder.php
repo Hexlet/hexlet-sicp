6a51622c7b872a10d7cadc71219da1a5f9c65634
Sergey Karpuk <sergeykarpuk@yandex.by>
2019-12-29 00:32:40 +0300

    change seed order

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(ChaptersTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
         $this->call(ExercisesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
