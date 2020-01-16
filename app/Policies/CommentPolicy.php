3c7630ee6fff2bac580723726dddc2618b49889a
Georgij Zsuravljov <georgyzhur@gmail.com>
2020-01-16 15:35:54 +0100

    fix codeclimate

--- app/Policies/CommentPolicy.php
+++ app/Policies/CommentPolicy.php
@@ -7,23 +7,23 @@ use Laravelista\Comments\Comment;
 class CommentPolicy
 {
 
-    public function create($user): bool
+    public function create(User $user): bool
     {
         return true;
     }
 
-    public function delete($user, Comment $comment): bool
+    public function delete(User $user, Comment $comment): bool
     {
         return $user->id == $comment->commenter_id || $user->isAdminComments();
     }
 
-    public function update($user, Comment $comment): bool
+    public function update(User $user, Comment $comment): bool
     {
         return $user->id == $comment->commenter_id || $user->isAdminComments();
     }
 
-    public function reply($user, Comment $comment): bool
+    public function reply(User $user, Comment $comment): bool
     {
         return $user->id != $comment->commenter_id;
     }
-}
+} 
