ef5dd3d0d25f4bf5166c4397bb8b77fed6277290
feycot <feycot@gmail.com>
2020-01-19 19:32:57 +0500

    add missing file exercise 1.1

---
+++ resources/views/exercise/listing/1_1.blade.php
@@ -0,0 +1,20 @@
+<pre><code>10
+(+ 5 3 4)
+(- 9 1)
+(/ 6 2)
+(+ (* 2 4) (- 4 6))
+(define a 3)
+(define b (+ a 1))
+(+ a b (* a b))
+(= a b)
+(if (and (> b a) (< b (* a b)))
+    b
+    a)
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
+(+ 2 (if (> b a) b a))
+(* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+   (+ a 1))</code></pre>
