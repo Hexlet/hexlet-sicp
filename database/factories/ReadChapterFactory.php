178c6e717ddd93d08f5afd9d772b28b3ec4ae1a2
feycot <feycot@gmail.com>
2020-03-09 03:31:00 +0500

    clear code

--- database/factories/ReadChapterFactory.php
+++ database/factories/ReadChapterFactory.php
@@ -8,7 +8,7 @@ use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Database\Eloquent\Factory;
 
-$factory->define(ReadChapter::class, function (Faker $faker) {
+$factory->define(ReadChapter::class, function () {
     return [
         'chapter_id' => factory(Chapter::class)->create()->id,
         'user_id'    => factory(User::class)->create()->id,
