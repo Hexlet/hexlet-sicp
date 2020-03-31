ed44768e5d9169a7097c53bce60965da00cf0419
feycot <feycot@gmail.com>
2020-03-31 12:49:49 +0500

    refactoring account page

--- resources/views/account/_menu.blade.php
+++ resources/views/account/_menu.blade.php
@@ -1,13 +1,19 @@
 <div class="col-12 col-md-3">
     <div class="card shadow-sm">
         <div class="card-header font-weight-bold text-muted">
-            {{ __('account.settings') }}
+            @lang('account.settings')
         </div>
         <div class="list-group list-group-flush">
-            <a class="nav-link list-group-item list-group-item-action {{ (\Request::route()->getName() == 'account.edit')  ? 'active' : '' }}" href="{{ route('account.edit', $user->id) }}">{{ __('account.profile') }}</a>
+            <a class="nav-link list-group-item list-group-item-action {{ isActiveRoute('account.edit')  ? 'active' : ''}}"
+               href="{{ route('account.edit', $user) }}">
+                @lang('account.profile')
+            </a>
         </div>
         <div class="list-group list-group-flush">
-            <a class="nav-link list-group-item list-group-item-action {{ (\Request::route()->getName() == 'account.delete')  ? 'active' : '' }}" href="{{ route('account.delete') }}">{{ __('account.account') }}</a>
+            <a class="nav-link list-group-item list-group-item-action {{ isActiveRoute('account.delete')  ? 'active' : '' }}"
+               href="{{ route('account.delete', $user) }}">
+                @lang('account.account')
+            </a>
         </div>
     </div>
 </div>
--- resources/views/account/edit.blade.php
+++ resources/views/account/edit.blade.php
@@ -1,22 +1,29 @@
 @extends('layouts.app')
-
+@php
+/**
+ * @var \App\User $user
+ */
+@endphp
 @section('content')
 <div class="row my-4">
     @include('account._menu')
         <div class="col-12 col-md-9">
-            <h3>{{ __('account.profile') }}: {{ $user->email }}</h3>
+            <h3>
+                @lang('account.profile'): {{ $user->email }}
+            </h3>
             <div class="card">
-                <div class="card-header">{{ __('account.сhange_name') }}</div>
+                <div class="card-header">
+                    @lang('account.сhange_name')
+                </div>
                 <div class="card-body">
-                    {!! Form::open()->patch()->route('account.update', [$user->id]) !!}
+                    {!! Form::open()->patch()->route('account.update', [$user]) !!}
                     {!! Form::text('name', __('register.namePlaceholder'))->value($user->name) !!}
                     <div class="form-group mb-0">
-                    {!! Form::submit(__('Save')) !!}
+                        {!! Form::submit(__('Save')) !!}
                     </div>
                     {!! Form::close() !!}
                 </div>
             </div>
         </div>
 </div>
-
 @endsection
