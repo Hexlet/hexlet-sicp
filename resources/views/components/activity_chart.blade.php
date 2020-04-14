aa51e93a23763ddaefdd7897a2abaebf40247217
Nuril Baradusov <baradusovnh@gmail.com>
2020-04-14 10:32:04 +0400

    fix activity chart layout

--- resources/views/components/activity_chart.blade.php
+++ resources/views/components/activity_chart.blade.php
@@ -1,7 +1,9 @@
 <div class="graph mt-5">
-    <ul class="squares">
-    @foreach($chart as $square)
-        <li data-level="{{ $square }}"></li>
-    @endforeach
-    </ul>
+    <div class="d-flex align-items-end flex-column overflow-hidden m-4">
+        <ul class="squares mb-0">
+            @foreach($chart as $square)
+                <li data-level="{{ $square }}"></li>
+            @endforeach
+        </ul>
+    </div>
 </div>
