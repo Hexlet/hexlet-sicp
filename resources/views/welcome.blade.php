fdba9d13655e9c491cb619bf63d5c5ef28a6b24a
Nuril Baradusov <baradusovnh@gmail.com>
2020-04-14 10:15:15 +0400

    fix welcome layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,38 @@
         <a href="https://mitpress.mit.edu/sites/default/files/sicp/index.html">
             <img class="img-fluid" src="{{ asset('img/Patchouli_Gives_SICP.png') }}" alt="Начать изучать sicp">
         </a>
+    </div>
+    <div class="col-md-4">
+        <h2 class="my-3">{{ __('welcome.what_is_here') }}</h2>
+        <p>{{ __('welcome.about_sicp') }}
+            <br>
+            <a href="https://guides.hexlet.io/how-to-learn-sicp/">{{ __('layout.nav.sicp_read') }}</a>
+        </p>
+        <h2 class="my-3">{{ __('welcome.features') }}</h2>
+        <ul>
+            @foreach (__('welcome.features_list') as $key => $item)
+            <li>{{ __(sprintf('welcome.features_list.%s', $key)) }}</li>
+            @endforeach
+        </ul>
+        <h2 class="my-3">{{ __('welcome.coming_soon') }}</h2>
+        <ul>
+            @foreach (__('welcome.coming_soon_list') as $key => $item)
+            <li>{{ __(sprintf('welcome.coming_soon_list.%s', $key)) }}</li>
+            @endforeach
+
+        </ul>
+        <a class="btn btn-primary" href="{{ (route('my')) }}" role="button">{{ __('layout.welcome.mark_read') }}</a>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
         @include('components.activity_chart')
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-8">
         <h3 class="my-3"><a href="{{ (route('log.index')) }}">{{ __('activitylog.title') }}</a></h3>
         @foreach($logItems as $logItem)
         <div class="media text-muted pt-1">
@@ -65,27 +96,7 @@
         </div>
         @endforeach
     </div>
-    <div class="col-md-4">
-        <h2 class="my-3">{{ __('welcome.what_is_here') }}</h2>
-        <p>{{ __('welcome.about_sicp') }}
-            <br>
-            <a href="https://guides.hexlet.io/how-to-learn-sicp/">{{ __('layout.nav.sicp_read') }}</a>
-        </p>
-        <h2 class="my-3">{{ __('welcome.features') }}</h2>
-        <ul>
-            @foreach (__('welcome.features_list') as $key => $item)
-            <li>{{ __(sprintf('welcome.features_list.%s', $key)) }}</li>
-            @endforeach
-        </ul>
-        <h2 class="my-3">{{ __('welcome.coming_soon') }}</h2>
-        <ul>
-            @foreach (__('welcome.coming_soon_list') as $key => $item)
-            <li>{{ __(sprintf('welcome.coming_soon_list.%s', $key)) }}</li>
-            @endforeach
-
-        </ul>
-        <a class="btn btn-primary" href="{{ (route('my')) }}" role="button">{{ __('layout.welcome.mark_read') }}</a>
-    </div>
 </div>
+
 <!-- /.row -->
 @endsection
