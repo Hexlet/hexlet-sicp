b87bf9e74516feb55f44509d5cc5f951caf51ed1
feycot <feycot@gmail.com>
2020-05-09 17:15:48 +0500

    refactor web routes

--- routes/web.php
+++ routes/web.php
@@ -6,30 +6,20 @@ Route::group([
     'prefix' => LaravelLocalization::setLocale(),
     'middleware' => [ 'localizationRedirect' ],
 ], function () {
+
+    Route::get('/oauth/github', 'Auth\\Social\\GithubController@redirectToProvider')->name('oauth.github');
+    Route::get('/oauth/github/callback', 'Auth\\Social\\GithubController@handleProviderCallback')->name('oauth.github-callback');
+
     Auth::routes(['verify' => true]);
     Route::post('/dev-login', 'Auth\LoginController@devLogin')->name('auth.dev-login');
-    Route::resource('/', 'WelcomeController')->only('index');
-    Route::group(
-        [
-            'prefix' => 'oauth',
-            'namespace' => '\\App\\Http\\Controllers\\Auth\\Social\\',
-        ],
-        static function () {
-            Route::get('/github', 'GithubController@redirectToProvider')->name('oauth.github');
-            Route::get('/github/callback', 'GithubController@handleProviderCallback')->name('oauth.github-callback');
-        }
-    );
-    Route::middleware('auth')->group(function () {
-        Route::resource('users.chapters', 'UserChapterController')
-            ->only('store', 'destroy');
-        Route::resource('users.exercises', 'UserExerciseController')
-            ->only('store', 'update', 'destroy');
 
-        Route::get('/my', 'MyController')->name('my');
-        Route::resource('account', 'AccountController')->only('index', 'edit', 'update', 'destroy');
-        Route::get('/account/delete', 'AccountController@delete')->name('account.delete');
-    });
+    Route::resource('/', 'WelcomeController')->only('index');
+    Route::resource('account', 'AccountController')->only('index', 'edit', 'update', 'destroy');
+    Route::get('/account/delete', 'AccountController@delete')->name('account.delete');
+    Route::get('/my', 'MyController')->name('my');
     Route::resource('users', 'UserController')->only('show');
+    Route::resource('users.chapters', 'UserChapterController')->only('store', 'destroy');
+    Route::resource('users.exercises', 'UserExerciseController')->only('store', 'update', 'destroy');
     Route::resource('ratings', 'RatingController')->only('index');
     Route::resource('chapters', 'ChapterController')->only('index', 'show');
     Route::resource('exercises', 'ExerciseController')->only('index', 'show');
