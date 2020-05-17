287ba318142ed3d659f52385dc880e0c080ff335
Roman Kozlov <rkozlov95@mail.ru>
2020-05-17 14:47:48 +0300

    fix rating templates

--- resources/views/rating/comments.blade.php
+++ resources/views/rating/comments.blade.php
@@ -1,31 +1,29 @@
 @extends('layouts.app')
 @section('content')
-    <div class="row my-4">
-        <div class="col-12 my-4">
-            @include('rating.menu')
+    <div class="my-4">
+        @include('rating.menu')
 
-            <h3>{{ __('rating.page_title_comments') }}</h3>
+        <h3>{{ __('rating.page_title_comments') }}</h3>
 
-            <div class="table-responsive">
-                <table class="table table-striped">
-                    <thead>
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>{{ __('rating.positions') }}</th>
+                        <th>{{ __('rating.user') }}</th>
+                        <th>{{ __('rating.number_of_comments') }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($commentsRating as $position => ['user' => $user, 'commentsCount' => $commentsCount])
                         <tr>
-                            <th>{{ __('rating.positions') }}</th>
-                            <th>{{ __('rating.user') }}</th>
-                            <th>{{ __('rating.number_of_comments') }}</th>
+                            <td>{{ $position }}</td>
+                            <td><a href="{{ route('users.show', $user) }}">{{ $user->name }}</a></td>
+                            <td>{{ $commentsCount }}</td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($commentsRating as $position => ['user' => $user, 'commentsCount' => $commentsCount])
-                            <tr>
-                                <td>{{ $position }}</td>
-                                <td><a href="{{ route('users.show', $user) }}">{{ $user->name }}</a></td>
-                                <td>{{ $commentsCount }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
--- resources/views/rating/index.blade.php
+++ resources/views/rating/index.blade.php
@@ -3,32 +3,30 @@
 /** @var \Illuminate\Support\Collection $rating */
 @endphp
 @section('content')
-    <div class="row my-4">
-        <div class="col-12 my-4">
-            @include('rating.menu')
+    <div class="my-4">
+        @include('rating.menu')
 
-            <h3>{{ __('rating.page_title') }}</h3>
+        <h3>{{ __('rating.page_title') }}</h3>
 
-            <div class="table-responsive">
-                <table class="table table-striped">
-                    <thead>
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>{{ __('rating.positions') }}</th>
+                        <th>{{ __('rating.user') }}</th>
+                        <th>{{ __('rating.number_of_points') }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($rating as $position => ['user' => $user, 'points' => $points])
                         <tr>
-                            <th>{{ __('rating.positions') }}</th>
-                            <th>{{ __('rating.user') }}</th>
-                            <th>{{ __('rating.number_of_points') }}</th>
+                            <td>{{ $position }}</td>
+                            <td><a href="{{ route('users.show', $user) }}">{{ $user->name }}</a></td>
+                            <td>{{ $points }}</td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($rating as $position => ['user' => $user, 'points' => $points])
-                            <tr>
-                                <td>{{ $position }}</td>
-                                <td><a href="{{ route('users.show', $user) }}">{{ $user->name }}</a></td>
-                                <td>{{ $points }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
