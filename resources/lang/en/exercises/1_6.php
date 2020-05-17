d08457a1c65f99ed9fcec3213aaafe61295ddb72
Руслан Куга <ruslankuga@MacBook-Air.local>
2020-05-18 02:01:21 +0300

    add exercise1.6

---
+++ resources/lang/en/exercises/1_6.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'title' => 'Special form',
+    'description' => [
+        '1' =>
+        "Alyssa P. Hacker doesn't see why if needs to be provided as a special form. ".
+        "''Why can't I just define it as an ordinary procedure in terms of cond?'' ".
+        "she asks. Alyssa's friend Eva Lu Ator claims this can indeed be done, and she defines a new version of if:",
+        '2' =>
+        "Eva demonstrates the program for Alyssa:",
+        '3' =>
+        "Delighted, Alyssa uses new-if to rewrite the square-root program:",
+        '4' =>
+        "What happens when Alyssa attempts to use this to compute square roots? Explain."
+    ]
+];
