c5159ad46624933b55cdb2e5b0ae47899e4e74fb
Anton Burenkov <buran665@yandex.ru>
2020-05-19 17:01:18 +0700

    add exercise 1.9

---
+++ resources/views/exercise/listing/1_9.blade.php
@@ -0,0 +1,12 @@
+<p>{{ __('exercises/1_9.description.1') }}</p>
+<pre><code>(define (+ a b)
+  (if (= a 0)
+      b
+      (inc (+ (dec a) b))))
+
+(define (+ a b)
+  (if (= a 0)
+      b
+      (+ (dec a) (inc b))))
+</code></pre>
+<p>{{ __('exercises/1_9.description.2') }}</p>
