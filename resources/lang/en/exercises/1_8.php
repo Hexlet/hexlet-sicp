14a6c2a2e68af4ec80c459c1747317dd1d0fdf70
Anton Burenkov <buran665@yandex.ru>
2020-05-19 10:43:51 +0700

    add description of exercise 1.8

---
+++ resources/lang/en/exercises/1_8.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title' => 'Cube root',
+    'description' => [
+        '1' =>
+        "Newton's method for cube roots is based on the fact that if y is an approximation to the cube root of x, " .
+        "then a better approximation is given by the value",
+        '2' =>
+        "Use this formula to implement a cube-root procedure analogous to the square-root procedure. " .
+        "In section 1.3.4 we will see how to implement Newton's method in general as an abstraction of these square-root and cube-root procedures.)"
+    ]
+];
