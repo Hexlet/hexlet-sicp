02a7a522cd36b34cb9e11984b7ffe94577a1c373
Anton Burenkov <buran665@yandex.ru>
2020-05-19 23:14:19 +0700

    add exercise 1.10

---
+++ resources/lang/ru/exercises/1_10.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'title' => "Ackermann's function",
+    'description' => [
+        '1' =>
+        "The following procedure computes a mathematical function called Ackermann's function.",
+        '2' =>
+        "What are the values of the following expressions?",
+        '3' =>
+        "Consider the following procedures, where A is the procedure defined above:",
+        '4' =>
+        "Give concise mathematical definitions for the functions computed by the procedures f, g, and h for positive integer values of n. " .
+        "For example, (k n) computes 5 * n * n."
+    ]
+];
---
+++ resources/views/exercise/listing/1_10.blade.php
@@ -0,0 +1,28 @@
+<p>{{ __('exercises/1_10.description.1') }}</p>
+<pre><code>
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+</code></pre>
+<p>{{ __('exercises/1_10.description.2') }}</p>
+<pre><code>
+(A 1 10)
+
+(A 2 4)
+
+(A 3 3)
+</code></pre>
+<p>{{ __('exercises/1_10.description.3') }}</p>
+<pre><code>
+(define (f n) (A 0 n))
+
+(define (g n) (A 1 n))
+
+(define (h n) (A 2 n))
+
+(define (k n) (* 5 n n))
+</code></pre>
+<p>{{ __('exercises/1_10.description.4') }}</p>
