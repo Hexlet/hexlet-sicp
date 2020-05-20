5889685f81b8e02f7d2e4e623b23a91eccc9159b
laserX <deectro@gmail.com>
2020-05-20 09:06:43 +0200

    add exercise 1.42

---
+++ resources/lang/en/exercises/1_42.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Composition',
+    'description' => [
+        '1' =>
+        "Let f and g be two one-argument functions. The composition f after g is defined to be the function x → f(g(x)). " .
+        "Define a procedure compose that implements composition. For example, if inc is a procedure that adds 1 to its argument."
+    ]
+];
---
+++ resources/views/exercise/listing/1_42.blade.php
@@ -0,0 +1,5 @@
+<p>{{ __('exercises/1_42.description.1') }}</p>
+<pre><code>
+((compose square inc) 6)
+49
+</code></pre>
