c3672d7268a5dc1c307d80c965a905dcf62a8ead
laserX <deectro@gmail.com>
2020-05-21 12:07:30 +0200

    add exercise 1.32

---
+++ resources/views/exercise/listing/1_32.blade.php
@@ -0,0 +1,6 @@
+<p>{{ __('exercises/1_32.description.1') }}</p>
+<pre><code>
+(accumulate combiner null-value term a next b)
+</code></pre>
+<p>{{ __('exercises/1_32.description.2') }}</p>
+<p>{{ __('exercises/1_32.description.3') }}</p>
