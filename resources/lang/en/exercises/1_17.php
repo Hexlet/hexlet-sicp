ace3b4a1752b07a3cb695a58f04f07aaed6dd33b
Anton Burenkov <buran665@yandex.ru>
2020-05-22 08:54:25 +0700

    add exercise 1.17

---
+++ resources/lang/en/exercises/1_17.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'title' => 'Multiplication by means of repeated addition',
+    'description' => [
+        '1' =>
+        "The exponentiation algorithms in this section are based on performing exponentiation by means of repeated multiplication. " .
+        "In a similar way, one can perform integer multiplication by means of repeated addition. " .
+        "The following multiplication procedure (in which it is assumed that our language can only add, not multiply) is analogous to the expt procedure:",
+        '2' =>
+        "This algorithm takes a number of steps that is linear in b. " .
+        "Now suppose we include, together with addition, operations double, which doubles an integer, and halve, which divides an (even) integer by 2. " .
+        "Using these, design a multiplication procedure analogous to fast-expt that uses a logarithmic number of steps."
+    ]
+];
---
+++ resources/views/exercise/listing/1_17.blade.php
@@ -0,0 +1,7 @@
+<p>{{ __('exercises/1_17.description.1') }}</p>
+<pre><code>(define (* a b)
+  (if (= b 0)
+      0
+      (+ a (* a (- b 1)))))
+</code></pre>
+<p>{{ __('exercises/1_17.description.2') }}</p>
