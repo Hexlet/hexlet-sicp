c197b8b38e0baeabd85391a2dc584f8643e70ee3
laserX <deectro@gmail.com>
2020-05-22 09:16:49 +0200

    add exercise 1.37

---
+++ resources/views/exercise/listing/1_37.blade.php
@@ -0,0 +1,28 @@
+<p>{{ __('exercises/1_37.description.1') }}</p>
+<pre><code>
+f =         N₁
+        -------------
+        D₁ +     N₂
+             -----------
+             D₂ +   N₃
+                -----------
+                D₃ + ...
+</code></pre>
+<p>{{ __('exercises/1_37.description.2') }}</p>
+<pre><code>
+      N₁
+---------------
+D₁ +   N₂
+    -----------
+    +     Nk
+        -------
+          Dk
+</code></pre>
+<p>{{ __('exercises/1_37.description.3') }}</p>
+<pre><code>
+(cont-frac (lambda (i) 1.0)
+           (lambda (i) 1.0)
+           k)
+</code></pre>
+<p>{{ __('exercises/1_37.description.4') }}</p>
+<p>{{ __('exercises/1_37.description.5') }}</p>
