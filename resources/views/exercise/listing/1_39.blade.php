7cebbe583c0e2a65347b32a52e84ac53feccd09e
laserX <deectro@gmail.com>
2020-05-22 09:36:58 +0200

    add exercise 1.39

---
+++ resources/views/exercise/listing/1_39.blade.php
@@ -0,0 +1,11 @@
+<p>{{ __('exercises/1_39.description.1') }}</p>
+<pre><code>
+tan x  =        x
+         ---------------
+         1 -     x²
+             -----------
+             3 -   x²
+                 -------
+                 5 - ...
+</code></pre>
+<p>{{ __('exercises/1_39.description.2') }}</p>
