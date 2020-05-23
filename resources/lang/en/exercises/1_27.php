d70d1c6b65d4a7207049eeffd1db8afdc45ab9b1
laserX <deectro@gmail.com>
2020-05-23 12:34:41 +0200

    add exercise 1.27

---
+++ resources/lang/en/exercises/1_27.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Carmichael numbers',
+    'description' => [
+        '1' =>
+        "Demonstrate that the Carmichael numbers listed in footnote 47 really do fool the Fermat test. That is, write a procedure " .
+        "that takes an integer n and tests whether a^n is congruent to a modulo n for every a < n, and try your procedure on the given Carmichael numbers. "
+    ]
+];
