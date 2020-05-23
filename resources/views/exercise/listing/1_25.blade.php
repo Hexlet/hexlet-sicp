6d94c2b05af2faaf0184090a52b529c985941102
laserX <deectro@gmail.com>
2020-05-23 13:08:11 +0200

    add exercise 1.25

---
+++ resources/views/exercise/listing/1_25.blade.php
@@ -0,0 +1,6 @@
+<p>{{ __('exercises/1_25.description.1') }}</p>
+<pre><code>
+(define (expmod base exp m)
+  (remainder (fast-expt base exp) m))
+</code></pre>
+<p>{{ __('exercises/1_25.description.2') }}</p>
