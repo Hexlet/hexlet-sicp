302f974b8221c6be23c355771d33239310d1728b
laserX <deectro@gmail.com>
2020-05-23 13:17:02 +0200

    add exercise 1.24

---
+++ resources/lang/en/exercises/1_24.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => "Modify the timed-prime-test",
+    'description' => [
+        '1' =>
+        "Modify the timed-prime-test procedure of exercise 1.22 to use fast-prime? (the Fermat method), " .
+        "and test each of the 12 primes you found in that exercise. Since the Fermat test has Θ(log n) growth, " .
+        "how would you expect the time to test primes near 1,000,000 to compare with the time needed to test primes near 1000? " .
+        "Do your data bear this out? Can you explain any discrepancy you find?"
+    ]
+];
