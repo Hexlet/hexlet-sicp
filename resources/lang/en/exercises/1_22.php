d84a241853ccab8ef68c9c15c0fd5b32813496c4
laserX <deectro@gmail.com>
2020-05-23 13:49:41 +0200

    add exercise 1.22

---
+++ resources/lang/en/exercises/1_22.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'title' => "Smallest primes",
+    'description' => [
+        '1' =>
+        "Most Lisp implementations include a primitive called runtime that returns an integer that specifies the amount of time the system has been running " .
+        "(measured, for example, in microseconds). The following timed-prime-test procedure, when called with an integer n, prints n and checks to see if n is prime. " .
+        "If n is prime, the procedure prints three asterisks followed by the amount of time used in performing the test.",
+        '2' =>
+        "Using this procedure, write a procedure search-for-primes that checks the primality of consecutive odd integers in a specified range. " .
+        "Use your procedure to find the three smallest primes larger than 1000; larger than 10,000; larger than 100,000; larger than 1,000,000. " .
+        "Note the time needed to test each prime. Since the testing algorithm has order of growth of Θ(√n), " .
+        "you should expect that testing for primes around 10,000 should take about √10 times as long as testing for primes around 1000. " .
+        "Do your timing data bear this out? How well do the data for 100,000 and 1,000,000 support the √n prediction? " .
+        "Is your result compatible with the notion that programs on your machine run in time proportional to the number of steps required for the computation? "
+    ]
+];
---
+++ resources/views/exercise/listing/1_22.blade.php
@@ -0,0 +1,14 @@
+<p>{{ __('exercises/1_22.description.1') }}</p>
+<pre><code>
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+</code></pre>
+<p>{{ __('exercises/1_22.description.2') }}</p>
