778c887e16b5c79d310262ea194b479794c1c03e
laserX <deectro@gmail.com>
2020-05-23 13:01:28 +0200

    add exercise 1.26

---
+++ resources/lang/en/exercises/1_26.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title' => "Slow procedure",
+    'description' => [
+        '1' =>
+        "Louis Reasoner is having great difficulty doing exercise 1.24. His fast-prime? test seems to run more slowly than his prime? test. " .
+        "Louis calls his friend Eva Lu Ator over to help. When they examine Louis's code, " .
+        "they find that he has rewritten the expmod procedure to use an explicit multiplication, rather than calling square:",
+        '2' =>
+        "``I don't see what difference that could make,'' says Louis. ``I do.'' says Eva. ``By writing the procedure like that, " .
+        "you have transformed the Θ(log n) process into a Θ(n) process.'' Explain. "
+    ]
+];
