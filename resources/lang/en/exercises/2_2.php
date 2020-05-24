0b2f0ca7d7a9373ed2fdde2172ad9708030b551a
Anton Burenkov <buran665@yandex.ru>
2020-05-24 08:58:02 +0700

    add exercise 2.2

---
+++ resources/lang/en/exercises/2_2.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title' => 'Representation of line segments on the plane.',
+    'description' =>
+        'Consider the problem of representing line segments in a plane. ' .
+        'Each segment is represented as a pair of points: a starting point and an ending point. ' .
+        'Define a constructor make-segment and selectors start-segment and end-segment that define the representation of segments in terms of points. ' .
+        'Furthermore, a point can be represented as a pair of numbers: the x coordinate and the y coordinate. ' .
+        'Accordingly, specify a constructor make-point and selectors x-point and y-point that define this representation. ' .
+        'Finally, using your selectors and constructors, define a procedure midpoint-segment that takes a line segment as argument and returns its midpoint (the point whose coordinates are the average of the coordinates of the endpoints). ' .
+        "To try your procedures, you'll need a way to print points:"
+];
