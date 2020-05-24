ee8164da8293a353e90af2114735302df2bb0fa6
Anton Burenkov <buran665@yandex.ru>
2020-05-24 09:28:26 +0700

    add exercise 2.5

---
+++ resources/lang/en/exercises/2_5.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'Representation of pairs using only numbers and arithmetic operations',
+    'description' =>
+        'Show that we can represent pairs of nonnegative integers using only numbers and arithmetic operations if we represent the pair a and b as the integer that is the product 2^a * 3^b. ' .
+        'Give the corresponding definitions of the procedures cons, car, and cdr.'
+];
