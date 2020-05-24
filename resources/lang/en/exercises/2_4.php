0fa32ceec5a4ae8e06867ec4ebd6a4cc3ba9ba5b
Anton Burenkov <buran665@yandex.ru>
2020-05-24 09:15:40 +0700

    add exercise 2.4

---
+++ resources/lang/en/exercises/2_4.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title' => 'Procedural representation of pairs',
+    'description' => [
+        '1' =>
+        "Here is an alternative procedural representation of pairs. " .
+        "For this representation, verify that (car (cons x y)) yields x for any objects x and y.",
+        '2' =>
+        "What is the corresponding definition of cdr? " .
+        "(Hint: To verify that this works, make use of the substitution model of section 1.1.5.)"
+    ]
+];
