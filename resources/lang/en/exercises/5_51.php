89157b19318ef6e95d761b412e6ba7e2570045a1
Pavel <mikhailov.sztu@gmail.com>
2020-05-26 18:02:25 +0300

    add exercise 5.51

---
+++ resources/lang/en/exercises/5_51.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'Rudimentary implementation of Scheme in C',
+    'description' =>
+        "Develop a rudimentary implementation of Scheme in C (or some other low-level language of your choice) by translating the explicit-control evaluator of 5.4 into C. " .
+        "In order to run this code you will need to also provide appropriate storage-allocation routines and other run-time support."
+];
