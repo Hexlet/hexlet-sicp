861af7441d2dbf1c095563ea601f959d67095cb9
Anton Burenkov <buran665@yandex.ru>
2020-05-27 09:17:45 +0700

    add exercise 2.17

---
+++ resources/lang/en/exercises/2_17.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => 'Last element of list',
+    'description' =>
+        "Define a procedure last-pair that returns the list that contains only the last element of a given (nonempty) list:"
+];
---
+++ resources/views/exercise/listing/2_17.blade.php
@@ -0,0 +1,4 @@
+<p>{{ __('exercises/2_17.description') }}</p>
+<pre><code>(last-pair (list 23 72 149 34))
+(34)
+</code></pre>
