6f040425462c5132391ecb1f2b611156ad1fa921
laserX <deectro@gmail.com>
2020-05-28 10:07:23 +0200

    change lists style

--- resources/views/chapter/list.blade.php
+++ resources/views/chapter/list.blade.php
@@ -4,33 +4,35 @@
  * @var \App\Chapter $chapter
  */
 @endphp
-<ul type="circle">
+<div class="list-group list-group-flush">
     @foreach ($chapters[$parent] as $chapter)
-    <li>
+    <div class="list-group-item">
         <a href="{{ route('chapters.show', $chapter) }}">
             {{ $chapter->path }} {{ getChapterName($chapter->path) }}
         </a>
         @if($chapter->exercises->isNotEmpty())
-        <div>
-            <a class="badge badge-primary text-wrap"
+            <a class="badge badge-primary text-wrap float-right"
                data-toggle="collapse"
                href="#collapse{{ $chapter->id }}"
                aria-expanded="false"
                aria-controls="collapse{{ $chapter->id }}">
-                {{ __('exercise.exercises') }} <i class="far fa-caret-square-down"></i>
+                {{ __('exercise.exercises') }}
+                <i class="far fa-caret-square-down"></i>
+                <span class="badge badge-light">{{ count($chapter->exercises) }}</span>
             </a>
-        </div>
         <div class="collapse" id="collapse{{ $chapter->id }}">
-            @foreach($chapter->exercises as $exercise)
-            <div class="collapse-item">
-                <a href="{{ route('exercises.show', [$exercise]) }}">
-                    {{ $exercise->path }} {{ getExerciseTitle($exercise) }}
-                </a>
-            </div>
-            @endforeach
+            <ul class="list-group">
+                @foreach($chapter->exercises as $exercise)
+                <li class="list-group-item p-1">
+                    <a class="pl-2" href="{{ route('exercises.show', [$exercise]) }}">
+                        {{ $exercise->path }} {{ getExerciseTitle($exercise) }}
+                    </a>
+                </li>
+                @endforeach
+            </ul>
         </div>
         @endIf
         @includeWhen(isset($chapters[$chapter->id]), 'chapter.list', ['chapters' => $chapters, 'parent' => $chapter->id])
-    </li>
+    </div>
     @endforeach
-</ul>
+</div>
