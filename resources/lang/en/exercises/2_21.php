9a4f3124486f7c37a4c6361fb1d64c64bc4c7a43
Anton Burenkov <buran665@yandex.ru>
2020-05-29 09:17:00 +0700

    add exercise 2.21

---
+++ resources/lang/en/exercises/2_21.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => 'A list of the squares',
+    'description' => [
+        '1' =>
+        "The procedure square-list takes a list of numbers as argument and returns a list of the squares of those numbers.",
+        '2' =>
+        "Here are two different definitions of square-list. Complete both of them by filling in the missing expressions:"
+    ]
+];
