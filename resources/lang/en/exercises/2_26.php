bca10496950accb3b6b887717a4b0ef37453475b
Anton Burenkov <buran665@yandex.ru>
2020-05-30 12:33:15 +0700

    add exercise 2.26

---
+++ resources/lang/en/exercises/2_26.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => 'Expressions with two lists',
+    'description' => [
+        '1' =>
+        "Suppose we define x and y to be two lists:",
+        '2' =>
+        "What result is printed by the interpreter in response to evaluating each of the following expressions:"
+    ]
+];
---
+++ resources/views/exercise/listing/2_26.blade.php
@@ -0,0 +1,12 @@
+<p>{{ __('exercises/2_26.description.1') }}</p>
+<pre><code>(define x (list 1 2 3))
+
+(define y (list 4 5 6))
+</code></pre>
+<p>{{ __('exercises/2_26.description.2') }}</p>
+<pre><code>(append x y)
+
+(cons x y)
+
+(list x y)
+</code></pre>
