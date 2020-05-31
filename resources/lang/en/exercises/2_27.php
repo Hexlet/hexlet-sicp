c0b248ae1f92535452058ffaeeed1c7250623e94
Anton Burenkov <buran665@yandex.ru>
2020-05-31 09:52:26 +0700

    add exercise 2.27

---
+++ resources/lang/en/exercises/2_27.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'Deep reverse',
+    'description' =>
+        "Modify your reverse procedure of exercise 2.18 to produce a deep-reverse procedure that takes a list as argument and returns as its value the list with its elements reversed and with all sublists deep-reversed as well. " .
+        "For example,"
+];
---
+++ resources/views/exercise/listing/2_27.blade.php
@@ -0,0 +1,12 @@
+<p>{{ __('exercises/2_27.description') }}</p>
+<pre><code>(define x (list (list 1 2) (list 3 4)))
+
+x
+((1 2) (3 4))
+
+(reverse x)
+((3 4) (1 2))
+
+(deep-reverse x)
+((4 3) (2 1))
+</code></pre>
