69808416df48aff27a1264201ecd7c8c11662fd4
feycot <feycot@gmail.com>
2020-06-02 00:52:28 +0500

    add highlight js to scheme

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,4 +6,11 @@ import '@fortawesome/fontawesome-free/js/brands'
 require('./bootstrap');
 require('jquery-ujs');
 
+import hljs from 'highlight.js/lib/core';
+import scheme from 'highlight.js/lib/languages/scheme';
+import 'highlight.js/styles/googlecode.css';
+hljs.registerLanguage('scheme', scheme);
+
 $('#flash-overlay-modal').modal();
+hljs.initHighlightingOnLoad();
+
