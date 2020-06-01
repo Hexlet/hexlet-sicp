ca97f56ff2858653782761a2e9d2c688f0a97b4b
Anton Burenkov <buran665@yandex.ru>
2020-06-01 09:15:23 +0700

    add exercise 2.29

---
+++ resources/lang/en/exercises/2_29.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'title' => 'Binary mobile',
+    'description' => [
+        '1' =>
+        "A binary mobile consists of two branches, a left branch and a right branch. " .
+        "Each branch is a rod of a certain length, from which hangs either a weight or another binary mobile. " .
+        "We can represent a binary mobile using compound data by constructing it from two branches (for example, using list):",
+        '2' =>
+        "A branch is constructed from a length (which must be a number) together with a structure, which may be either a number (representing a simple weight) or another mobile:",
+        '3' =>
+        "a.  Write the corresponding selectors left-branch and right-branch, which return the branches of a mobile, and branch-length and branch-structure, which return the components of a branch.",
+        '4' =>
+        "b.  Using your selectors, define a procedure total-weight that returns the total weight of a mobile.",
+        '5' =>
+        "c.  A mobile is said to be balanced if the torque applied by its top-left branch is equal to that applied by its top-right branch (that is, if the length of the left rod multiplied by the weight hanging from that rod is equal to the corresponding product for the right side) and if each of the submobiles hanging off its branches is balanced. " .
+        "Design a predicate that tests whether a binary mobile is balanced.",
+        '6' =>
+        "d.  Suppose we change the representation of mobiles so that the constructors are",
+        '7' =>
+        "How much do you need to change your programs to convert to the new representation?"
+    ]
+];
---
+++ resources/views/exercise/listing/2_29_blade.php
@@ -0,0 +1,18 @@
+<p>{{ __('exercises/2_29.description.1') }}</p>
+<pre><code>(define (make-mobile left right)
+  (list left right))
+</code></pre>
+<p>{{ __('exercises/2_29.description.2') }}</p>
+<pre><code>(define (make-branch length structure)
+  (list length structure))
+</code></pre>
+<p>{{ __('exercises/2_29.description.3') }}</p>
+<p>{{ __('exercises/2_29.description.4') }}</p>
+<p>{{ __('exercises/2_29.description.5') }}</p>
+<p>{{ __('exercises/2_29.description.6') }}</p>
+<pre><code>(define (make-mobile left right)
+  (cons left right))
+(define (make-branch length structure)
+  (cons length structure))
+</code></pre>
+<p>{{ __('exercises/2_29.description.7') }}</p>
