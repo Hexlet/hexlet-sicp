d708faf7c658e4b161137fea860c78df6a7afaa8
Anton Burenkov <buran665@yandex.ru>
2020-06-01 09:28:14 +0700

    add exercise 2.30

---
+++ resources/views/exercise/listing/2_30.blade.php
@@ -0,0 +1,8 @@
+<p>{{ __('exercises/2_30.description.1') }}</p>
+<pre><code>(square-tree
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)))
+(1 (4 (9 16) 25) (36 49))
+</code></pre>
+<p>{{ __('exercises/2_30.description.2') }}</p>
