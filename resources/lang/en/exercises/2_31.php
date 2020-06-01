f0ada3dd3086b16c8074ab885a47d9dd4ac2556f
Anton Burenkov <buran665@yandex.ru>
2020-06-01 09:36:06 +0700

    add exercise 2.31

---
+++ resources/lang/en/exercises/2_31.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => 'Tree map',
+    'description' =>
+        "Abstract your answer to exercise 2.30 to produce a procedure tree-map with the property that square-tree could be defined as"
+];
