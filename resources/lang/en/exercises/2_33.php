aa685d99a50142e4d4a3f1e1f02a3a072ca160d5
Anton Burenkov <buran665@yandex.ru>
2020-06-02 09:07:32 +0700

    add exercise 2.33

---
+++ resources/lang/en/exercises/2_33.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => 'List-manipulation operations as accumulations',
+    'description' =>
+        "Fill in the missing expressions to complete the following definitions of some basic list-manipulation operations as accumulations:"
+];
---
+++ resources/views/exercise/listing/2_33.blade.php
@@ -0,0 +1,10 @@
+<p>{{ __('exercises/2_33.description') }}</p>
+<pre><code>(define (map p sequence)
+  (accumulate (lambda (x y) &lt;??&gt;) nil sequence))
+
+(define (append seq1 seq2)
+  (accumulate cons &lt;??&gt; &lt;??&gt;))
+
+(define (length sequence)
+  (accumulate &lt;??&gt; 0 sequence))
+</code></pre>
