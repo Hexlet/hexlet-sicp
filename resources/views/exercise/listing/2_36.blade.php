27557f0ba8b62b9345318cf8b017298e6cfd36fa
Anton Burenkov <buran665@yandex.ru>
2020-06-03 09:54:26 +0700

    add exercise 2.36

---
+++ resources/views/exercise/listing/2_36.blade.php
@@ -0,0 +1,7 @@
+<p>{{ __('exercises/2_36.description') }}</p>
+<pre><code>(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init &lt;??&gt;)
+            (accumulate-n op init &lt;??&gt;))))
+</code></pre>
