2a2db7f08ffa02d0735d8b0843315323c3cf4bab
Anton Burenkov <buran665@yandex.ru>
2020-06-03 09:36:44 +0700

    add exercise 2.34

---
+++ resources/views/exercise/listing/2_34.blade.php
@@ -0,0 +1,13 @@
+<p>{{ __('exercises/2_34.description.1') }}</p>
+<img class="img-fluid" src="{{ asset('img/exercises/2_34_1.gif') }}" alt="2.34.1">
+<p>{{ __('exercises/2_34.description.2') }}</p>
+<img class="img-fluid" src="{{ asset('img/exercises/2_34_2.gif') }}" alt="2.34.2">
+<p>{{ __('exercises/2_34.description.3') }}</p>
+<pre><code>(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms) &lt;??&gt;)
+              0
+              coefficient-sequence))
+</code></pre>
+<p>{{ __('exercises/2_34.description.4') }}</p>
+<pre><code>(horner-eval 2 (list 1 3 0 5 0 1))
+</code></pre>
