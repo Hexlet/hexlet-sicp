ac522496f84df31bab3cb5c3b65dae43d0c4c953
Pavel <mikhailov.sztu@gmail.com>
2020-06-04 17:35:15 +0300

    add exercise 5.49

---
+++ resources/lang/en/exercises/5_49.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'title' => 'Register machine',
+    'description' =>
+        "As an alternative to using the explicit-control evaluator’s read-eval-print loop, design a register machine that performs a read-compile-execute-print loop. " .
+        "That is, the machine should run a loop that reads an expression, compiles it, assembles and executes the resulting code, and prints the result. " .
+        "This is easy to run in our simulated setup, since we can arrange to call the procedures compile and assemble as “register-machine operations”."
+];
