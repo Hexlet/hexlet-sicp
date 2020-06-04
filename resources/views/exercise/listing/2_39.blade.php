078c5a73f6325e41cd21d62b23393f715892c936
Anton Burenkov <buran665@yandex.ru>
2020-06-04 10:01:01 +0700

    add exercise 2.39

---
+++ resources/views/exercise/listing/2_39.blade.php
@@ -0,0 +1,7 @@
+<p>{{ __('exercises/2_39.description') }}</p>
+<pre><code>(define (reverse sequence)
+  (fold-right (lambda (x y) &lt;??&gt;) nil sequence))
+
+(define (reverse sequence)
+  (fold-left (lambda (x y) &lt;??&gt;) nil sequence))
+</code></pre>
