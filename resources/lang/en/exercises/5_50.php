104d4b4a07efe39dce326c737f9f5474a0cce585
Pavel <mikhailov.sztu@gmail.com>
2020-06-04 17:26:13 +0300

    add exercise 5.50

---
+++ resources/lang/en/exercises/5_50.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'title' => 'Metacircular evaluator',
+    'description' =>
+        "Use the compiler to compile the metacircular evaluator of 4.1 and run this program using the register-machine simulator " .
+        "(To compile more than one definition at a time, you can package the definitions in a begin)." .
+        "The resulting interpreter will run very slowly because of the multiple levels of interpretation, but getting all the details to work is an instructive exercise."
+];
---
+++ resources/views/exercise/listing/5_50.blade.php
@@ -0,0 +1,1 @@
+<p>{{ __('exercises/5_50.description') }}</p>
