4d5d4c4e92ea980c2913604a4f1c9da024624ec0
Anton Burenkov <buran665@yandex.ru>
2020-06-05 08:40:45 +0700

    add exercise 2.40

---
+++ resources/lang/en/exercises/2_40.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'Unique pairs',
+    'description' =>
+        "Define a procedure unique-pairs that, given an integer n, generates the sequence of pairs (i,j) with 1 ≤ j < i ≤ n. " .
+        "Use unique-pairs to simplify the definition of prime-sum-pairs given above."
+];
