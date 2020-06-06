43ebb290a733ff9d1c496f79e0ae5072ea3ee60a
Anton Burenkov <buran665@yandex.ru>
2020-06-06 10:21:02 +0700

    add exercise 2.48

---
+++ resources/lang/en/exercises/2_48.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'Representation of segments using vectors.',
+    'description' =>
+        "A directed line segment in the plane can be represented as a pair of vectors -- the vector running from the origin to the start-point of the segment, and the vector running from the origin to the end-point of the segment. " .
+        "Use your vector representation from exercise 2.46 to define a representation for segments with a constructor make-segment and selectors start-segment and end-segment."
+];
