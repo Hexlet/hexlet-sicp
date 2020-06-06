2033f2bfce4d5f58df9d936064b54eb2115b4076
Anton Burenkov <buran665@yandex.ru>
2020-06-06 10:14:23 +0700

    add exercise 2.47

---
+++ resources/lang/en/exercises/2_47.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => "Frames implementation",
+    'description' => [
+        '1' =>
+        "Here are two possible constructors for frames:",
+        '2' =>
+        "For each constructor supply the appropriate selectors to produce an implementation for frames."
+    ]
+];
