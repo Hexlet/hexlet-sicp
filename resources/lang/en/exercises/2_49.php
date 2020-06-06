53a32d902b20550ce4f497ff481c6d1d1b74906a
Anton Burenkov <buran665@yandex.ru>
2020-06-06 10:31:10 +0700

    add exercise 2.49

---
+++ resources/lang/en/exercises/2_49.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'title' => "Primitive painters",
+    'description' => [
+        '1' =>
+        "Use segments->painter to define the following primitive painters:",
+        '2' =>
+        "a.  The painter that draws the outline of the designated frame.",
+        '3' =>
+        "b.  The painter that draws an ''X'' by connecting opposite corners of the frame.",
+        '4' =>
+        "c.  The painter that draws a diamond shape by connecting the midpoints of the sides of the frame.",
+        '5' =>
+        "d.  The wave painter."
+    ]
+];
