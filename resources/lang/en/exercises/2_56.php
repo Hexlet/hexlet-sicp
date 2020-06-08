d7529b339ac990beb285cf2bba6ff72198ee6a09
Anton Burenkov <buran665@yandex.ru>
2020-06-08 15:54:58 +0700

    add 2.56

---
+++ resources/lang/en/exercises/2_56.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => "Basic differentiator",
+    'description' => [
+        '1' =>
+        "Show how to extend the basic differentiator to handle more kinds of expressions. For instance, implement the differentiation rule",
+        '2' =>
+        "by adding a new clause to the deriv program and defining appropriate procedures exponentiation?, base, exponent, and make-exponentiation. " .
+        "(You may use the symbol ** to denote exponentiation.) Build in the rules that anything raised to the power 0 is 1 and anything raised to the power 1 is the thing itself."
+    ]
+];
