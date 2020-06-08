87bb98e2c81ce6e3c294ed1c305f5e325221a9d9
laserX <deectro@gmail.com>
2020-06-08 10:40:01 +0200

    add exercise 2.94

---
+++ resources/lang/en/exercises/2_94.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title' => "Generic operation",
+    'description' => [
+        '1' =>
+        "Using div-terms, implement the procedure remainder-terms and use this to define gcd-terms as above. " .
+        "Now write a procedure gcd-poly that computes the polynomial GCD of two polys. (The procedure should signal an error if the two polys are not in the same variable.) " .
+        "Install in the system a generic operation greatest-common-divisor that reduces to gcd-poly for polynomials and to ordinary gcd for ordinary numbers. As a test, try",
+        '2' =>
+        "and check your result by hand."
+    ]
+];
---
+++ resources/views/exercise/listing/2_94.blade.php
@@ -0,0 +1,7 @@
+<p>{{ __('exercises/2_94.description.1') }}</p>
+<pre><code>
+(define p1 (make-polynomial 'x '((4 1) (3 -1) (2 -2) (1 2))))
+(define p2 (make-polynomial 'x '((3 1) (1 -1))))
+(greatest-common-divisor p1 p2)
+</code></pre>
+<p>{{ __('exercises/2_94.description.2') }}</p>
