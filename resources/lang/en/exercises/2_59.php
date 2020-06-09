9da7e0951438469a19cf3a198a9a3ae58881597c
Anton Burenkov <buran665@yandex.ru>
2020-06-09 08:39:53 +0700

    add exercise 2.59

---
+++ resources/lang/en/exercises/2_59.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => 'Union-set for the unordered list',
+    'description' =>
+        "Implement the union-set operation for the unordered-list representation of sets."
+];
