f9dace0d3d8e5045b55787488b5e0ac9fb9a0dde
Anton Burenkov <buran665@yandex.ru>
2020-06-09 09:07:50 +0700

    improve title of 2.59

--- resources/lang/en/exercises/2_59.php
+++ resources/lang/en/exercises/2_59.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title' => 'Union-set for the unordered list',
+    'title' => 'Union-set for sets represented as unordered lists',
     'description' =>
         "Implement the union-set operation for the unordered-list representation of sets."
 ];
