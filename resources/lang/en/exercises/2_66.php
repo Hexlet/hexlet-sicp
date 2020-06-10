5248f8f4ff0de51eb8fcc0c7836f0457727788ec
Anton Burenkov <buran665@yandex.ru>
2020-06-10 18:10:11 +0700

    add exercise 2.66

---
+++ resources/lang/en/exercises/2_66.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => 'Lookup procedure',
+    'description' =>
+        "Implement the lookup procedure for the case where the set of records is structured as a binary tree, ordered by the numerical values of the keys."
+];
