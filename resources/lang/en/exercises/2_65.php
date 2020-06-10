6246f3a43aaaf373f7331b5ea48edfb65a7e263e
Anton Burenkov <buran665@yandex.ru>
2020-06-10 18:06:21 +0700

    add exercise 2.65

---
+++ resources/lang/en/exercises/2_65.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => 'Procedures for sets implemented as (balanced) binary trees',
+    'description' =>
+        "Use the results of exercises 2.63 and  2.64 to give (n) implementations of union-set and intersection-set for sets implemented as (balanced) binary trees."
+];
