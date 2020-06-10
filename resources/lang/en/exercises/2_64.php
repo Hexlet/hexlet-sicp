7162ad0a1544282f33e664700bc46c744ad71230
Anton Burenkov <buran665@yandex.ru>
2020-06-10 18:00:05 +0700

    add exercise 2.64

---
+++ resources/lang/en/exercises/2_64.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'title' => "Partial-tree procedure",
+    'description' => [
+        '1' =>
+        "The following procedure list->tree converts an ordered list to a balanced binary tree. " .
+        "The helper procedure partial-tree takes as arguments an integer n and list of at least n elements and constructs a balanced tree containing the first n elements of the list. " .
+        "The result returned by partial-tree is a pair (formed with cons) whose car is the constructed tree and whose cdr is the list of elements not included in the tree.",
+        '2' =>
+        "a. Write a short paragraph explaining as clearly as you can how partial-tree works. Draw the tree produced by list->tree for the list (1 3 5 7 9 11).",
+        '3' =>
+        "b. What is the order of growth in the number of steps required by list->tree to convert a list of n elements?"
+    ]
+];
---
+++ resources/views/exercise/listing/2_64.blade.php
@@ -0,0 +1,22 @@
+<p>{{ __('exercises/2_64.description.1') }}</p>
+<pre><code>(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts)
+                                              right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry left-tree right-tree)
+                      remaining-elts))))))))
+</code></pre>
+<p>{{ __('exercises/2_64.description.2') }}</p>
+<p>{{ __('exercises/2_64.description.3') }}</p>
