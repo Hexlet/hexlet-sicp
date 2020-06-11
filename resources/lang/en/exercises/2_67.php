4544a5e051e6472075c1ee2ac5b642c6fb269813
Anton Burenkov <buran665@yandex.ru>
2020-06-11 09:53:12 +0700

    add exercise 2.67

---
+++ resources/lang/en/exercises/2_67.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => "Message decoding",
+    'description' => [
+        '1' =>
+        "Define an encoding tree and a sample message:",
+        '2' =>
+        "Use the decode procedure to decode the message, and give the result."
+    ]
+];
---
+++ resources/views/exercise/listing/2_67.blade.php
@@ -0,0 +1,11 @@
+<p>{{ __('exercises/2_67.description.1') }}</p>
+<pre><code>(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+</code></pre>
+<p>{{ __('exercises/2_67.description.2') }}</p>
