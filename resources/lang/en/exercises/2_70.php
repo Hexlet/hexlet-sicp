c62f337be8ed9ae4f068f6180aa9efb88bfdc5a5
Anton Burenkov <buran665@yandex.ru>
2020-06-11 10:18:35 +0700

    add exercise 2.70

---
+++ resources/lang/en/exercises/2_70.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title' => "Encode the message",
+    'description' => [
+        '1' =>
+        "The following eight-symbol alphabet with associated relative frequencies was designed to efficiently encode the lyrics of 1950s rock songs. (Note that the ''symbols'' of an ''alphabet'' need not be individual letters.)",
+        '2' =>
+        "Use generate-huffman-tree (exercise 2.69) to generate a corresponding Huffman tree, and use encode (exercise 2.68) to encode the following message:",
+        '3' =>
+        "How many bits are required for the encoding? What is the smallest number of bits that would be needed to encode this song if we used a fixed-length code for the eight-symbol alphabet?"
+    ]
+];
---
+++ resources/views/exercise/listing/2_70.blade.php
@@ -0,0 +1,21 @@
+<p>{{ __('exercises/2_70.description.1') }}</p>
+<pre><code>
+A       2   NA  16
+BOOM    1   SHA 3
+GET     2   YIP 9
+JOB     2   WAH 1
+</code></pre>
+<p>{{ __('exercises/2_70.description.2') }}</p>
+<pre><code>Get a job
+
+Sha na na na na na na na na
+
+Get a job
+
+Sha na na na na na na na na
+
+Wah yip yip yip yip yip yip yip yip yip
+
+Sha boom
+</code></pre>
+<p>{{ __('exercises/2_70.description.3') }}</p>
