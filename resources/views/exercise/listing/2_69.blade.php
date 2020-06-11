fe30b43d94b80c10928e4ea41c88d67a79a60ec6
Anton Burenkov <buran665@yandex.ru>
2020-06-11 10:08:28 +0700

    add exercise 2.69

---
+++ resources/views/exercise/listing/2_69.blade.php
@@ -0,0 +1,5 @@
+<p>{{ __('exercises/2_69.description.1') }}</p>
+<pre><code>(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+</code></pre>
+<p>{{ __('exercises/2_69.description.2') }}</p>
