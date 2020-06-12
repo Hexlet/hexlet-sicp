eb3ee65405e52f5132e1b9061ffc5335952555d6
Anton Burenkov <buran665@yandex.ru>
2020-06-12 10:29:21 +0700

    add exercise 2.74

---
+++ resources/views/exercise/listing/2_74.blade.php
@@ -0,0 +1,6 @@
+<p>{{ __('exercises/2_74.description.1') }}</p>
+<p>{{ __('exercises/2_74.description.2') }}</p>
+<p>{{ __('exercises/2_74.description.3') }}</p>
+<p>{{ __('exercises/2_74.description.4') }}</p>
+<p>{{ __('exercises/2_74.description.5') }}</p>
+<p>{{ __('exercises/2_74.description.6') }}</p>
