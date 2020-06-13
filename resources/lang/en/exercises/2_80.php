dde2730556f0fb5e47b43c159dc17b3bbf61d9af
Anton Burenkov <buran665@yandex.ru>
2020-06-13 09:23:50 +0700

    add exercise 2.80

---
+++ resources/lang/en/exercises/2_80.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'Generic predicate =zero?',
+    'description' =>
+        "Define a generic predicate =zero? that tests if its argument is zero, and install it in the generic arithmetic package. " .
+        "This operation should work for ordinary numbers, rational numbers, and complex numbers."
+];
