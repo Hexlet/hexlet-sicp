b92dde702f101de070ffbae1a5225bd04d82eb20
Anton Burenkov <buran665@yandex.ru>
2020-06-15 09:04:40 +0700

    add exercise 2.84

---
+++ resources/lang/en/exercises/2_84.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'title' => 'Successive raising',
+    'description' =>
+        "Using the raise operation of exercise 2.83, modify the apply-generic procedure so that it coerces its arguments to have the same type by the method of successive raising, as discussed in this section. " .
+        "You will need to devise a way to test which of two types is higher in the tower. " .
+        "Do this in a manner that is ''compatible'' with the rest of the system and will not lead to problems in adding new levels to the tower."
+];
