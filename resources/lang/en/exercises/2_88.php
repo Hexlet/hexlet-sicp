56a3d8c32dca90bd5bc06ab6316c45e67500adf9
Anton Burenkov <buran665@yandex.ru>
2020-06-16 15:43:58 +0700

    add exercise 2.88

---
+++ resources/lang/en/exercises/2_88.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'Subtraction of polynomials',
+    'description' =>
+        "Extend the polynomial system to include subtraction of polynomials. " .
+        "(Hint: You may find it helpful to define a generic negation operation.)"
+];
