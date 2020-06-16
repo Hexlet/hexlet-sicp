904d296f3da5b58c3bd51a445ef57d86db5309c9
Anton Burenkov <buran665@yandex.ru>
2020-06-16 15:39:49 +0700

    add exercise 2.87

---
+++ resources/lang/en/exercises/2_87.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => '=zero? for polynomials',
+    'description' =>
+        "Install =zero? for polynomials in the generic arithmetic package. " .
+        "This will allow adjoin-term to work for polynomials with coefficients that are themselves polynomials."
+];
---
+++ resources/views/exercise/listing/2_87.blade.php
@@ -0,0 +1,1 @@
+<p>{{ __('exercises/2_87.description') }}</p>
