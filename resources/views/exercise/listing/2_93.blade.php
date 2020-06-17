7c4bcc7ce294f432e76064466cc03d10cd01b0d5
Anton Burenkov <buran665@yandex.ru>
2020-06-17 08:48:15 +0700

    add exercise 2.93

---
+++ resources/views/exercise/listing/2_93.blade.php
@@ -0,0 +1,8 @@
+<p>{{ __('exercises/2_93.description.1') }}</p>
+<pre><code>(define p1 (make-polynomial 'x '((2 1)(0 1))))
+
+(define p2 (make-polynomial 'x '((3 1)(0 1))))
+
+(define rf (make-rational p2 p1))
+</code></pre>
+<p>{{ __('exercises/2_93.description.2') }}</p>
