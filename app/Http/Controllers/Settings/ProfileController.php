516cb01e7b8da9a741a191c1531a37be5eba039b
Roman Kozlov <rkozlov95@mail.ru>
2020-06-19 00:37:02 +0300

    fix redirect for unauthorized users

--- app/Http/Controllers/Settings/ProfileController.php
+++ app/Http/Controllers/Settings/ProfileController.php
@@ -8,6 +8,11 @@ use Auth;
 
 class ProfileController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $user = auth()->user();
