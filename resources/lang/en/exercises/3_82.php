1a08c9a998af79a4fbb0b4d1293cd40fcf12f25d
Anton Burenkov <buran665@yandex.ru>
2020-06-18 15:56:30 +0700

    add exercise 3.82

---
+++ resources/lang/en/exercises/3_82.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'title' => 'Monte Carlo integration in terms of streams',
+    'description' =>
+        "Redo exercise 3.5 on Monte Carlo integration in terms of streams. " .
+        "The stream version of estimate-integral will not have an argument telling how many trials to perform. " .
+        "Instead, it will produce a stream of estimates based on successively more trials."
+];
