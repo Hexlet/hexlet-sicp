698df5be2e4804f47cc3ce19b5c4eed04d88cc2e
Alexey Shobanov <shobanov@gmail.com>
2020-06-19 14:30:06 +0000

    add model Solution and migration

---
+++ app/Solution.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Solution extends Model
+{
+    protected $fillable = [
+        'description'
+    ];
+
+    public function completedExercise()
+    {
+        return $this->hasOne('App\CompletedExercise');
+    }
+}
