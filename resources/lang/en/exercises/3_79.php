2041268ac06ff94223bdb77d9c1b32897f759858
Anton Burenkov <buran665@yandex.ru>
2020-06-19 10:06:06 +0700

    add exercise 3.79

---
+++ resources/lang/en/exercises/3_79.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => 'Generalize the solve-2nd procedure',
+    'description' =>
+        "Generalize the solve-2nd procedure of exercise 3.78 so that it can be used to solve general second-order differential equations d²y/dt² = f(dy/dt, y)."
+];
