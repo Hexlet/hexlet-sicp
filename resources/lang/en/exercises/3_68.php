a0f1cca0de605a2df812ae40f905cd6c6d534e92
Anton Burenkov <buran665@yandex.ru>
2020-06-23 09:39:06 +0700

    add exercise 3.68

---
+++ resources/lang/en/exercises/3_68.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => "Alternative definition of pairs",
+    'description' => [
+        '1' =>
+        "Louis Reasoner thinks that building a stream of pairs from three parts is unnecessarily complicated. " .
+        "Instead of separating the pair (S₀,T₀) from the rest of the pairs in the first row, he proposes to work with the whole first row, as follows:",
+        '2' =>
+        "Does this work? Consider what happens if we evaluate (pairs integers integers) using Louis's definition of pairs."
+    ]
+];
