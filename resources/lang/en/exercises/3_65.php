516ba1700f46b410739439c31a69d2cb333714f3
Anton Burenkov <buran665@yandex.ru>
2020-06-24 11:06:24 +0700

    add exercise 3.65

---
+++ resources/lang/en/exercises/3_65.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => "Three sequences of approximations to the natural logarithm of 2",
+    'description' => [
+        '1' =>
+        "Use the series",
+        '2' =>
+        "to compute three sequences of approximations to the natural logarithm of 2, in the same way we did above for π. How rapidly do these sequences converge?"
+    ]
+];
