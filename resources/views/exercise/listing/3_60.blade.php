8658bc930504bdd2ea332ec59fb6465ebb0e050b
Anton Burenkov <buran665@yandex.ru>
2020-06-25 10:42:12 +0700

    add exercise 3.60

---
+++ resources/views/exercise/listing/3_60.blade.php
@@ -0,0 +1,5 @@
+<p>{{ __('exercises/3_60.description.1') }}</p>
+<pre><code>(define (mul-series s1 s2)
+  (cons-stream &lt;??&gt; (add-streams &lt;??&gt; &lt;??&gt;)))
+</code></pre>
+<p>{{ __('exercises/3_60.description.2') }}</p>
