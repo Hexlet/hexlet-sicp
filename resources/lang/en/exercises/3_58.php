ecaf4af2429bcddfbed0279db40f84c1a729a45a
Anton Burenkov <buran665@yandex.ru>
2020-06-26 14:54:10 +0700

    add exercise 3.58

---
+++ resources/lang/en/exercises/3_58.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => "An interpretation of the stream",
+    'description' => [
+        '1' =>
+        "Give an interpretation of the stream computed by the following procedure:",
+        '2' =>
+        "(Quotient is a primitive that returns the integer quotient of two integers.) What are the successive elements produced by (expand 1 7 10) ? What is produced by (expand 3 8 10) ?"
+    ]
+];
