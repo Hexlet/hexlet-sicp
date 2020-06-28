3c995a4acc6f5daedc9415db0febb727f0bd27ce
Anton Burenkov <buran665@yandex.ru>
2020-06-28 13:10:09 +0700

    add exercise 3.55

---
+++ resources/lang/en/exercises/3_55.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'Partial-sums procedure',
+    'description' =>
+        "Define a procedure partial-sums that takes as argument a stream S and returns the stream whose elements are S₀, S₀ + S₁, S₀ + S₁ + S₂, .... " .
+        "For example, (partial-sums integers) should be the stream 1, 3, 6, 10, 15, ...."
+];
