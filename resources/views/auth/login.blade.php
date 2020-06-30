5ced83dd763a8d34674f929ccf46a62872093395
Roman Kozlov <rkozlov95@mail.ru>
2020-06-30 21:04:42 +0300

    update login template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,28 +1,33 @@
 @extends('layouts.app')
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-md-8">
+<div class="row">
+    <div class="col-md-5 mx-auto">
         <div class="card">
-            <div class="card-header"><h1 class="h4">{{ __('layout.login.form_header') }}</h1></div>
+            <div class="card-header"><h1 class="h4 text-center">{{ __('layout.login.form_header') }}</h1></div>
             <div class="card-body">
-                {!! Form::open()->route('login')->attrs(['class' => 'd-flex flex-column align-items-center']) !!}
-                {!! Form::text('email', __('layout.login.email'))->wrapperAttrs(['class' => 'col-sm-6']) !!}
-                {!! Form::text('password', __('layout.login.password'))->type('password')->wrapperAttrs(['class' => 'col-sm-6']) !!}
-                <div class="form-group col-sm-6 mb-2">
+                {!! Form::open()->route('login') !!}
+                    {!! Form::text('email', __('layout.login.email')) !!}
+                    {!! Form::text('password', __('layout.login.password'))->type('password') !!}
+                <div class="form-group">
                     {!! Form::checkbox('remember', __('layout.login.remember_me')) !!}
                 </div>
-                <div class="form-group col-sm-6 mb-0">
+                <div class="form-group">
                     {!! Form::submit(__('layout.login.button')) !!}
                     @if (Route::has('password.request'))
-                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                    <a class="btn btn-link float-right" href="{{ route('password.request') }}">
                         {{ __('layout.login.reset_password') }}
                     </a>
                     @endif
                 </div>
                 {!! Form::close() !!}
             </div>
+            <div class="card-footer d-flex justify-content-center bg-transparent">
+                <a href="{{ route('oauth.github') }}" class="btn btn-outline-secondary m-3">
+                    {{ __('auth.with_github') }}
+                    <img src="{{ url('icons/octoface.svg') }}" alt="" />
+                </a>
+            </div>
         </div>
     </div>
 </div>
-@include('components.social_login')
 @endsection
