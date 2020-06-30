a3e89705c47eef9ba8f35a213fc402e2a7dde7b1
Roman Ashikov <rmn.shkv@gmail.com>
2020-06-30 16:38:35 +0700

    add exercise 4.7

---
+++ resources/views/exercise/listing/4_7.blade.php
@@ -0,0 +1,8 @@
+<p>{{ __('exercises/4_7.description.1') }}</p>
+<pre><code>(let* ((x 3)
+      (y (+ x 2))
+      (z (+ x y 5)))
+  (* x z))</code></pre>
+<p>{{ __('exercises/4_7.description.2') }}</p>
+<pre><code>(eval (let*->nested-lets exp) env)</code></pre>
+ <p>{{ __('exercises/4_7.description.3') }}</p>
