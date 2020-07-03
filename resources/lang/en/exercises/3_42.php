dc05cf2a27de3ddf64838ac2cb421d9881ae83f2
Anton Burenkov <buran665@yandex.ru>
2020-07-03 11:42:08 +0700

    add exercise 3.42

---
+++ resources/lang/en/exercises/3_42.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title' => "Make-account changing",
+    'description' => [
+        '1' =>
+        "Ben Bitdiddle suggests that it's a waste of time to create a new serialized procedure in response to every withdraw and deposit message. " .
+        "He says that make-account could be changed so that the calls to protected are done outside the dispatch procedure. " .
+        "That is, an account would return the same serialized procedure (which was created at the same time as the account) each time it is asked for a withdrawal procedure.",
+        '2' =>
+        "Is this a safe change to make? In particular, is there any difference in what concurrency is allowed by these two versions of make-account ?"
+    ]
+];
