65dffe0ea7212e795dba20d3ad59e89fe6546256
Anton Burenkov <buran665@yandex.ru>
2020-07-06 10:48:33 +0700

    add exercise 3.36

---
+++ resources/views/exercise/listing/3_36.blade.php
@@ -0,0 +1,11 @@
+<p>{{ __('exercises/3_36.description.1') }}</p>
+<pre><code>(define a (make-connector))
+
+(define b (make-connector))
+
+(set-value! a 10 'user)
+</code></pre>
+<p>{{ __('exercises/3_36.description.2') }}</p>
+<pre><code>(for-each-except setter inform-about-value constraints)
+</code></pre>
+<p>{{ __('exercises/3_36.description.3') }}</p>
