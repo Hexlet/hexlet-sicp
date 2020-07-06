1a9cf27e93de8f6328e5c3dc38cf1413c4ab23e1
Anton Burenkov <buran665@yandex.ru>
2020-07-06 10:33:39 +0700

    add exercise 3.38

---
+++ resources/lang/en/exercises/3_38.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title' => "Joint bank account",
+    'description' => [
+        '1' =>
+        "Suppose that Peter, Paul, and Mary share a joint bank account that initially contains $100. " .
+        "Concurrently, Peter deposits $10, Paul withdraws $20, and Mary withdraws half the money in the account, by executing the following commands:",
+        '2' =>
+        "a. List all the different possible values for balance after these three transactions have been completed, assuming that the banking system forces the three processes to run sequentially in some order.",
+        '3' =>
+        "b. What are some other values that could be produced if the system allows the processes to be interleaved? Draw timing diagrams like the one in figure 3.29 to explain how these values can occur."
+    ]
+];
---
+++ resources/views/exercise/listing/3_38.blade.php
@@ -0,0 +1,7 @@
+<p>{{ __('exercises/3_38.description.1') }}</p>
+<pre><code>Peter:	(set! balance (+ balance 10))
+Paul:	(set! balance (- balance 20))
+Mary:	(set! balance (- balance (/ balance 2)))
+</code></pre>
+<p>{{ __('exercises/3_38.description.2') }}</p>
+<p>{{ __('exercises/3_38.description.3') }}</p>
