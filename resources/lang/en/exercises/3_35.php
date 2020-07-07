7da514c30e530e20cdd50a3c7cc79521da9f8d3f
Anton Burenkov <buran665@yandex.ru>
2020-07-07 09:56:59 +0700

    add exercise 3.35

---
+++ resources/lang/en/exercises/3_35.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => 'Squarer as a primitive constraint',
+    'description' =>
+        "Ben Bitdiddle tells Louis that one way to avoid the trouble in exercise 3.34 is to define a squarer as a new primitive constraint. Fill in the missing portions in Ben's outline for a procedure to implement such a constraint:"
+];
