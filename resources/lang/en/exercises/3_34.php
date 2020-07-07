682d710f2a6897f948ee07a855436051a82ecfa3
Anton Burenkov <buran665@yandex.ru>
2020-07-07 10:03:25 +0700

    add exercise 3.34

---
+++ resources/lang/en/exercises/3_34.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => "Squarer",
+    'description' => [
+        '1' =>
+        "Louis Reasoner wants to build a squarer, a constraint device with two terminals such that the value of connector b on the second terminal will always be the square of the value a on the first terminal. He proposes the following simple device made from a multiplier:",
+        '2' =>
+        "There is a serious flaw in this idea. Explain."
+    ]
+];
---
+++ resources/views/exercise/listing/3_34.blade.php
@@ -0,0 +1,5 @@
+<p>{{ __('exercises/3_34.description.1') }}</p>
+<pre><code>(define (squarer a b)
+  (multiplier a a b))
+</code></pre>
+<p>{{ __('exercises/3_34.description.2') }}</p>
