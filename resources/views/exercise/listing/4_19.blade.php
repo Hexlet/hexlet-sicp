c0813f8c8fbc36937aa15e5591de24f073cfd531
Roman Ashikov <rmn.shkv@gmail.com>
2020-07-07 14:01:45 +0700

    add exercise 4.19

---
+++ resources/views/exercise/listing/4_19.blade.php
@@ -0,0 +1,8 @@
+<p>{{ __('exercises/4_19.description.1') }}</p>
+<pre><code>(let ((a 1))
+  (define (f x)
+    (define b (+ a x))
+    (define a 5)
+    (+ a b))
+  (f 10))</code></pre>
+<p>{{ __('exercises/4_19.description.2') }}</p>
