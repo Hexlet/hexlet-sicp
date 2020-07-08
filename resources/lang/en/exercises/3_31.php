2e2ef73c7833aa9b970ca61a9d2ba75b95004e40
Anton Burenkov <buran665@yandex.ru>
2020-07-08 15:56:50 +0700

    add exercise 3.31

---
+++ resources/lang/en/exercises/3_31.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'Accept-action-procedure! procedure',
+    'description' =>
+        "The internal procedure accept-action-procedure! defined in make-wire specifies that when a new action procedure is added to a wire, the procedure is immediately run. " .
+        "Explain why this initialization is necessary. In particular, trace through the half-adder example in the paragraphs above and say how the system's response would differ if we had defined accept-action-procedure! as"
+];
---
+++ resources/views/exercise/listing/3_31.blade.php
@@ -0,0 +1,4 @@
+<p>{{ __('exercises/3_31.description') }}</p>
+<pre><code>(define (accept-action-procedure! proc)
+  (set! action-procedures (cons proc action-procedures)))
+</code></pre>
