b3234916adebd991974cf3b133057f474555212f
Roman Ashikov <rmn.shkv@gmail.com>
2020-07-08 12:19:09 +0700

    add exercise 4.20

---
+++ resources/views/exercise/listing/4_20.blade.php
@@ -0,0 +1,25 @@
+<p>{{ __('exercises/4_20.description.1') }}</p>
+<pre><code>(define (f x)
+  (letrec ((even?
+            (lambda (n)
+              (if (= n 0)
+                  true
+                  (odd? (- n 1)))))
+           (odd?
+            (lambda (n)
+              (if (= n 0)
+                  false
+                  (even? (- n 1))))))
+    rest of body of f))</code></pre>
+<p>{{ __('exercises/4_20.description.2') }}</p>
+<pre><code>(letrec ((var1 exp1) ... (varn expn))
+  body)</code></pre>
+<p>{{ __('exercises/4_20.description.3') }}</p>
+<pre><code>(letrec ((fact
+          (lambda (n)
+            (if (= n 1)
+                1
+                (* n (fact (- n 1)))))))
+  (fact 10))</code></pre>
+<p>{{ __('exercises/4_20.description.4') }}</p>
+<p>{{ __('exercises/4_20.description.5') }}</p>
