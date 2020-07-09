74576a734fcbfcf91501bb351782b1052cc66c20
Anton Burenkov <buran665@yandex.ru>
2020-07-09 11:00:28 +0700

    fix 3_29 title

--- resources/lang/en/exercises/3_29.php
+++ resources/lang/en/exercises/3_29.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title' => 'An or-gate is as a compound digital logic device',
+    'title' => 'An or-gate as a compound digital logic device',
     'description' =>
         "Another way to construct an or-gate is as a compound digital logic device, built from and-gates and inverters. " .
         "Define a procedure or-gate that accomplishes this. What is the delay time of the or-gate in terms of and-gate-delay and inverter-delay?"
