20eaf15b3d33137803feb216a4c67b3c89666b7e
Anton Burenkov <buran665@yandex.ru>
2020-07-09 11:00:04 +0700

    add exercise 3.28

---
+++ resources/lang/en/exercises/3_28.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => 'An or-gate as a primitive function box',
+    'description' =>
+        "Define an or-gate as a primitive function box. Your or-gate constructor should be similar to and-gate."
+];
