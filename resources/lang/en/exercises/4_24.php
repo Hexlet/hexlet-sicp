f4da7d1874e0f460bd0027bae828f02dc3d2b57e
Roman Ashikov <rmn.shkv@gmail.com>
2020-07-09 15:13:35 +0700

    add exercise 4.24

---
+++ resources/lang/en/exercises/4_24.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'Design and carry out some experiments',
+    'description' => "Design and carry out some experiments to compare the speed of the original metacircular " .
+    "evaluator with the version in this section. Use your results to estimate the fraction of time that is " .
+    "spent in analysis versus execution for various procedures."
+];
