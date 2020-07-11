fae378e147b8b57f25c7d6590d5ea24635ead261
Anton Burenkov <buran665@yandex.ru>
2020-07-11 07:24:43 +0700

    add exercise 4.79

---
+++ resources/views/exercise/listing/4_79.blade.php
@@ -0,0 +1,11 @@
+<p>{{ __('exercises/4_79.description.1') }}</p>
+<pre><code>(define (square x)
+  (* x x))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
+(sum-of-squares 3 4)
+</code></pre>
+<p>{{ __('exercises/4_79.description.2') }}</p>
+<p>{{ __('exercises/4_79.description.3') }}</p>
