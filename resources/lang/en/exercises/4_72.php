44181b818a7e64bf064757dfce66936c499132ee
Anton Burenkov <buran665@yandex.ru>
2020-07-14 09:27:59 +0700

    add exercise 4.72

---
+++ resources/lang/en/exercises/4_72.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => "Streams interleaving",
+    'description' =>
+        "Why do disjoin and stream-flatmap interleave the streams rather than simply append them? Give examples that illustrate why interleaving works better. (Hint: Why did we use interleave in section 3.5.3?)"
+];
