3aa44a87d02d4823043b5c5a39eb09383d30b62f
Anton Burenkov <buran665@yandex.ru>
2020-07-14 09:34:15 +0700

    add exercise 4.71

---
+++ resources/views/exercise/listing/4_71.blade.php
@@ -0,0 +1,16 @@
+<p>{{ __('exercises/4_71.description.1') }}</p>
+<pre><code>(define (simple-query query-pattern frame-stream)
+  (stream-flatmap
+   (lambda (frame)
+     (stream-append (find-assertions query-pattern frame)
+                    (apply-rules query-pattern frame)))
+   frame-stream))
+
+(define (disjoin disjuncts frame-stream)
+  (if (empty-disjunction? disjuncts)
+      the-empty-stream
+      (interleave
+       (qeval (first-disjunct disjuncts) frame-stream)
+       (disjoin (rest-disjuncts disjuncts) frame-stream))))
+</code></pre>
+<p>{{ __('exercises/4_71.description.2') }}</p>
