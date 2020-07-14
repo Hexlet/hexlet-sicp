275fecd4e1890160f87476a3225c10d1e4ca7498
Roman Ashikov <rmn.shkv@gmail.com>
2020-07-14 15:26:39 +0700

    add exercise 4.25

---
+++ resources/views/exercise/listing/4_25.blade.php
@@ -0,0 +1,6 @@
+<p>{{ __('exercises/4_25.description.1') }}</p>
+<pre><code>(define (factorial n)
+  (unless (= n 1)
+          (* n (factorial (- n 1)))
+          1))</code></pre>
+<p>{{ __('exercises/4_25.description.2') }}</p>
