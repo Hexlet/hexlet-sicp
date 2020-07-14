52a072a872d12aea3359acf0d41deb98107e4086
Roman Ashikov <rmn.shkv@gmail.com>
2020-07-14 15:43:46 +0700

    add exercise 4.28

---
+++ resources/lang/en/exercises/4_28.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => 'Evaluate the operator using actual-value',
+    'description' => "Eval uses actual-value rather than eval to evaluate the operator before passing it to apply, " .
+    "in order to force the value of the operator. Give an example that demonstrates the need for this forcing."
+];
