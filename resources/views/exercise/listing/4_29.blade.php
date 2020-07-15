49edaea01d55d4de33df72ce78e7d237404a2b24
Roman Ashikov <rmn.shkv@gmail.com>
2020-07-15 22:27:36 +0700

    add exercise 4.29

---
+++ resources/views/exercise/listing/4_29.blade.php
@@ -0,0 +1,13 @@
+<p>{{ __('exercises/4_29.description.1') }}<a href="{{ route('exercises.show', getExercise('4.27')) }}">4.27</a>
+{{ __('exercises/4_29.description.2') }}</p>
+<pre><code>(define (square x)
+  (* x x))
+;;; L-Eval input:
+(square (id 10))
+;;; L-Eval value:
+response
+;;; L-Eval input:
+count
+;;; L-Eval value:
+response</code></pre>
+<p>{{ __('exercises/4_29.description.3') }}</p>
