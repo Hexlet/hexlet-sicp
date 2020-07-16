50b4598a71983433bd6e188df4d3c5a22db3032a
Anton Burenkov <buran665@yandex.ru>
2020-07-16 08:50:01 +0700

    add exercise 4.66

---
+++ resources/lang/en/exercises/4_66.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'title' => "The total salaries of all the computer programmers",
+    'description' => [
+        '1' =>
+        "Ben has been generalizing the query system to provide statistics about the company. For example, to find the total salaries of all the computer programmers one will be able to say",
+        '2' =>
+        "In general, Ben's new system allows expressions of the form",
+        '3' =>
+        "where accumulation-function can be things like sum, average, or maximum. Ben reasons that it should be a cinch to implement this. " .
+        "He will simply feed the query pattern to qeval. This will produce a stream of frames. " .
+        "He will then pass this stream through a mapping function that extracts the value of the designated variable from each frame in the stream and feed the resulting stream of values to the accumulation function. " .
+        "Just as Ben completes the implementation and is about to try it out, Cy walks by, still puzzling over the wheel query result in exercise 4.65. When Cy shows Ben the system's response, Ben groans, ''Oh, no, my simple accumulation scheme won't work!''",
+        '4' =>
+        "What has Ben just realized? Outline a method he can use to salvage the situation."
+    ]
+];
---
+++ resources/views/exercise/listing/4_66.blade.php
@@ -0,0 +1,11 @@
+<p>{{ __('exercises/4_66.description.1') }}</p>
+<pre><code>(sum ?amount
+     (and (job ?x (computer programmer))
+          (salary ?x ?amount)))
+</code></pre>
+<p>{{ __('exercises/4_66.description.2') }}</p>
+<pre><code>(accumulation-function &lt;variable&gt;
+                       &lt;query pattern&gt;)
+</code></pre>
+<p>{{ __('exercises/4_66.description.3') }}</p>
+<p>{{ __('exercises/4_66.description.4') }}</p>
