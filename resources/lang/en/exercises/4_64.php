cdaba00d7930c0e86f87a1c2e10225793b740985
Anton Burenkov <buran665@yandex.ru>
2020-07-16 09:07:58 +0700

    add exercise 4.64

---
+++ resources/lang/en/exercises/4_64.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title' => "An infinite loop",
+    'description' => [
+        '1' =>
+        "Louis Reasoner mistakenly deletes the outranked-by rule (section 4.4.1) from the data base. When he realizes this, he quickly reinstalls it. Unfortunately, he makes a slight change in the rule, and types it in as",
+        '2' =>
+        "Just after Louis types this information into the system, DeWitt Aull comes by to find out who outranks Ben Bitdiddle. He issues the query",
+        '3' =>
+        "After answering, the system goes into an infinite loop. Explain why."
+    ]
+];
