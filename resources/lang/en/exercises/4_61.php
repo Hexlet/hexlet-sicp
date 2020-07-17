80cef7225d9f14f55db8e169cab7caf919cd37db
Anton Burenkov <buran665@yandex.ru>
2020-07-17 21:01:02 +0700

    add exercise 4.61

---
+++ resources/lang/en/exercises/4_61.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => "Rules for next-to relation",
+    'description' => [
+        '1' =>
+        "The following rules implement a next-to relation that finds adjacent elements of a list:",
+        '2' =>
+        "What will the response be to the following queries?"
+    ]
+];
