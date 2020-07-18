11ee0f0b2ccb7cf61b859c28af59a2af90fd17ff
Anton Burenkov <buran665@yandex.ru>
2020-07-18 11:09:07 +0700

    add exercise 4.58

---
+++ resources/lang/en/exercises/4_58.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => "''Big shot'' rule",
+    'description' =>
+        "Define a rule that says that a person is a ''big shot'' in a division if the person works in the division but does not have a supervisor who works in the division."
+];
