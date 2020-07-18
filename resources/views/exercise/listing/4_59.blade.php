5cfcbb444bdc28c965fceaa99d0b4f3dcd4d1ef5
Anton Burenkov <buran665@yandex.ru>
2020-07-18 11:04:59 +0700

    add exercise 4.59

---
+++ resources/views/exercise/listing/4_59.blade.php
@@ -0,0 +1,15 @@
+<p>{{ __('exercises/4_59.description.1') }}</p>
+<pre><code>(meeting accounting (Monday 9am))
+(meeting administration (Monday 10am))
+(meeting computer (Wednesday 3pm))
+(meeting administration (Friday 1pm))
+</code></pre>
+<p>{{ __('exercises/4_59.description.2') }}</p>
+<pre><code>(meeting whole-company (Wednesday 4pm))
+</code></pre>
+<p>{{ __('exercises/4_59.description.3') }}</p>
+<p>{{ __('exercises/4_59.description.4') }}</p>
+<pre><code>(rule (meeting-time ?person ?day-and-time)
+      &lt;rule-body&gt;)
+</code></pre>
+<p>{{ __('exercises/4_59.description.5') }}</p>
