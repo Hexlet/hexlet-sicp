7a262fd609b84538d0321522a3c2ef13c9dc48f0
Roman Ashikov <rmn.shkv@gmail.com>
2020-07-19 14:31:05 +0700

    add exercise 4.35

---
+++ resources/views/exercise/listing/4_35.blade.php
@@ -0,0 +1,7 @@
+<p>{{ __('exercises/4_35.description') }}</p>
+<pre><code>(define (a-pythagorean-triple-between low high)
+  (let ((i (an-integer-between low high)))
+    (let ((j (an-integer-between i high)))
+      (let ((k (an-integer-between j high)))
+        (require (= (+ (* i i) (* j j)) (* k k)))
+        (list i j k)))))</code></pre>
