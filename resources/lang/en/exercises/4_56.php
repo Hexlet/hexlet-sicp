439b536e47ae8c837a02c100010dae8f1f7faf97
Anton Burenkov <buran665@yandex.ru>
2020-07-19 21:40:43 +0700

    add exercises 4.56

---
+++ resources/lang/en/exercises/4_56.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'title' => "Compound queries",
+    'description' => [
+        '1' =>
+        "Formulate compound queries that retrieve the following information:",
+        '2' =>
+        "a. the names of all people who are supervised by Ben Bitdiddle, together with their addresses;",
+        '3' =>
+        "b. all people whose salary is less than Ben Bitdiddle's, together with their salary and Ben Bitdiddle's salary;",
+        '4' =>
+        "c. all people who are supervised by someone who is not in the computer division, together with the supervisor's name and job."
+    ]
+];
---
+++ resources/views/exercise/listing/4_56.blade.php
@@ -0,0 +1,4 @@
+<p>{{ __('exercises/4_56.description.1') }}</p>
+<p>{{ __('exercises/4_56.description.2') }}</p>
+<p>{{ __('exercises/4_56.description.3') }}</p>
+<p>{{ __('exercises/4_56.description.4') }}</p>
