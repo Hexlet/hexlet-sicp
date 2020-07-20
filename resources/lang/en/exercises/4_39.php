e75d34216a2277db750422ec151f34f10adf2262
Roman Ashikov <rmn.shkv@gmail.com>
2020-07-20 17:34:33 +0700

    add exercise 4.39

---
+++ resources/lang/en/exercises/4_39.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => 'The order of the restrictions in the multiple-dwelling',
+    'description' => "Does the order of the restrictions in the multiple-dwelling procedure affect the answer? " .
+    "Does it affect the time to find an answer? If you think it matters, demonstrate a faster program obtained " .
+    "from the given one by reordering the restrictions. If you think it does not matter, argue your case."
+];
---
+++ resources/views/exercise/listing/4_39.blade.php
@@ -0,0 +1,1 @@
+<p>{{ __('exercises/4_39.description') }}</p>
