af37505ab70e4d01a91d646302eb324e624d3451
Roman Ashikov <rmn.shkv@gmail.com>
2020-07-20 17:27:46 +0700

    add exercise 4.37

---
+++ resources/lang/en/exercises/4_37.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Efficiency of generating Pythagorean triples',
+    'description' => [
+        '1' => "Ben Bitdiddle claims that the following method for generating Pythagorean triples is more " .
+        "efficient than the one in exercise ",
+        '2' => ". Is he correct? (Hint: Consider the number of possibilities that must be explored.)",
+    ]
+];
---
+++ resources/views/exercise/listing/4_37.blade.php
@@ -0,0 +1,11 @@
+<p>{{ __('exercises/4_37.description.1') }}<a href="{{ route('exercises.show', getExercise('4.35')) }}">4.35</a>
+{{ __('exercises/4_37.description.2') }}</p>
+<pre><code>(define (a-pythagorean-triple-between low high)
+  (let ((i (an-integer-between low high))
+        (hsq (* high high)))
+    (let ((j (an-integer-between i high)))
+      (let ((ksq (+ (* i i) (* j j))))
+        (require (>= hsq ksq))
+        (let ((k (sqrt ksq)))
+          (require (integer? k))
+          (list i j k))))))</code></pre>
