6e704d6fc9379834a601b80e1e59f0b239a8c222
Anton Burenkov <buran665@yandex.ru>
2020-07-21 10:15:00 +0700

    add exercise 4.50

---
+++ resources/lang/en/exercises/4_50.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => "Special form ramb",
+    'description' =>
+        "Implement a new special form ramb that is like amb except that it searches alternatives in a random order, rather than from left to right. Show how this can help with Alyssa's problem in exercise 4.49."
+];
