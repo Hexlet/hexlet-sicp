5300e0cf70d39b4d106aa0cec1fbc40848e8d0a9
Roman Ashikov <rmn.shkv@gmail.com>
2020-07-21 15:53:46 +0700

    add exercise 4.42

---
+++ resources/lang/en/exercises/4_42.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'title' => 'Solve the "Liars" puzzle',
+    'description' => [
+        '1' => 'Solve the following "Liars" puzzle (from Phillips 1934):',
+        '2' => "Five schoolgirls sat for an examination. Their parents -- so they thought -- showed an undue " .
+        "degree of interest in the result. They therefore agreed that, in writing home about the examination, " .
+        "each girl should make one true statement and one untrue one. The following are the relevant passages from " .
+        "their letters:",
+        '3' => 'Betty: "Kitty was second in the examination. I was only third."',
+        '4' => 'Ethel: "You will be glad to hear that I was on top. Joan was second."',
+        '5' => 'Joan: "I was third, and poor old Ethel was bottom."',
+        '6' => 'Kitty: "I came out second. Mary was only fourth."',
+        '7' => 'Mary: "I was fourth. Top place was taken by Betty."',
+        '8' => 'What in fact was the order in which the five girls were placed?'
+    ]
+];
