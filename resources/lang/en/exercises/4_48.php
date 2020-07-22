858fe02c25f18960bc2b7af80043060abf332874
Anton Burenkov <buran665@yandex.ru>
2020-07-22 10:49:54 +0700

    add exercise 4.48

---
+++ resources/lang/en/exercises/4_48.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => "The grammar extension",
+    'description' =>
+        "Extend the grammar given above to handle more complex sentences. For example, you could extend noun phrases and verb phrases to include adjectives and adverbs, or you could handle compound sentences."
+];
