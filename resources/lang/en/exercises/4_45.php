9f61c2932e744574303afb2f42c4b9ec92aca1da
Anton Burenkov <buran665@yandex.ru>
2020-07-22 11:06:09 +0700

    add exercise 4.45

---
+++ resources/lang/en/exercises/4_45.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title' => "Five different ways of sentence parsing",
+    'description' =>
+        "With the grammar given above, the following sentence can be parsed in five different ways: ''The professor lectures to the student in the class with the cat.'' Give the five parses and explain the differences in shades of meaning among them."
+];
