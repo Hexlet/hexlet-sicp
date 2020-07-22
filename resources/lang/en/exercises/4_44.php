47b230f4598891d0b041d74b494f1e89b36191c8
Anton Burenkov <buran665@yandex.ru>
2020-07-22 11:15:57 +0700

    add exercise 4.44

---
+++ resources/lang/en/exercises/4_44.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => "A nondeterministic program to solve ''eight-queens puzzle''",
+    'description' => [
+        '1' =>
+        "Exercise ",
+        '2' =>
+        " described the ''eight-queens puzzle'' of placing queens on a chessboard so that no two attack each other. Write a nondeterministic program to solve this puzzle."
+    ]
+];
