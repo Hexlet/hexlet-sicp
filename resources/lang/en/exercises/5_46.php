00f9be4c2e061ca4d0528fda679368eb500181bf
Anton Burenkov <buran665@yandex.ru>
2020-07-23 10:09:58 +0700

    add exercise 5.46

---
+++ resources/lang/en/exercises/5_46.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'title' => "The effectiveness of compiling the tree-recursive Fibonacci procedure",
+    'description' => [
+        '1' =>
+        "Carry out an analysis like the one in exercise ",
+        '2' =>
+        " to determine the effectiveness of compiling the tree-recursive Fibonacci procedure",
+        '3' =>
+        "compared to the effectiveness of using the special-purpose Fibonacci machine of figure 5.12. (For measurement of the interpreted performance, see exercise ",
+        '4' =>
+        ".) For Fibonacci, the time resource used is not linear in n; hence the ratios of stack operations will not approach a limiting value that is independent of n."
+    ]
+];
---
+++ resources/views/exercise/listing/5_46.blade.php
@@ -0,0 +1,9 @@
+<p>{{ __('exercises/5_46.description.1') }}<a href="{{ route('exercises.show', getExercise('5.45')) }}">5.45</a>
+{{ __('exercises/5_46.description.2') }}</p>
+<pre><code>(define (fib n)
+  (if (< n 2)
+      n
+      (+ (fib (- n 1)) (fib (- n 2)))))
+</code></pre>
+<p>{{ __('exercises/5_46.description.3') }}<a href="{{ route('exercises.show', getExercise('5.29')) }}">5.29</a>
+{{ __('exercises/5_46.description.4') }}</p>
