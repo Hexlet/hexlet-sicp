3452f31ad43eb6c28a644f45e2dd106b11154dfc
Roman Ashikov <rmn.shkv@gmail.com>
2020-07-23 13:47:54 +0700

    add exercise 5.8

---
+++ resources/views/exercise/listing/5_8.blade.php
@@ -0,0 +1,11 @@
+<p>{{ __('exercises/5_8.description.1') }}</p>
+<pre><code>start
+  (goto (label here))
+here
+  (assign a (const 3))
+  (goto (label there))
+here
+  (assign a (const 4))
+  (goto (label there))
+there</code></pre>
+<p>{{ __('exercises/5_8.description.2') }}</p>
