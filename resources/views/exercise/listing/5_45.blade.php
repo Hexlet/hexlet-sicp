8728aa66efc534f435d4f65d5f8ac26b7523e6e7
Anton Burenkov <buran665@yandex.ru>
2020-07-24 19:02:56 +0700

    add exercise 5.45

---
+++ resources/views/exercise/listing/5_45.blade.php
@@ -0,0 +1,7 @@
+<p>{{ __('exercises/5_45.description.1') }}</p>
+<p>{{ __('exercises/5_45.description.2') }}<a href="{{ route('exercises.show', getExercise('5.27')) }}">5.27</a>
+{{ __('exercises/5_45.description.3') }}<a href="{{ route('exercises.show', getExercise('5.14')) }}">5.14</a>
+{{ __('exercises/5_45.description.4') }}</p>
+<p>{{ __('exercises/5_45.description.5') }}</p>
+<p>{{ __('exercises/5_45.description.6') }}</p>
+<p>{{ __('exercises/5_45.description.7') }}</p>
