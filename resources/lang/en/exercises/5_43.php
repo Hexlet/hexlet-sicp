c192aeeffd0e19e676101e096bd8e8a2869349a3
Anton Burenkov <buran665@yandex.ru>
2020-07-25 09:46:33 +0700

    add exercise 5.43

---
+++ resources/lang/en/exercises/5_43.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => "The compiler and internal definitions",
+    'description' => [
+        '1' =>
+        "We argued in section 4.1.6 that internal definitions for block structure should not be considered ''real'' defines. Rather, a procedure body should be interpreted as if the internal variables being defined were installed as ordinary lambda variables initialized to their correct values using set!. Section 4.1.6 and exercise ",
+        '2' =>
+        " showed how to modify the metacircular interpreter to accomplish this by scanning out internal definitions. Modify the compiler to perform the same transformation before it compiles a procedure body."
+    ]
+];
