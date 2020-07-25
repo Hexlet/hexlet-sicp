3df42e7921cc82949fb63188b7a5bdcaaca44a8f
Anton Burenkov <buran665@yandex.ru>
2020-07-25 09:38:13 +0700

    add exercise 5.44

---
+++ resources/views/exercise/listing/5_44.blade.php
@@ -0,0 +1,7 @@
+<p>{{ __('exercises/5_44.description.1') }}<a href="{{ route('exercises.show', getExercise('5.38')) }}">5.38</a>
+{{ __('exercises/5_44.description.2') }}<a href="{{ route('exercises.show', getExercise('5.38')) }}">5.38</a>
+{{ __('exercises/5_44.description.3') }}</p>
+<pre><code>(lambda (+ * a b x y)
+  (+ (* a x) (* b y)))
+</code></pre>
+<p>{{ __('exercises/5_44.description.4') }}</p>
