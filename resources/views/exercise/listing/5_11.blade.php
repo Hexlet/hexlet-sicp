9aebccfe495a6d57fab486e355f9cfbd02b83a85
Roman Ashikov <rmn.shkv@gmail.com>
2020-07-26 16:43:44 +0700

    add exercise 5.11

---
+++ resources/views/exercise/listing/5_11.blade.php
@@ -0,0 +1,8 @@
+<p>{{ __('exercises/5_11.description.1') }}</p>
+<pre><code>(save y)
+(save x)
+(restore y)</code></pre>
+<p>{{ __('exercises/5_11.description.2') }}</p>
+<p>{{ __('exercises/5_11.description.3') }}</p>
+<p>{{ __('exercises/5_11.description.4') }}</p>
+<p>{{ __('exercises/5_11.description.5') }}</p>
