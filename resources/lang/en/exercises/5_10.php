cdd849e7b617023e00626aa7299b77ff9a72b76a
Roman Ashikov <rmn.shkv@gmail.com>
2020-07-26 16:35:06 +0700

    add exercise 5.10

---
+++ resources/lang/en/exercises/5_10.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => "New syntax for register-machine",
+    'description' =>  "Design a new syntax for register-machine instructions and modify the simulator to use " .
+    "your new syntax. Can you implement your new syntax without changing any part of the simulator except the " .
+    "syntax procedures in this section?"
+];
