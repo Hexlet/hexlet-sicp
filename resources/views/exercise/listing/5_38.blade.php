1cc3030a9eff5372d91a896eb5343fd4124af611
Anton Burenkov <buran665@yandex.ru>
2020-07-27 21:18:06 +0700

    add exercise 5.38

---
+++ resources/views/exercise/listing/5_38.blade.php
@@ -0,0 +1,10 @@
+<p>{{ __('exercises/5_38.description.1') }}</p>
+<pre><code>(assign val (op lookup-variable-value) (const a) (reg env))
+(assign val (op +) (reg val) (const 1))
+</code></pre>
+<p>{{ __('exercises/5_38.description.2') }}</p>
+<p>{{ __('exercises/5_38.description.3') }}</p>
+<p>{{ __('exercises/5_38.description.4') }}</p>
+<p>{{ __('exercises/5_38.description.5') }}</p>
+<p>{{ __('exercises/5_38.description.6') }}</p>
+<p>{{ __('exercises/5_38.description.7') }}</p>
