1635cf11be46c581d90320a48a93f9bc23822684
Anton Burenkov <buran665@yandex.ru>
2020-07-29 11:07:05 +0700

    add exercise 5.34

---
+++ resources/lang/en/exercises/5_34.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => "The essential difference between the code for iterative and recursive versions of factorial",
+    'description' => [
+        '1' =>
+        "Compile the iterative factorial procedure",
+        '2' =>
+        "Annotate the resulting code, showing the essential difference between the code for iterative and recursive versions of factorial that makes one process build up stack space and the other run in constant stack space."
+    ]
+];
