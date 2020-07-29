80395263dd419bcaf5fb2fb7d6df64a1bbd6f5a3
Anton Burenkov <buran665@yandex.ru>
2020-07-29 11:16:23 +0700

    add exercise 5.33

---
+++ resources/lang/en/exercises/5_33.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => "Сompare the resulting code of a factorial procedure",
+    'description' => [
+        '1' =>
+        "Consider the following definition of a factorial procedure, which is slightly different from the one given above:",
+        '2' =>
+        "Compile this procedure and compare the resulting code with that produced for factorial. Explain any differences you find. Does either program execute more efficiently than the other?"
+    ]
+];
---
+++ resources/views/exercise/listing/5_33.blade.php
@@ -0,0 +1,7 @@
+<p>{{ __('exercises/5_33.description.1') }}</p>
+<pre><code>(define (factorial-alt n)
+  (if (= n 1)
+      1
+      (* n (factorial-alt (- n 1)))))
+</code></pre>
+<p>{{ __('exercises/5_33.description.2') }}</p>
