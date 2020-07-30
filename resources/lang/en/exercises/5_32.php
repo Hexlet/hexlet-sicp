acf2c11dff115288d6b19a13d8be43f4865a29df
Anton Burenkov <buran665@yandex.ru>
2020-07-30 21:59:05 +0700

    add exercise 5.32

---
+++ resources/lang/en/exercises/5_32.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title' => "Combinations whose operator is a symbol",
+    'description' => [
+        '1' =>
+        "Using the preserving mechanism, the compiler will avoid saving and restoring env around the evaluation of the operator of a combination in the case where the operator is a symbol. " .
+        "We could also build such optimizations into the evaluator. Indeed, the explicit-control evaluator of section 5.4 already performs a similar optimization, by treating combinations with no operands as a special case.",
+        '2' =>
+        "a. Extend the explicit-control evaluator to recognize as a separate class of expressions combinations whose operator is a symbol, and to take advantage of this fact in evaluating such expressions.",
+        '3' =>
+        "b. Alyssa P. Hacker suggests that by extending the evaluator to recognize more and more special cases we could incorporate all the compiler's optimizations, and that this would eliminate the advantage of compilation altogether. What do you think of this idea?"
+    ]
+];
---
+++ resources/views/exercise/listing/5_32.blade.php
@@ -0,0 +1,3 @@
+<p>{{ __('exercises/5_32.description.1') }}</p>
+<p>{{ __('exercises/5_32.description.2') }}</p>
+<p>{{ __('exercises/5_32.description.3') }}</p>
