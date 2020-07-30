a883ebf9f3617c0b843aad06621caffb03fb42a7
Anton Burenkov <buran665@yandex.ru>
2020-07-30 22:05:11 +0700

    add exercise 5.31

---
+++ resources/lang/en/exercises/5_31.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => "Superfluous operations",
+    'description' =>
+        "In evaluating a procedure application, the explicit-control evaluator always saves and restores the env register around the evaluation of the operator, saves and restores env around the evaluation of each operand (except the final one), saves and restores argl around the evaluation of each operand, and saves and restores proc around the evaluation of the operand sequence. " .
+        "For each of the following combinations, say which of these save and restore operations are superfluous and thus could be eliminated by the compiler's preserving mechanism:"
+];
---
+++ resources/views/exercise/listing/5_31.blade.php
@@ -0,0 +1,9 @@
+<p>{{ __('exercises/5_31.description') }}</p>
+<pre><code>(f 'x 'y)
+
+((f) 'x 'y)
+
+(f (g 'x) y)
+
+(f (g 'x) 'y)
+</code></pre>
