798eba774447cd3b81add4ac9fe0065532f476c3
Anton Burenkov <buran665@yandex.ru>
2020-08-03 14:52:50 +0700

    fix lint

--- resources/lang/en/exercises/5_25.php
+++ resources/lang/en/exercises/5_25.php
@@ -3,5 +3,5 @@
 return [
     'title' => "Normal order evaluator based on lazy evaluator",
     'description' =>
-        "Modify the evaluator so that it uses normal-order evaluation, based on the lazy evaluator of section 4.2."
+        "Modify the evaluator so that it uses normal-order evaluation, based on the lazy evaluator of section 4.2.",
 ];
