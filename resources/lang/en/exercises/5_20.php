a3f99a75067e6d01acb378c6eacae90987b94e90
Anton Burenkov <buran665@yandex.ru>
2020-08-04 10:23:12 +0700

    add exercise 5.20

---
+++ resources/lang/en/exercises/5_20.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => "Box-and-pointer and memory-vector representations",
+    'description' => [
+        '1' =>
+        "Draw the box-and-pointer representation and the memory-vector representation (as in figure 5.14) of the list structure produced by",
+        '2' =>
+        "with the free pointer initially p1. What is the final value of free ? What pointers represent the values of x and y ?",
+    ],
+];
