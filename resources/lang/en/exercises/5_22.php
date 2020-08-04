bb775bb5a86879cec947906270d78386d87cdf11
Anton Burenkov <buran665@yandex.ru>
2020-08-04 10:10:57 +0700

    add exercise 5.22

---
+++ resources/lang/en/exercises/5_22.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => "A register machine for append procedure",
+    'description' => [
+        '1' =>
+        "Exercise ",
+        '2' =>
+        " of section 3.3.1 presented an append procedure that appends two lists to form a new list and an append! procedure that splices two lists together. " .
+        "Design a register machine to implement each of these procedures. Assume that the list-structure memory operations are available as primitive operations.",
+    ],
+];
