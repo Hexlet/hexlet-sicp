61e5f194db9f49faa3c4a745c0638f9ebae928b6
Anton Burenkov <buran665@yandex.ru>
2020-08-05 15:02:14 +0700

    add exercise 5.16

---
+++ resources/lang/en/exercises/5_16.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title' => "Instruction tracing",
+    'description' =>
+        "Augment the simulator to provide for instruction tracing. That is, before each instruction is executed, the simulator should print the text of the instruction. " .
+        "Make the machine model accept trace-on and trace-off messages to turn tracing on and off.",
+];
