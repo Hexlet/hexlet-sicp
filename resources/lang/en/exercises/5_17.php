567769cbba91b1eeae7a608cacc83781ecf04090
Anton Burenkov <buran665@yandex.ru>
2020-08-05 14:52:29 +0700

    add exercise 5.17

---
+++ resources/lang/en/exercises/5_17.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title' => "Labels print",
+    'description' => [
+        '1' =>
+        "Extend the instruction tracing of exercise ",
+        '2' =>
+        " so that before printing an instruction, the simulator prints any labels that immediately precede that instruction in the controller sequence. Be careful to do this in a way that does not interfere with instruction counting (exercise ",
+        '3' =>
+        "). You will have to make the simulator retain the necessary label information.",
+    ],
+];
