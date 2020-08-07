22b72ae5fe1189804c1341db8d00bcb30e47bfc0
Anton Burenkov <buran665@yandex.ru>
2020-08-07 11:31:44 +0700

    add links in 1.18

--- resources/lang/en/exercises/1_18.php
+++ resources/lang/en/exercises/1_18.php
@@ -2,6 +2,12 @@
 
 return [
     'title' => 'Iterative process for multiplying two integers',
-    'description' =>
-        'Using the results of exercises 1.16 and 1.17, devise a procedure that generates an iterative process for multiplying two integers in terms of adding, doubling, and halving and uses a logarithmic number of steps.',
+    'description' => [
+        '1' =>
+        "Using the results of exercises ",
+        '2' =>
+        " and ",
+        '3' =>
+        ", devise a procedure that generates an iterative process for multiplying two integers in terms of adding, doubling, and halving and uses a logarithmic number of steps.",
+    ],
 ];
