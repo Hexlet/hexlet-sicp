44aafcf9594beebc361d6a9b47de5a7bd333bf1c
Maksim Litvinov <litvinovmksm@gmail.com>
2020-08-07 18:02:42 +0300

    Add links to exercise 1.24

--- resources/views/exercise/listing/1_24.blade.php
+++ resources/views/exercise/listing/1_24.blade.php
@@ -1,1 +1,2 @@
-<p>{{ __('exercises/1_24.description.1') }}</p>
+<p>{{ __('exercises/1_24.description.1') }}<a href="{{ route('exercises.show', getExercise('1.22')) }}">1.22</a>
+{{ __('exercises/1_24.description.2') }}</p>
