a56432cd9743e399b87bdacfedff1f8a9040e97b
Anton Burenkov <buran665@yandex.ru>
2020-08-12 21:45:14 +0700

    add link in 4.67

--- resources/lang/en/exercises/4_67.php
+++ resources/lang/en/exercises/4_67.php
@@ -2,9 +2,12 @@
 
 return [
     'title' => "The query system loop detector",
-    'description' =>
-        "Devise a way to install a loop detector in the query system so as to avoid the kinds of simple loops illustrated in the text and in exercise 4.64. " .
-        "The general idea is that the system should maintain some sort of history of its current chain of deductions and should not begin processing a query that it is already working on. " .
+    'description' => [
+        '1' =>
+        "Devise a way to install a loop detector in the query system so as to avoid the kinds of simple loops illustrated in the text and in exercise ",
+        '2' =>
+        ". The general idea is that the system should maintain some sort of history of its current chain of deductions and should not begin processing a query that it is already working on. " .
         "Describe what kind of information (patterns and frames) is included in this history, and how the check should be made. " .
         "(After you study the details of the query-system implementation in section 4.4.4, you may want to modify the system to include your loop detector.)",
+    ],
 ];
--- resources/views/exercise/listing/4_67.blade.php
+++ resources/views/exercise/listing/4_67.blade.php
@@ -1,1 +1,2 @@
-<p>{{ __('exercises/4_67.description') }}</p>
+<p>{{ __('exercises/4_67.description.1') }}<a href="{{ route('exercises.show', getExercise('4.64')) }}">4.64</a>
+{{ __('exercises/4_67.description.2') }}</p>
