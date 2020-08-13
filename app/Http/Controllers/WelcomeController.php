7b4cc76e936a6ea5c041bcc7f3acbe042ab653d4
Alexey Shobanov <shobanov@gmail.com>
2020-08-13 20:25:50 +0000

    add view landing

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,21 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Activity;
+use App\Chapter;
 use App\Comment;
+use App\Exercise;
+use App\User;
 use Illuminate\View\View;
 
 class WelcomeController extends Controller
 {
     public function index(): View
     {
-        $logItems = Activity::latest()->with('causer')->limit(10)->get();
-        $chart = getChart();
-        $comments = Comment::latest()->limit(10)->get();
 
-        return view('welcome', compact(
-            'logItems',
-            'chart',
-            'comments'
+        if (auth()->user()) {
+            $logItems = Activity::latest()->with('causer')->limit(10)->get();
+            $chart = getChart();
+            $comments = Comment::latest()->limit(10)->get();
+
+            return view('welcome', compact(
+                'logItems',
+                'chart',
+                'comments'
+            ));
+        }
+
+        $countComments = Comment::count();
+        $countExercises = Exercise::count();
+        $countChapters = Chapter::count();
+        $countUsers = User::count();
+
+
+        return view('landing', compact(
+            'countComments',
+            'countExercises',
+            'countChapters',
+            'countUsers'
         ));
     }
 }
---
+++ resources/views/landing.blade.php
@@ -0,0 +1,91 @@
+@extends('layouts.land')
+@section('content')
+<div class="jumbotron jumbotron-fluid bg-light">
+    <div class="container">
+        <div class="row align-items-lg-center">
+            <div class="col-12 col-lg-7">
+                <h1 class="">СИКП — книга об информатике (computer science)</h1>
+                <blockquote class="text-secondary mb-1">Это одна из великих классиков информатики. Я купил свой первый экземпляр 15 лет назад и до сих пор не чувствую, что выучил все, чему должна научить книга.</blockquote>
+                <p class="text-secondary font-italic mb-3 mb-lg-5">Пол Грэм</p>
+                <p class="h4 text-secondary">Хочешь прокачаться в computer science?</p>
+                <p class="h4 text-secondary">Проходи упражнения из книги на sicp.hexlet.io и отмечай свой прогресс.</p>
+                <h4 class="mt-3 mt-lg-5">sicp.hexlet.io — это:</h4>
+                <div class="row no-gutters my-2 ml-lg-n5 ml-md-n3">
+                    <div class="col-6 col-md text-center my-2">
+                        <div class="h2 text-primary">
+                            {{ $countChapters }}
+                        </div>
+                        <div class="text-secondary">
+                            разделов
+                        </div>
+                    </div>
+                    <div class="col-6 col-md text-center my-2">
+                        <div class="h2 text-primary">
+                            {{ $countExercises }}
+                        </div>
+                        <div class="text-secondary">
+                            упражнений
+                        </div>
+                    </div>
+                    <div class="col-6 col-md text-center my-2">
+                        <div class="h2 text-primary">
+                            {{ $countUsers }}
+                        </div>
+                        <div class="text-secondary">
+                            участников
+                        </div>
+                    </div>
+                    <div class="col-6 col-md text-center my-2">
+                        <div class="h2 text-primary">
+                            {{ $countComments }}
+                        </div>
+                        <div class="text-secondary">
+                            комментариев
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-5 card shadow-lg p-2 p-lg-4">
+                <h2>Возможности</h4>
+                <ul class="text-secondary">
+                    <li>Отмечай прочитанные главы</li>
+                    <li>Смотри рейтинг других участников</li>
+                    <li>Обсуждай главы и задачи</li>
+                    <li>Отмечай пройденные упражнения</li>
+                </ul>
+                <button type="button" class="btn btn-primary btn-lg btn-block mb-4">Начать учиться</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="jumbotron jumbotron-fluid mb-0">
+    <div class="container">
+        <div class="row align-items-lg-center">
+            <div class="col-12 col-md text-center">
+                <i class="fas fa-book-reader fa-7x"></i>
+                <div class="h4 text-secondary mt-2">
+                    читай
+                </div>
+            </div>
+            <div class="col-12 col-md text-center">
+                <i class="fas fa-laptop-code fa-7x"></i>
+                <div class="h4 text-secondary mt-2">
+                    решай
+                </div>
+            </div>
+            <div class="col-12 col-md text-center">
+                <i class="fas fa-pen-alt fa-7x"></i>
+                <div class="h4 text-secondary mt-2">
+                    общайся
+                </div>
+            </div>
+            <div class="col-12 col-md text-center">
+                <i class="fas fa-award fa-7x"></i>
+                <div class="h4 text-secondary mt-2">
+                    стань лучшим
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
