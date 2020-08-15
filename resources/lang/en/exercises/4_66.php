1c4c85adde060d68904dd990bb8bd0952baa7255
Anton Burenkov <buran665@yandex.ru>
2020-08-15 13:18:57 +0700

    add link at 4.66

--- resources/lang/en/exercises/4_66.php
+++ resources/lang/en/exercises/4_66.php
@@ -11,8 +11,10 @@ return [
         "where accumulation-function can be things like sum, average, or maximum. Ben reasons that it should be a cinch to implement this. " .
         "He will simply feed the query pattern to qeval. This will produce a stream of frames. " .
         "He will then pass this stream through a mapping function that extracts the value of the designated variable from each frame in the stream and feed the resulting stream of values to the accumulation function. " .
-        "Just as Ben completes the implementation and is about to try it out, Cy walks by, still puzzling over the wheel query result in exercise 4.65. When Cy shows Ben the system's response, Ben groans, ''Oh, no, my simple accumulation scheme won't work!''",
+        "Just as Ben completes the implementation and is about to try it out, Cy walks by, still puzzling over the wheel query result in exercise ",
         '4' =>
+        ". When Cy shows Ben the system's response, Ben groans, ''Oh, no, my simple accumulation scheme won't work!''",
+        '5' =>
         "What has Ben just realized? Outline a method he can use to salvage the situation.",
     ],
 ];
--- resources/views/exercise/listing/4_66.blade.php
+++ resources/views/exercise/listing/4_66.blade.php
@@ -7,5 +7,6 @@
 <pre><code>(accumulation-function &lt;variable&gt;
                        &lt;query pattern&gt;)
 </code></pre>
-<p>{{ __('exercises/4_66.description.3') }}</p>
-<p>{{ __('exercises/4_66.description.4') }}</p>
+<p>{{ __('exercises/4_66.description.3') }}<a href="{{ route('exercises.show', getExercise('4.65')) }}">4.65</a>
+{{ __('exercises/4_66.description.4') }}</p>
+<p>{{ __('exercises/4_66.description.5') }}</p>
