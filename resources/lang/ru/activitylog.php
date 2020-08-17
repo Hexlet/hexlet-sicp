dbe1594e651e6b1242fed6f8c1e9affbc12764f4
Anton Burenkov <buran665@yandex.ru>
2020-08-17 16:19:08 +0700

    add action_add_solution in russian

--- resources/lang/ru/activitylog.php
+++ resources/lang/ru/activitylog.php
@@ -11,4 +11,5 @@ return [
     'action_unknown' => 'Сделал что-то неизвестное',
     'action_completed_exercise' => 'Выполнил упражнение',
     'action_destroy_exercise' => 'Убрал упражнение из выполненных',
+    'action_add_solution' => 'Добавил решение для упражнения',
 ];
