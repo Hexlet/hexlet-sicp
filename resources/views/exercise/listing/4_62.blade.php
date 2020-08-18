9d51b6b39398ec1772228cd7d562564e3c568cbd
Anton Burenkov <buran665@yandex.ru>
2020-08-18 10:20:19 +0700

    add link at 4.62

--- resources/views/exercise/listing/4_62.blade.php
+++ resources/views/exercise/listing/4_62.blade.php
@@ -1,1 +1,2 @@
-<p>{{ __('exercises/4_62.description') }}</p>
+<p>{{ __('exercises/4_62.description.1') }}<a href="{{ route('exercises.show', getExercise('2.17')) }}">2.17</a>
+{{ __('exercises/4_62.description.2') }}</p>
