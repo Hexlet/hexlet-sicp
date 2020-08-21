ee5af1369a486e37599c46f57deca80c3b193041
Anton Burenkov <buran665@yandex.ru>
2020-08-21 12:04:52 +0700

    add link to 4.19

--- resources/lang/en/exercises/4_19.php
+++ resources/lang/en/exercises/4_19.php
@@ -8,8 +8,8 @@ return [
         '2' => "Ben asserts that the result should be obtained using the sequential rule for define: b is " .
         "defined to be 11, then a is defined to be 5, so the result is 16. Alyssa objects that mutual recursion " .
         "requires the simultaneous scope rule for internal procedure definitions, and that it is unreasonable to " .
-        "treat procedure names differently from other names. Thus, she argues for the mechanism implemented in " .
-        "exercise 4.16. This would lead to a being unassigned at the time that the value for b is to be computed. " .
+        "treat procedure names differently from other names. Thus, she argues for the mechanism implemented in exercise ",
+        '3' => ". This would lead to a being unassigned at the time that the value for b is to be computed. " .
         "Hence, in Alyssa's view the procedure should produce an error. Eva has a third opinion. She says that if " .
         "the definitions of a and b are truly meant to be simultaneous, then the value 5 for a should be used in " .
         "evaluating b. Hence, in Eva's view a should be 5, b should be 15, and the result should be 20. Which (if " .
--- resources/views/exercise/listing/4_19.blade.php
+++ resources/views/exercise/listing/4_19.blade.php
@@ -5,4 +5,5 @@
     (define a 5)
     (+ a b))
   (f 10))</code></pre>
-<p>{{ __('exercises/4_19.description.2') }}</p>
+<p>{{ __('exercises/4_19.description.2') }}<a href="{{ route('exercises.show', getExercise('4.16')) }}">4.16</a>
+{{ __('exercises/4_19.description.3') }}</p>
