3e0e9591175b832e27ba0c4f072f080bc225391c
Anton Burenkov <buran665@yandex.ru>
2020-08-22 10:00:42 +0700

    add link at 4.16

--- resources/lang/en/exercises/4_16.php
+++ resources/lang/en/exercises/4_16.php
@@ -4,12 +4,13 @@ return [
     'title' => 'Implement the method for interpreting internal definitions',
     'description' => [
         '1' => "In this exercise we implement the method just described for interpreting internal " .
-        "definitions. We assume that the evaluator supports let (see exercise 4.6).",
-        '2' => "a. Change lookup-variable-value (section 4.1.3) to signal an error if the value it " .
+        "definitions. We assume that the evaluator supports let (see exercise ",
+        '2' => ").",
+        '3' => "a. Change lookup-variable-value (section 4.1.3) to signal an error if the value it " .
         "finds is the symbol *unassigned*.",
-        '3' => "b. Write a procedure scan-out-defines that takes a procedure body and returns an " .
+        '4' => "b. Write a procedure scan-out-defines that takes a procedure body and returns an " .
         "equivalent one that has no internal definitions, by making the transformation described above.",
-        '4' => "c. Install scan-out-defines in the interpreter, either in make-procedure or in " .
+        '5' => "c. Install scan-out-defines in the interpreter, either in make-procedure or in " .
         "procedure-body (see section 4.1.3). Which place is better? Why?",
     ],
 ];
--- resources/views/exercise/listing/4_16.blade.php
+++ resources/views/exercise/listing/4_16.blade.php
@@ -1,4 +1,5 @@
-<p>{{ __('exercises/4_16.description.1') }}</p>
-<p>{{ __('exercises/4_16.description.2') }}</p>
+<p>{{ __('exercises/4_16.description.1') }}<a href="{{ route('exercises.show', getExercise('4.6')) }}">4.6</a>
+{{ __('exercises/4_16.description.2') }}</p>
 <p>{{ __('exercises/4_16.description.3') }}</p>
 <p>{{ __('exercises/4_16.description.4') }}</p>
+<p>{{ __('exercises/4_16.description.5') }}</p>
