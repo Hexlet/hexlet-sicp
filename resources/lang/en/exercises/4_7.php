8250ac3c716b10f4e0100645eb8bcf9fb2d7b6c8
Anton Burenkov <buran665@yandex.ru>
2020-08-24 13:03:16 +0700

    add link at 4.7

--- resources/lang/en/exercises/4_7.php
+++ resources/lang/en/exercises/4_7.php
@@ -7,9 +7,8 @@ return [
         "from left to right, and each binding is made in an environment in which all of the preceding bindings " .
         "are visible. For example",
         '2' => "returns 39. Explain how a let* expression can be rewritten as a set of nested let expressions, and " .
-        "write a procedure let*->nested-lets that performs this transformation. If we have already implemented let " .
-        "(exercise 4.6) and we want to extend the evaluator to handle let*, is it sufficient to add a clause to " .
-        "eval whose action is",
-        '3' => "or must we explicitly expand let* in terms of non-derived expressions?",
+        "write a procedure let*->nested-lets that performs this transformation. If we have already implemented let (exercise ",
+        '3' => ") and we want to extend the evaluator to handle let*, is it sufficient to add a clause to eval whose action is",
+        '4' => "or must we explicitly expand let* in terms of non-derived expressions?",
     ],
 ];
--- resources/views/exercise/listing/4_7.blade.php
+++ resources/views/exercise/listing/4_7.blade.php
@@ -3,6 +3,7 @@
       (y (+ x 2))
       (z (+ x y 5)))
   (* x z))</code></pre>
-<p>{{ __('exercises/4_7.description.2') }}</p>
+<p>{{ __('exercises/4_7.description.2') }}<a href="{{ route('exercises.show', getExercise('4.6')) }}">4.6</a>
+{{ __('exercises/4_7.description.3') }}</p>
 <pre><code>(eval (let*->nested-lets exp) env)</code></pre>
- <p>{{ __('exercises/4_7.description.3') }}</p>
+ <p>{{ __('exercises/4_7.description.4') }}</p>
