8334fb9b68440d5c008e93b3af1cb690200cf4cd
Maksim Litvinov <litvinovmksm@gmail.com>
2020-08-25 17:35:05 +0300

    Add links to exercise 2.27

--- resources/lang/en/exercises/2_27.php
+++ resources/lang/en/exercises/2_27.php
@@ -2,7 +2,11 @@
 
 return [
     'title' => 'Deep reverse',
-    'description' =>
-        "Modify your reverse procedure of exercise 2.18 to produce a deep-reverse procedure that takes a list as argument and returns as its value the list with its elements reversed and with all sublists deep-reversed as well. " .
+    'description' => [
+        '1' =>
+        "Modify your reverse procedure of exercise ",
+        '2' =>
+        " to produce a deep-reverse procedure that takes a list as argument and returns as its value the list with its elements reversed and with all sublists deep-reversed as well. " .
         "For example,",
+    ],
 ];
