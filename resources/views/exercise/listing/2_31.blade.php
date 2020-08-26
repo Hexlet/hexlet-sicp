3943efc5c5c434e3d8d0f5f022548e7edfa04751
Maksim Litvinov <litvinovmksm@gmail.com>
2020-08-26 17:17:24 +0300

    Add links to exercise 2.31

--- resources/views/exercise/listing/2_31.blade.php
+++ resources/views/exercise/listing/2_31.blade.php
@@ -1,3 +1,4 @@
-<p>{{ __('exercises/2_31.description') }}</p>
+<p>{{ __('exercises/2_31.description.1') }}<a href="{{ route('exercises.show', getExercise('2.30')) }}">2.30</a>
+{{ __('exercises/2_31.description.2') }}</p>
 <pre><code>(define (square-tree tree) (tree-map square tree))
 </code></pre>
