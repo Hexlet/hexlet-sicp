57bef1f1e6c20fb0b3636fa9e397b86bda9b9e80
Anton Burenkov <buran665@yandex.ru>
2020-08-31 10:21:17 +0700

    add link at 3.79

--- resources/lang/en/exercises/3_79.php
+++ resources/lang/en/exercises/3_79.php
@@ -2,6 +2,10 @@
 
 return [
     'title' => 'Generalize the solve-2nd procedure',
-    'description' =>
-        "Generalize the solve-2nd procedure of exercise 3.78 so that it can be used to solve general second-order differential equations d²y/dt² = f(dy/dt, y).",
+    'description' => [
+        '1' =>
+        "Generalize the solve-2nd procedure of exercise ",
+        '2' =>
+        " so that it can be used to solve general second-order differential equations d²y/dt² = f(dy/dt, y).",
+    ],
 ];
--- resources/views/exercise/listing/3_79.blade.php
+++ resources/views/exercise/listing/3_79.blade.php
@@ -1,1 +1,2 @@
-<p>{{ __('exercises/3_79.description') }}</p>
+<p>{{ __('exercises/3_79.description.1') }}<a href="{{ route('exercises.show', getExercise('3.78')) }}">3.78</a>
+{{ __('exercises/3_79.description.2') }}</p>
