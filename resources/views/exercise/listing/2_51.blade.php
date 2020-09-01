20f54a84e1ffd7ebd827b11a375cc32065cd3c1b
Maksim Litvinov <litvinovmksm@gmail.com>
2020-09-01 17:50:41 +0300

    Add links to exercise 2.51

--- resources/views/exercise/listing/2_51.blade.php
+++ resources/views/exercise/listing/2_51.blade.php
@@ -1,1 +1,2 @@
-<p>{{ __('exercises/2_51.description') }}</p>
+<p>{{ __('exercises/2_51.description.1') }}<a href="{{ route('exercises.show', getExercise('2.50')) }}">2.50</a>
+{{ __('exercises/2_51.description.2') }}</p>
