840039294deea64e950e12dfd17ade329a474871
Anton Burenkov <buran665@yandex.ru>
2020-09-02 12:55:09 +0700

    add link at 3.75

--- resources/views/exercise/listing/3_75.blade.php
+++ resources/views/exercise/listing/3_75.blade.php
@@ -1,8 +1,9 @@
-<p>{{ __('exercises/3_75.description.1') }}</p>
+<p>{{ __('exercises/3_75.description.1') }}<a href="{{ route('exercises.show', getExercise('3.74')) }}">3.74</a>
+{{ __('exercises/3_75.description.2') }}</p>
 <pre><code>(define (make-zero-crossings input-stream last-value)
   (let ((avpt (/ (+ (stream-car input-stream) last-value) 2)))
     (cons-stream (sign-change-detector avpt last-value)
                  (make-zero-crossings (stream-cdr input-stream)
                                       avpt))))
 </code></pre>
-<p>{{ __('exercises/3_75.description.2') }}</p>
+<p>{{ __('exercises/3_75.description.3') }}</p>
