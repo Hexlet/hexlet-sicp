5f36a42db152ab136e6c274863b770f6c4fe2aff
Maksim Litvinov <litvinovmksm@gmail.com>
2020-09-03 17:09:51 +0300

    Add links to exercise 2.52

--- resources/lang/en/exercises/2_52.php
+++ resources/lang/en/exercises/2_52.php
@@ -6,10 +6,12 @@ return [
         '1' =>
         "Make changes to the square limit of wave shown in figure 2.9 by working at each of the levels described above. In particular:",
         '2' =>
-        "a.  Add some segments to the primitive wave painter of exercise 2.49 (to add a smile, for example).",
+        "a.  Add some segments to the primitive wave painter of exercise ",
         '3' =>
-        "b.  Change the pattern constructed by corner-split (for example, by using only one copy of the up-split and right-split images instead of two).",
+        " (to add a smile, for example).",
         '4' =>
+        "b.  Change the pattern constructed by corner-split (for example, by using only one copy of the up-split and right-split images instead of two).",
+        '5' =>
         "c.  Modify the version of square-limit that uses square-of-four so as to assemble the corners in a different pattern. (For example, you might make the big Mr. Rogers look outward from each corner of the square.)",
     ],
 ];
