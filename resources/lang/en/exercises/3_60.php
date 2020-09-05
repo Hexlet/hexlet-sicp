b9ec53964eec60fabed9b2b91b3fc6b3cac0f9a2
Anton Burenkov <buran665@yandex.ru>
2020-09-05 13:21:35 +0700

    add link at 3.60

--- resources/lang/en/exercises/3_60.php
+++ resources/lang/en/exercises/3_60.php
@@ -4,9 +4,11 @@ return [
     'title' => "Procedure for multiplying series",
     'description' => [
         '1' =>
-        "With power series represented as streams of coefficients as in exercise 3.59, adding series is implemented by add-streams. " .
-        "Complete the definition of the following procedure for multiplying series:",
+        "With power series represented as streams of coefficients as in exercise ",
         '2' =>
-        "You can test your procedure by verifying that sin²x + cos²x = 1, using the series from exercise 3.59.",
+        ", adding series is implemented by add-streams. " .
+        "Complete the definition of the following procedure for multiplying series:",
+        '3' =>
+        "You can test your procedure by verifying that sin²x + cos²x = 1, using the series from exercise ",
     ],
 ];
