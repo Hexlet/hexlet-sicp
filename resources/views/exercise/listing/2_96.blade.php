52489c934f5082c86e767b98246ad87bca556990
Anton Burenkov <buran665@yandex.ru>
2020-09-06 20:23:57 +0700

    add link at 2.96

--- resources/views/exercise/listing/2_96.blade.php
+++ resources/views/exercise/listing/2_96.blade.php
@@ -1,8 +1,8 @@
-<p>{{ __('exercises/2_96.description.1') }}</p>
+<p>{{ __('exercises/2_96.description.1') }}<a href="{{ route('exercises.show', getExercise('2.95')) }}">2.95</a></p>
 <p>{{ __('exercises/2_96.description.2') }}</p>
 <p>{{ __('exercises/2_96.description.3') }}</p>
 <ul>
-    <li>{{ __('exercises/2_96.description.4') }}</li>
+    <li>{{ __('exercises/2_96.description.4') }}<a href="{{ route('exercises.show', getExercise('2.96')) }}">2.96</a></li>
     <li>{{ __('exercises/2_96.description.5') }}</li>
     <li>{{ __('exercises/2_96.description.6') }}</li>
 </ul>
