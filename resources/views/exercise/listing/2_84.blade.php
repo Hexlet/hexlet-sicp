1eeec660ccbdc1e5c9251482ff885778d3f5c2bb
Anton Burenkov <buran665@yandex.ru>
2020-09-09 11:36:52 +0700

    add link at 2.83

--- resources/views/exercise/listing/2_84.blade.php
+++ resources/views/exercise/listing/2_84.blade.php
@@ -1,1 +1,2 @@
-<p>{{ __('exercises/2_84.description') }}</p>
+<p>{{ __('exercises/2_84.description.1') }}<a href="{{ route('exercises.show', getExercise('2.83')) }}">2.83</a>
+{{ __('exercises/2_84.description.2') }}</p>
