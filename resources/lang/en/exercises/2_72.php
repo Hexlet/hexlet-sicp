9ba9a61c331626f548534b41801c1f4acd48c8a4
Anton Burenkov <buran665@yandex.ru>
2020-09-10 12:29:56 +0700

    add links at 2.72

--- resources/lang/en/exercises/2_72.php
+++ resources/lang/en/exercises/2_72.php
@@ -2,8 +2,14 @@
 
 return [
     'title' => 'Growth order for character encoding',
-    'description' =>
-        "Consider the encoding procedure that you designed in exercise 2.68. What is the order of growth in the number of steps needed to encode a symbol? " .
+    'description' => [
+        '1' =>
+        "Consider the encoding procedure that you designed in exercise ",
+        '2' =>
+        ". What is the order of growth in the number of steps needed to encode a symbol? " .
         "Be sure to include the number of steps needed to search the symbol list at each node encountered. To answer this question in general is difficult. " .
-        "Consider the special case where the relative frequencies of the n symbols are as described in exercise 2.71, and give the order of growth (as a function of n) of the number of steps needed to encode the most frequent and least frequent symbols in the alphabet.",
+        "Consider the special case where the relative frequencies of the n symbols are as described in exercise ",
+        '3' =>
+        ", and give the order of growth (as a function of n) of the number of steps needed to encode the most frequent and least frequent symbols in the alphabet.",
+    ],
 ];
--- resources/views/exercise/listing/2_72.blade.php
+++ resources/views/exercise/listing/2_72.blade.php
@@ -1,1 +1,3 @@
-<p>{{ __('exercises/2_72.description') }}</p>
+<p>{{ __('exercises/2_72.description.1') }}<a href="{{ route('exercises.show', getExercise('2.68')) }}">2.68</a>
+{{ __('exercises/2_72.description.2') }}<a href="{{ route('exercises.show', getExercise('2.71')) }}">2.71</a>
+{{ __('exercises/2_72.description.3') }}</p>
