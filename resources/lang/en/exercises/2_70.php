223379824a76858f098136f53461d825f0ef64b4
Anton Burenkov <buran665@yandex.ru>
2020-09-11 15:17:00 +0700

    add links at 2.70

--- resources/lang/en/exercises/2_70.php
+++ resources/lang/en/exercises/2_70.php
@@ -6,8 +6,12 @@ return [
         '1' =>
         "The following eight-symbol alphabet with associated relative frequencies was designed to efficiently encode the lyrics of 1950s rock songs. (Note that the ''symbols'' of an ''alphabet'' need not be individual letters.)",
         '2' =>
-        "Use generate-huffman-tree (exercise 2.69) to generate a corresponding Huffman tree, and use encode (exercise 2.68) to encode the following message:",
+        "Use generate-huffman-tree (exercise ",
         '3' =>
+        ") to generate a corresponding Huffman tree, and use encode (exercise ",
+        '4' =>
+        ") to encode the following message:",
+        '5' =>
         "How many bits are required for the encoding? What is the smallest number of bits that would be needed to encode this song if we used a fixed-length code for the eight-symbol alphabet?",
     ],
 ];
--- resources/views/exercise/listing/2_70.blade.php
+++ resources/views/exercise/listing/2_70.blade.php
@@ -5,7 +5,9 @@ BOOM    1   SHA 3
 GET     2   YIP 9
 JOB     2   WAH 1
 </code></pre>
-<p>{{ __('exercises/2_70.description.2') }}</p>
+<p>{{ __('exercises/2_70.description.2') }}<a href="{{ route('exercises.show', getExercise('2.69')) }}">2.69</a>
+{{ __('exercises/2_70.description.3') }}<a href="{{ route('exercises.show', getExercise('2.68')) }}">2.68</a>
+{{ __('exercises/2_70.description.4') }}</p>
 <pre><code>Get a job
 
 Sha na na na na na na na na
@@ -18,4 +20,4 @@ Wah yip yip yip yip yip yip yip yip yip
 
 Sha boom
 </code></pre>
-<p>{{ __('exercises/2_70.description.3') }}</p>
+<p>{{ __('exercises/2_70.description.5') }}</p>
