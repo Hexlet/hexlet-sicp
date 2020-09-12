b6b8192b52b9ec81ba3b8c6a88a93dd3330f4ce5
Anton Burenkov <buran665@yandex.ru>
2020-09-12 11:06:13 +0700

    add link at 2.68

--- resources/views/exercise/listing/2_68.blade.php
+++ resources/views/exercise/listing/2_68.blade.php
@@ -5,4 +5,5 @@
       (append (encode-symbol (car message) tree)
               (encode (cdr message) tree))))
 </code></pre>
-<p>{{ __('exercises/2_68.description.2') }}</p>
+<p>{{ __('exercises/2_68.description.2') }}<a href="{{ route('exercises.show', getExercise('2.67')) }}">2.67</a>
+{{ __('exercises/2_68.description.3') }}</p>
