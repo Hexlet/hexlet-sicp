a069b01b3f7ab0725170e9404f7edb5625183f69
Anton Burenkov <buran665@yandex.ru>
2020-09-15 13:43:45 +0700

    add img to 2.24

--- resources/lang/en/exercises/2_24.php
+++ resources/lang/en/exercises/2_24.php
@@ -2,7 +2,11 @@
 
 return [
     'title' => 'Box-and-pointer structure',
-    'description' =>
+    'description' => [
+        '1' =>
         "Suppose we evaluate the expression (list 1 (list 2 (list 3 4))). " .
         "Give the result printed by the interpreter, the corresponding box-and-pointer structure, and the interpretation of this as a tree (as in figure 2.6).",
+        '2' =>
+        "Figure 2.6:  The list structure in figure 2.5 viewed as a tree.",
+    ],
 ];
