791b8c9b8889b5b5ba13faa6568df67952321ca3
Anton Burenkov <buran665@yandex.ru>
2020-09-17 12:35:33 +0700

    add link at 1.23

--- resources/lang/en/exercises/1_23.php
+++ resources/lang/en/exercises/1_23.php
@@ -9,7 +9,9 @@ return [
         "This suggests that the values used for test-divisor should not be 2, 3, 4, 5, 6, ..., but rather 2, 3, 5, 7, 9, .... " .
         "To implement this change, define a procedure next that returns 3 if its input is equal to 2 and otherwise returns its input plus 2. " .
         "Modify the smallest-divisor procedure to use (next test-divisor) instead of (+ test-divisor 1). With timed-prime-test incorporating this modified version of smallest-divisor, " .
-        "run the test for each of the 12 primes found in exercise 1.22. Since this modification halves the number of test steps, you should expect it to run about twice as fast. " .
+        "run the test for each of the 12 primes found in exercise ",
+        '2' =>
+        ". Since this modification halves the number of test steps, you should expect it to run about twice as fast. " .
         "Is this expectation confirmed? If not, what is the observed ratio of the speeds of the two algorithms, and how do you explain the fact that it is different from 2?",
     ],
 ];
