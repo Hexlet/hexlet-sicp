20ab3b91ff6caa85bade5566ea01642418e6a04c
Anton Burenkov <buran665@yandex.ru>
2020-09-21 10:48:40 +0700

    add image at 2.83

--- resources/lang/en/exercises/2_83.php
+++ resources/lang/en/exercises/2_83.php
@@ -2,8 +2,12 @@
 
 return [
     'title' => 'A generic raise operation',
-    'description' =>
+    'description' => [
+        '1' =>
         "Suppose you are designing a generic arithmetic system for dealing with the tower of types shown in figure 2.25: integer, rational, real, complex. " .
         "For each type (except complex), design a procedure that raises objects of that type one level in the tower. " .
         "Show how to install a generic raise operation that will work for each type (except complex).",
+        '2' =>
+        "Figure 2.25:  A tower of types.",
+    ],
 ];
