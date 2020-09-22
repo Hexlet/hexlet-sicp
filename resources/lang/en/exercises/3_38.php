ac70657f8e5885ef4c3a0289f85d6bf2e63e89aa
Anton Burenkov <buran665@yandex.ru>
2020-09-22 14:47:31 +0700

    add image at 3.38

--- resources/lang/en/exercises/3_38.php
+++ resources/lang/en/exercises/3_38.php
@@ -10,5 +10,7 @@ return [
         "a. List all the different possible values for balance after these three transactions have been completed, assuming that the banking system forces the three processes to run sequentially in some order.",
         '3' =>
         "b. What are some other values that could be produced if the system allows the processes to be interleaved? Draw timing diagrams like the one in figure 3.29 to explain how these values can occur.",
+        '4' =>
+        "Figure 3.29:  Timing diagram showing how interleaving the order of events in two banking withdrawals can lead to an incorrect final balance.",
     ],
 ];
