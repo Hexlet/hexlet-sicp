9a87e186df35deab7e0cada4accf997ba7a68c14
Anton Burenkov <buran665@yandex.ru>
2020-09-30 11:12:13 +0700

    add images at 5.14

--- resources/views/exercise/listing/5_14.blade.php
+++ resources/views/exercise/listing/5_14.blade.php
@@ -1,1 +1,43 @@
-<p>{{ __('exercises/5_14.description') }}</p>
+<p>{{ __('exercises/5_14.description.1') }}</p>
+<img class="img-fluid" src="{{ asset('img/exercises/5_14.gif') }}" alt="5.14">
+<pre><code>(controller
+  gcd-loop
+    (assign a (op read))
+    (assign b (op read))
+  test-b
+    (test (op =) (reg b) (const 0))
+    (branch (label gcd-done))
+    (assign t (op rem) (reg a) (reg b))
+    (assign a (reg b))
+    (assign b (reg t))
+    (goto (label test-b))
+  gcd-done
+    (perform (op print) (reg a))
+    (goto (label gcd-loop)))
+</code></pre>
+<p>{{ __('exercises/5_14.description.2') }}</p>
+<img class="img-fluid" src="{{ asset('img/exercises/5_12.gif') }}" alt="5.12">
+<pre><code>(controller
+   (assign continue (label fact-done))     ; set up final return address
+ fact-loop
+   (test (op =) (reg n) (const 1))
+   (branch (label base-case))
+   ;; Set up for the recursive call by saving n and continue.
+   ;; Set up continue so that the computation will continue
+   ;; at after-fact when the subroutine returns.
+   (save continue)
+   (save n)
+   (assign n (op -) (reg n) (const 1))
+   (assign continue (label after-fact))
+   (goto (label fact-loop))
+ after-fact
+   (restore n)
+   (restore continue)
+   (assign val (op *) (reg n) (reg val))   ; val now contains n(n - 1)!
+   (goto (reg continue))                   ; return to caller
+ base-case
+   (assign val (const 1))                  ; base case: 1! = 1
+   (goto (reg continue))                   ; return to caller
+ fact-done)
+</code></pre>
+<p>{{ __('exercises/5_14.description.3') }}</p>
