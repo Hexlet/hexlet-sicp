bd1476f3964d5409772116e80ecd6f3bbd9f4ccd
Nikolay Gagarinov <feycot@gmail.com>
2020-10-01 19:31:38 +0500

    refactor gh controller

--- app/Http/Controllers/Auth/Social/GithubController.php
+++ app/Http/Controllers/Auth/Social/GithubController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Auth\Social;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use Exception;
+use Illuminate\Contracts\Validation\Validator as ValidatorContract;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Socialite;
 use Validator;
-use Exception;
 
 class GithubController extends Controller
 {
@@ -18,13 +20,13 @@ class GithubController extends Controller
     public function __construct(Socialite $socialite, User $user)
     {
         $this->socialite = $socialite;
-        $this->user      = $user;
+        $this->user = $user;
     }
 
     /**
      * Redirect the user to the GitHub authentication page.
      */
-    public function redirectToProvider()
+    public function redirectToProvider(): RedirectResponse
     {
         try {
             return $this->socialite::driver('github')->scopes(['user:email'])->redirect();
@@ -36,7 +38,7 @@ class GithubController extends Controller
     /**
      * Obtain the user information from GitHub.
      */
-    public function handleProviderCallback()
+    public function handleProviderCallback(): RedirectResponse
     {
         try {
             $socialiteUser = $this->socialite::driver('github')->user();
@@ -54,43 +56,33 @@ class GithubController extends Controller
             return $this->sendFailedResponse();
         }
 
-        return $this->loginOrCreateAccount($name, $email);
-    }
+        $user = User::withTrashed()->firstOrNew(['email' => $email]);
 
-    protected function loginOrCreateAccount($name, $email)
-    {
-        $userForAuth = User::firstOrNew(['email' => $email]);
-
-        if (User::where('email', $email)->doesntExist()) {
-            $deleteUser = User::withTrashed()->where('email', $email)->first();
-
-            if ($deleteUser) {
-                $deleteUser->restore();
-                Auth::login($deleteUser, true);
-                flash()->success(__('auth.logged_in'));
-                return redirect()->route('my');
-            }
-
-            $userForAuth->name              = $name;
-            $userForAuth->email_verified_at = now();
-            $userForAuth->password          = Hash::make(random_bytes(10));
-
-            $userForAuth->saveOrFail();
+        if ($user->trashed()) {
+            $user->restore();
         }
 
-        Auth::login($userForAuth, true);
+        if (!$user->exists) {
+            $user->name = $name;
+            $user->email_verified_at = now();
+            $user->password = Hash::make(random_bytes(10));
+            $user->saveOrFail();
+        }
+
+        Auth::login($user, true);
         flash()->success(__('auth.logged_in'));
 
         return redirect()->route('my');
     }
 
-    protected function sendFailedResponse($msg = null)
+    private function sendFailedResponse($msg = null): RedirectResponse
     {
         flash()->error($msg ?? __('auth.provider_fails'));
+
         return redirect()->back()->withInput();
     }
 
-    protected function validator(array $data)
+    private function validator(array $data): ValidatorContract
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'min:2','max:255'],
