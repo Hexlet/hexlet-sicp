2f38a62518bdb5ce073585e6d00524dbce10beb8
Anton Burenkov <buran665@yandex.ru>
2020-10-04 11:46:12 +0700

    add image at 5.45

--- resources/views/exercise/listing/5_45.blade.php
+++ resources/views/exercise/listing/5_45.blade.php
@@ -5,3 +5,28 @@
 <p>{{ __('exercises/5_45.description.5') }}</p>
 <p>{{ __('exercises/5_45.description.6') }}</p>
 <p>{{ __('exercises/5_45.description.7') }}</p>
+<img class="img-fluid" src="{{ asset('img/exercises/5_12.gif') }}" alt="5.12">
+<pre><code>(controller
+   (assign continue (label fact-done))     ; set up final return address
+ fact-loop
+   (test (op =) (reg n) (const 1))
+   (branch (label base-case))
+   ;; Set up for the recursive call by saving n and continue.
+   ;; Set up continue so that the computation will continue
+   ;; at after-fact when the subroutine returns.
+   (save continue)
+   (save n)
+   (assign n (op -) (reg n) (const 1))
+   (assign continue (label after-fact))
+   (goto (label fact-loop))
+ after-fact
+   (restore n)
+   (restore continue)
+   (assign val (op *) (reg n) (reg val))   ; val now contains n(n - 1)!
+   (goto (reg continue))                   ; return to caller
+ base-case
+   (assign val (const 1))                  ; base case: 1! = 1
+   (goto (reg continue))                   ; return to caller
+ fact-done)
+</code></pre>
+<p>{{ __('exercises/5_45.description.8') }}</p>
