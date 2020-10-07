a0d70300dcae9f6c93eab2ee1edb2950fad964ea
Anton Burenkov <buran665@yandex.ru>
2020-10-07 11:05:34 +0700

    remove gaps at 1.10

--- resources/views/exercise/listing/1_10.blade.php
+++ resources/views/exercise/listing/1_10.blade.php
@@ -1,6 +1,5 @@
 <p>{{ __('exercises/1_10.description.1') }}</p>
-<pre><code>
-(define (A x y)
+<pre><code>(define (A x y)
   (cond ((= y 0) 0)
         ((= x 0) (* 2 y))
         ((= y 1) 2)
@@ -8,16 +7,14 @@
                  (A x (- y 1))))))
 </code></pre>
 <p>{{ __('exercises/1_10.description.2') }}</p>
-<pre><code>
-(A 1 10)
+<pre><code>(A 1 10)
 
 (A 2 4)
 
 (A 3 3)
 </code></pre>
 <p>{{ __('exercises/1_10.description.3') }}</p>
-<pre><code>
-(define (f n) (A 0 n))
+<pre><code>(define (f n) (A 0 n))
 
 (define (g n) (A 1 n))
 
