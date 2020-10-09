555ea8cb14d7876dc5db144f54f7ac9874e0d885
Anton Burenkov <buran665@yandex.ru>
2020-10-09 13:46:21 +0700

    remove gap at 1.25

--- resources/views/exercise/listing/1_25.blade.php
+++ resources/views/exercise/listing/1_25.blade.php
@@ -1,6 +1,5 @@
 <p>{{ __('exercises/1_25.description.1') }}</p>
-<pre><code>
-(define (expmod base exp m)
+<pre><code>(define (expmod base exp m)
   (remainder (fast-expt base exp) m))
 </code></pre>
 <p>{{ __('exercises/1_25.description.2') }}</p>
