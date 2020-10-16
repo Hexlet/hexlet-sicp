5de8ac7a2063a6e3595307b29e7e682fd54701ff
Anton Burenkov <buran665@yandex.ru>
2020-10-16 21:02:17 +0700

    remove gaps at 1.34

--- resources/views/exercise/listing/1_34.blade.php
+++ resources/views/exercise/listing/1_34.blade.php
@@ -1,11 +1,9 @@
 <p>{{ __('exercises/1_34.description.1') }}</p>
-<pre><code>
-(define (f g)
+<pre><code>(define (f g)
   (g 2))
 </code></pre>
 <p>{{ __('exercises/1_34.description.2') }}</p>
-<pre><code>
-(f square)
+<pre><code>(f square)
 4
 
 (f (lambda (z) (* z (+ z 1))))
