c0d812d5c2997c5900cfee074d1c7d3f8047b5fb
Anton Burenkov <buran665@yandex.ru>
2020-10-18 18:23:51 +0700

    remove gaps at 1.40-1.43

--- resources/views/exercise/listing/1_40.blade.php
+++ resources/views/exercise/listing/1_40.blade.php
@@ -1,5 +1,4 @@
 <p>{{ __('exercises/1_40.description.1') }}</p>
-<pre><code>
-(newtons-method (cubic a b c) 1)
+<pre><code>(newtons-method (cubic a b c) 1)
 </code></pre>
 <p>{{ __('exercises/1_40.description.2') }}</p>
