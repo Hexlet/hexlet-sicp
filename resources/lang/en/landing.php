66606f0aaa1bf1544379ce1ad6ddec5ddd1fa580
ava <ava239@yandex.ru>
2020-11-04 23:52:15 +0300

    Change content positioning to make slider smaller

--- resources/lang/en/landing.php
+++ resources/lang/en/landing.php
@@ -2,9 +2,18 @@
 
 return [
     'title' => 'SICP — a book about computer science',
-    'epigraph' => 'This is one of the great classics of computer science.
+    'epigraph' => [
+        'This is one of the great classics of computer science.
     I bought my first copy 15 years ago and still don\'t feel like I\'ve learned everything the book has to teach.',
-    'author_of_epigraph' => 'Paul Graham',
+        'I just dumped the C++ server I wrote last year for a new one in Racket.
+    May not scale, but it is winning for development even as a newbie.',
+        'I wish I had read SICP when I was in high school!',
+    ],
+    'author_of_epigraph' => [
+        'Paul Graham',
+        'John Carmack',
+        'John Carmack',
+    ],
     'what_for' => 'Do you want to upgrade to computer science?',
     'what_do' => 'Follow the exercises from the book at sicp.hexlet.io and note your progress.',
     'this_is' => 'this is',
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,38 +3,32 @@
 
     <main role="main">
 
-        <div id="sicpCarousel" class="carousel slide mb-5" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#sicpCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#sicpCarousel" data-slide-to="1"></li>
-            </ol>
+        <div id="sicpCarousel" class="carousel slide mb-3" data-ride="carousel" data-speed="7000">
             <div class="carousel-inner">
                 <div class="sicp-carousel-item carousel-item active">
                     <div class="container">
                         <div class="sicp-carousel-caption text-left text-secondary">
-                            <div class="row">
-                                <div class="col-md-6 mt-5">
-                                    <h1>{{ __('landing.title') }}</h1>
-                                    <blockquote class="mb-1">{{ __('landing.epigraph') }}</blockquote>
-                                    <p class="font-italic mb-3 mb-lg-5">{{ __('landing.author_of_epigraph') }}</p>
-                                </div>
-                                <div class="col-md-6 d-none d-sm-block">
-                                    <img class="img-fluid" src="{{ asset('img/Patchouli_Gives_SICP.png') }}"
-                                         alt="{{ __('landing.start_learning') }}">
-                                </div>
+                            <div class="float-lg-right w-25 d-none d-sm-block">
+                                <img class="img-fluid"
+                                     src="{{ asset('img/Patchouli_Gives_SICP.png') }}"
+                                     alt="{{ __('landing.start_learning') }}">
+                            </div>
+                            <div>
+                                <h1>{{ __('landing.title') }}</h1>
+                                <blockquote class="mb-1">{{ __('landing.epigraph.0') }}</blockquote>
+                                <p class="font-italic mb-3 mb-lg-5">{{ __('landing.author_of_epigraph.0') }}</p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="sicp-carousel-item carousel-item">
                     <div class="container">
-                        <div class="sicp-carousel-caption text-left row text-secondary align-top">
-
-                            <div class="col-md-6 d-sm-none d-md-block mt-5 ">
-                                <div class="h1">{{ __('landing.what_for') }}</div>
-                                <p class="h4">{{ __('landing.what_do') }}</p>
-                            </div>
-                            <div class="col-md-6">
+                        <div class="sicp-carousel-caption text-left text-secondary">
+                            <div class="w-75">
+                                <blockquote class="mb-1">{{ __('landing.epigraph.1') }}</blockquote>
+                                <p class="font-italic mb-3 mb-lg-5">{{ __('landing.author_of_epigraph.1') }}</p>
+                                <blockquote class="mb-1">{{ __('landing.epigraph.2') }}</blockquote>
+                                <p class="font-italic mb-3 mb-lg-5">{{ __('landing.author_of_epigraph.2') }}</p>
                             </div>
                         </div>
                     </div>
@@ -53,8 +47,10 @@
         <div class="container ">
 
             <div class="row">
-                <div class="col-md-6">
-                    <h2>sicp.hexlet.io — {{ __('landing.this_is') }}:</h2>
+                <div class="col-md-7">
+                    <div class="h4 text-secondary">{{ __('landing.what_for') }}</div>
+                    <p class="h4 text-secondary">{{ __('landing.what_do') }}</p>
+                    <h2 class="h4 mt-3 mt-lg-5">sicp.hexlet.io — {{ __('landing.this_is') }}:</h2>
                     <div class="row no-gutters my-2 ml-lg-n5 ml-md-n3">
                         <div class="col-6 col-md text-center my-2">
                             <div class="h2 text-primary">
@@ -90,11 +86,11 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-5 card shadow-lg p-2 p-lg-4">
 
 
                     <h2>{{ __('landing.features') }}</h2>
-                    <ul class="h4">
+                    <ul class="text-secondary">
                         <li>{{ __('landing.features_list.1') }}</li>
                         <li>{{ __('landing.features_list.2') }}</li>
                         <li>{{ __('landing.features_list.3') }}</li>
