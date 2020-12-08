1413c51534e09af082de3ca2678d282002ee3712
Maksim Litvinov <litvinovmksm@gmail.com>
2020-12-08 18:36:32 +0300

    update tests

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,6 +12,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'github_name' => $faker->userName,
+        'hexlet_nickname' => $faker->userName,
         // password
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
         'remember_token' => Str::random(10),
