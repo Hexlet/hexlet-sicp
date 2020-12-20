54e5c65969add69b7821bcd508335e7d1866825b
feycot <feycot@gmail.com>
2020-12-20 21:22:22 +0500

    add tests

---
+++ tests/Feature/Http/Controllers/SolutionControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Solution;
+use Tests\ControllerTestCase;
+
+class SolutionControllerTest extends ControllerTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        $solutions = factory(Solution::class, 5)->make();
+        $this->user->solutions()->saveMany($solutions);
+
+        $this->actingAs($this->user);
+    }
+
+    public function testIndex()
+    {
+        $route = route('solutions.index');
+
+        $response = $this->get($route);
+
+        $response->assertSessionDoesntHaveErrors();
+        $response->assertOk();
+    }
+
+    public function testShow()
+    {
+        $solution = Solution::inRandomOrder()->first();
+        $route = route('solutions.show', $solution);
+
+        $response = $this->get($route);
+        $response->assertSessionDoesntHaveErrors();
+        $response->assertOk();
+    }
+}
