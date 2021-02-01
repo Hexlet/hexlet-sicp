9bbd1bbff6a31817040dcac0493069bae7e19e6c
Anton Burenkov <buran665@yandex.ru>
2021-02-01 12:52:19 +0700

    Update 1_7.blade.php

--- resources/views/exercise/solution_stub/1_7.blade.php
+++ resources/views/exercise/solution_stub/1_7.blade.php
@@ -5,7 +5,7 @@
 {!! $solution !!}
 ;;; END
 
-(check-equal? (round (* 1000 (solution 4.0))) 2000.0)
-(check-equal? (round (* 1000 (solution 100.0))) 10000.0)
-(check-equal? (round (* 1000 (solution 1000000.0))) 1000000.0)
-(check-equal? (round (* 1000 (solution 0.04))) 200.0)
+(check-equal? (round (* 1000 (sqrt 4.0))) 2000.0)
+(check-equal? (round (* 1000 (sqrt 100.0))) 10000.0)
+(check-equal? (round (* 1000 (sqrt 1000000.0))) 1000000.0)
+(check-equal? (round (* 1000 (sqrt 0.04))) 200.0)
