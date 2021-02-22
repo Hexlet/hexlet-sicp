2a2118b7941d1360c7caa53abef651034d61626a
Anton Burenkov <buran665@yandex.ru>
2021-02-22 14:34:35 +0700

    add tests for 2.38

---
+++ resources/views/exercise/solution_stub/2_38_solution.blade.php
@@ -0,0 +1,13 @@
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
