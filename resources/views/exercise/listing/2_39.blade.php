a206f71f5ddbede014d9882b62ebe75e40f75ec3
Anton Burenkov <buran665@yandex.ru>
2021-02-23 13:35:02 +0700

    add tests for 2.39

--- resources/views/exercise/listing/2_39.blade.php
+++ resources/views/exercise/listing/2_39.blade.php
@@ -1,9 +1,9 @@
 <p>{{ __('exercises/2_39.description.1') }}<a href="{{ route('exercises.show', getExercise('2.18')) }}">2.18</a>
 {{ __('exercises/2_39.description.2') }}<a href="{{ route('exercises.show', getExercise('2.38')) }}">2.38</a>
 {{ __('exercises/2_39.description.3') }}</p>
-<pre><code>(define (reverse sequence)
+<pre><code>(define (reverse-right sequence)
   (fold-right (lambda (x y) &lt;??&gt;) nil sequence))
 
-(define (reverse sequence)
+(define (reverse-left sequence)
   (fold-left (lambda (x y) &lt;??&gt;) nil sequence))
 </code></pre>
