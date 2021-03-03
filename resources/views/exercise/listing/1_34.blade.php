57d09a3a0e05cd311f7ca42f9ee00585bc9b62dc
Anton Burenkov <buran665@yandex.ru>
2021-03-04 00:08:49 +0700

    change the answer display in chapter 1

--- resources/views/exercise/listing/1_34.blade.php
+++ resources/views/exercise/listing/1_34.blade.php
@@ -4,9 +4,9 @@
 </code></pre>
 <p>{{ __('exercises/1_34.description.2') }}</p>
 <pre><code>(f square)
-4
+<i>4</i>
 
 (f (lambda (z) (* z (+ z 1))))
-6
+<i>6</i>
 </code></pre>
 <p>{{ __('exercises/1_34.description.3') }}</p>
