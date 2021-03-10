1dc61f4102421a5437f9d0a730cc49e49d3f1fad
Anton Burenkov <buran665@yandex.ru>
2021-03-10 12:13:26 +0700

    add tests for 1.23

---
+++ resources/views/exercise/solution_stub/1_23_solution.blade.php
@@ -0,0 +1,15 @@
+(define (square x) (* x x))
+
+(define (next x)
+  (if (= x 2)
+      3
+      (+ x 2)))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
