5bfceb83de9726f87506c945418e3ad63b6ae5c3
Anton Burenkov <buran665@yandex.ru>
2021-03-12 15:58:51 +0700

    add tests for 1.28

--- resources/lang/en/exercises/1_28.php
+++ resources/lang/en/exercises/1_28.php
@@ -11,7 +11,7 @@ return [
         "we check to see if we have discovered a ``nontrivial square root of 1 modulo n,'' that is, a number not equal to 1 or n - 1 whose square is equal to 1 modulo n. " .
         "It is possible to prove that if such a nontrivial square root of 1 exists, then n is not prime. It is also possible to prove that if n is an odd number that is not prime, " .
         "then, for at least half the numbers a<n, computing an-1 in this way will reveal a nontrivial square root of 1 modulo n. (This is why the Miller-Rabin test cannot be fooled.) " .
-        "Modify the expmod procedure to signal if it discovers a nontrivial square root of 1, and use this to implement the Miller-Rabin test with a procedure analogous to fermat-test. " .
+        "Modify the expmod procedure to signal if it discovers a nontrivial square root of 1, and use this to implement the Miller-Rabin test with a procedure miller-rabin-test analogous to fermat-test. " .
         "Check your procedure by testing various known primes and non-primes. Hint: One convenient way to make expmod signal is to have it return 0. ",
     ],
 ];
