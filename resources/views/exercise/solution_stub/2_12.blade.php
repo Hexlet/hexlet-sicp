2a292ee4139a6c15de8994249a7bcd7f93080447
Anton Burenkov <buran665@yandex.ru>
2021-03-27 12:20:21 +0700

    add tests for 2.12

---
+++ resources/views/exercise/solution_stub/2_12.blade.php
@@ -0,0 +1,31 @@
+#lang racket/base
+(require rackunit)
+
+;;; BEGIN
+{!! $solution !!}
+;;; END
+
+(define (make-interval a b) (cons a b))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+(define center-test 4)
+
+(define interval (make-interval 3 5))
+
+(define center-interval (make-center-percent center-test 25))
+
+(define center-test2 100)
+
+(define interval2 (make-interval 90 110))
+
+(define center-interval2 (make-center-percent center-test2 10))
+
+
+(check-equal? (center interval) center-test)
+(check-equal? interval center-interval)
+(check-equal? (center interval2) center-test2)
+(check-equal? interval2 center-interval2)
+(check-equal? (percent interval) 25)
+(check-equal? (percent center-interval2) 10)
