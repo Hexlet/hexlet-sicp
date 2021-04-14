f3a31b4fd41e8e7bcdd0987021972c439698979d
Anton Burenkov <buran665@yandex.ru>
2021-04-14 14:36:17 +0700

    add tests for 1.39

---
+++ resources/views/exercise/solution_stub/1_39.blade.php
@@ -0,0 +1,10 @@
+#lang racket
+(require rackunit)
+
+;;; BEGIN
+{!! $solution !!}
+;;; END
+
+(check-equal? (tan-cf (/ pi 4) 100) 1.0)
+(check-equal? (tan-cf 0 10) 0.0)
+(check-equal? (round (* 100 (tan-cf (/ pi 3) 100))) 173.0)
