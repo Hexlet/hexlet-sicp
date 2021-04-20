f3e41d74ce77fb72a932338e9c4e3a186e3e2e27
Anton Burenkov <buran665@yandex.ru>
2021-04-20 13:08:50 +0700

    add tests for 2.68

---
+++ resources/views/exercise/solution_stub/2_68.blade.php
@@ -0,0 +1,53 @@
+#lang racket/base
+(require rackunit)
+
+;;; BEGIN
+{!! $solution !!}
+;;; END
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbol-leaf x) (cadr x))
+
+(define (weight-leaf x) (caddr x))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+
+(define test-message '(A D A B B C A))
+
+(define encoded-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+
+(check-equal? (encode test-message sample-tree) encoded-message)
+(check-equal? (encode '(A B C D) sample-tree) '(0 1 0 1 1 1 1 1 0))
