d8620e94fb01229c2c5f55e0cf9fb62a0ee8cdb2
Anton Burenkov <buran665@yandex.ru>
2021-05-08 20:28:11 +0700

    add tests for 3.5

---
+++ resources/views/exercise/solution_stub/3_5.blade.php
@@ -0,0 +1,38 @@
+#lang sicp
+(#%require rackunit)
+
+
+;;; BEGIN
+{!! $solution !!}
+;;; END
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1) (+ trials-passed 1)))
+          (else
+           (iter (- trials-remaining 1) trials-passed))))
+  (iter trials 0))
+
+
+(define (random-in-range low high)
+    (let ((range (- high low)))
+        (+ low (random range))))
+
+
+(define (square x) (* x x))
+
+(define (square-predicate? x y)
+  (< (+ (square x)
+        (square y))
+  1.0))
+
+(define (test-pi trials)
+  (exact->inexact
+  (estimate-integral square-predicate? -1.0 1.0 -1.0 1.0 trials)))
+
+
+(check-equal? (floor (* 10 (test-pi 1000))) 31.0)
+(check-equal? (floor (* 10 (test-pi 10000))) 31.0)
