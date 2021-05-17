2261f3bb52c6e783b574640c7a15285db6ccf330
Anton Burenkov <buran665@yandex.ru>
2021-05-17 11:32:20 +0700

    add tests for 3.24

---
+++ resources/views/exercise/solution_stub/3_24.blade.php
@@ -0,0 +1,31 @@
+#lang racket/base
+(require rackunit)
+
+
+;;; BEGIN
+{!! $solution !!}
+;;; END
+
+(define table-equal (make-table equal?))
+(define get-equal (table-equal 'lookup-proc))
+(define put-equal (table-equal 'insert-proc!))
+
+(put-equal 1 2 42)
+(check-equal? (get-equal 1 2) 42)
+(check-false (get-equal 1 2.0000001))
+
+
+(define (same-key-epsilon? epsilon)
+  (lambda (x y) 
+      (if (< (abs (- x y)) epsilon) #t
+          #f)))
+
+(define table-epsilon (make-table (same-key-epsilon? 0.1)))
+(define get-epsilon (table-epsilon 'lookup-proc))
+(define put-epsilon (table-epsilon 'insert-proc!))
+
+(put-epsilon 1 2 42)
+(check-equal? (get-epsilon 1 2) 42)
+(check-equal? (get-epsilon 1 2.001) 42)
+(check-equal? (get-epsilon 1.01 2.01001) 42)
+(check-false (get-epsilon 1.2 2.1))
