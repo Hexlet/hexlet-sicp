72080d14dead7583f9dbc0b5ee1f9f0ec5b5e10e
Anton Burenkov <buran665@yandex.ru>
2021-05-22 12:32:35 +0700

    add missed dashes

--- resources/views/exercise/listing/3_31.blade.php
+++ resources/views/exercise/listing/3_31.blade.php
@@ -2,9 +2,9 @@
     {{ __('exercises/3_31.description.1') }}
     <code>accept-action-procedure!</code>{{ __('exercises/3_31.description.2') }}
     <code>make-wire</code>{{ __('exercises/3_31.description.3') }}
-    <code>halfadder</code>
+    <code>half-adder</code>
     {{ __('exercises/3_31.description.4') }}
-    <code>accept-actionprocedure!</code>
+    <code>accept-action-procedure!</code>
     {{ __('exercises/3_31.description.5') }}
 </p>
 <pre><code>(define (accept-action-procedure! proc)
