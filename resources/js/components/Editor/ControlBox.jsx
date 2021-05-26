22aaa502501ef7b8edadcb89d07135f6f079280a
alex-ismailov <alexx.ismailov@gmail.com>
2021-05-26 17:52:53 +0300

    Add(resources/js/components/Editor/) ControlBox.jsx

---
+++ resources/js/components/Editor/ControlBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const ControlBox = () => {
+
+  return (
+    <div className='d-flex py-2 w-100 justify-content-end card-footer py-3'>
+      <Button variant="primary">Запустить</Button>
+    </div>
+  );
+};
+
+export default ControlBox;
