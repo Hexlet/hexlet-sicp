c5a044aa55d38ebc2053e1bf670faccc22a7ee3e
alex-ismailov <alexx.ismailov@gmail.com>
2021-05-28 01:14:49 +0300

    Add(resources/js/components/codeEditor/features/editor/editorSlice.js) focusesCount to state, extraReducer on changeTab action

--- resources/js/components/codeEditor/features/editor/editorSlice.js
+++ resources/js/components/codeEditor/features/editor/editorSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { changeTab } from '../tabsBox/tabsBoxSlice.js';
 
 const initialContent = `#lang racket
 
@@ -12,12 +13,21 @@ const slice = createSlice({
   name: 'editor',
   initialState: {
     content: initialContent,
+    focusesCount: 1,
   },
   reducers: {
     changeContent(state, { payload: { content } }) {
       state.content = content;
     },
-  }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(changeTab, (state, { payload: { newActiveTab } }) => {
+        if (newActiveTab === 'editor') {
+          state.focusesCount += 1;
+        }
+      });
+  },
 });
 
 export const { changeContent } = slice.actions;
