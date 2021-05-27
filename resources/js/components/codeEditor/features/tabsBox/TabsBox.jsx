f02d2f58ec94c66e8f71f72c7cf8359163473b4d
alex-ismailov <alexx.ismailov@gmail.com>
2021-05-28 01:16:47 +0300

    Refactor(resources/js/components/codeEditor/features/tabsBox/TabsBox.jsx) tab links building

--- resources/js/components/codeEditor/features/tabsBox/TabsBox.jsx
+++ resources/js/components/codeEditor/features/tabsBox/TabsBox.jsx
@@ -1,30 +1,44 @@
 import React from 'react';
 import Editor from '../editor/Editor.jsx';
 import { Nav, Tab } from 'react-bootstrap';
+import tabNames from './tabNamesMap.js';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeTab } from './tabsBoxSlice.js';
 
 const TabsBox = () => {
+  const { currentTab } = useSelector((state) => state.tabsBox);
+  const dispatch = useDispatch();
+
+  const {
+    editor, output, testForExercise,
+  } = tabNames;
+
+  const changeActiveTab = (newActiveTab) => {
+    dispatch(changeTab({ newActiveTab }));
+  };
 
   return (
-    <Tab.Container defaultActiveKey="editor" className='card-body'>
+    <Tab.Container activeKey={currentTab} onSelect={changeActiveTab} className='card-body'>
       <Nav variant='tabs' className='justify-content-center'>
-        <Nav.Item>
-          <Nav.Link eventKey="editor">Редактор</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="tests">Тесты</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="output">Вывод</Nav.Link>
-        </Nav.Item>
+        {Object.values(tabNames).map((tabName) => (
+          <Nav.Item key={tabName}>
+            <Nav.Link
+              className="border-top-0 text-muted rounded-0"
+              eventKey={tabName}
+            >
+              {tabName}
+            </Nav.Link>
+          </Nav.Item>
+        ))}
       </Nav>
       <Tab.Content className='h-100 overflow-auto'>
-        <Tab.Pane eventKey="editor" className='h-100'>
+        <Tab.Pane eventKey={editor} bsPrefix="tab-pane h-100 w-100">
           <Editor />
         </Tab.Pane>
-        <Tab.Pane eventKey="tests">
+        <Tab.Pane eventKey={testForExercise} bsPrefix="tab-pane h-100 p-3 w-100">
           Результат тестов
         </Tab.Pane>
-        <Tab.Pane eventKey="output">
+        <Tab.Pane eventKey={output} bsPrefix="tab-pane h-100 p-3 w-100">
           Вывод
         </Tab.Pane>
       </Tab.Content>
@@ -33,3 +47,34 @@ const TabsBox = () => {
 };
 
 export default TabsBox;
+
+
+// const TabsBox = () => {
+
+//   return (
+//     <Tab.Container defaultActiveKey="editor" className='card-body'>
+//       <Nav variant='tabs' className='justify-content-center'>
+//         <Nav.Item>
+//           <Nav.Link eventKey="editor">Редактор</Nav.Link>
+//         </Nav.Item>
+//         <Nav.Item>
+//           <Nav.Link eventKey="tests">Тесты</Nav.Link>
+//         </Nav.Item>
+//         <Nav.Item>
+//           <Nav.Link eventKey="output">Вывод</Nav.Link>
+//         </Nav.Item>
+//       </Nav>
+//       <Tab.Content className='h-100 overflow-auto'>
+//         <Tab.Pane eventKey="editor" className='h-100'>
+//           <Editor />
+//         </Tab.Pane>
+//         <Tab.Pane eventKey="tests">
+//           Результат тестов
+//         </Tab.Pane>
+//         <Tab.Pane eventKey="output">
+//           Вывод
+//         </Tab.Pane>
+//       </Tab.Content>
+//     </Tab.Container>
+//   );
+// };
