6078565f2fbf059903cba7b516097440a7230738
Anton Burenkov <buran665@yandex.ru>
2021-06-16 21:49:30 +0700

    add tests for 3.69

---
+++ resources/views/exercise/solution_stub/3_69.blade.php
@@ -0,0 +1,64 @@
+#lang sicp
+(#%require rackunit)
+
+
+;;; BEGIN
+{!! $solution !!}
+;;; END
+
+(define (stream-car stream) (car stream))
+
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-map proc . list-of-stream)
+    (if (null? (car list-of-stream))
+        '()
+        (cons-stream
+            (apply proc 
+                   (map (lambda (s)
+                            (stream-car s))
+                        list-of-stream))
+            (apply stream-map 
+                   (cons proc (map (lambda (s)
+                                       (stream-cdr s))
+                                   list-of-stream))))))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define (pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+    (stream-map (lambda (x) (list (stream-car s) x))
+                (stream-cdr t))
+    (pairs (stream-cdr s) (stream-cdr t)))))
+
+(define ones (cons-stream 1 ones))
+
+(define integers (cons-stream 1 (add-streams ones integers)))
+
+
+(define s (triples integers integers integers))
+
+(check-equal? (stream-car s) '(1 1 1))
+(check-equal? (stream-car (stream-cdr s)) '(1 1 2))
+(check-equal? (stream-car (stream-cdr (stream-cdr s))) '(2 2 2))
+(check-equal? (stream-car (stream-cdr (stream-cdr (stream-cdr s)))) '(1 1 3))
+
+(check-equal? (stream-car pythagorian-triples) '(3 4 5))
+(check-equal? (stream-car (stream-cdr pythagorian-triples)) '(6 8 10))
