0f1dad08c97a29f3706ba6a1ddb6cb64cefda879
feycot <feycot@gmail.com>
2021-06-26 23:09:21 +0500

    fix nav

--- resources/views/layouts/bootstrap5/app.blade.php
+++ resources/views/layouts/bootstrap5/app.blade.php
@@ -28,33 +28,25 @@
     @stack('styles')
     <link href="{{ mix('css/_custom.css') }}" rel="stylesheet">
     {{--<link href="{{ mix('css/app.css') }}" rel="stylesheet">--}}
-
-</head>
-
-<body class="min-vh-100 d-flex flex-column">
     @includeWhen(app()->environment('production'), 'layouts.deps._gtm_body')
-    @include('layouts.bootstrap5._nav')
-    <div class="flex-grow-1">
-        <main class="my-4">
-            <div class="container mb-3">
-                {{-- @include('flash::message')--}}
-                @include('layouts.bootstrap5._flash')
-                @yield('content')
-            </div>
-        </main>
-    </div>
-    @include('layouts.bootstrap5._footer')
-    @livewireScripts
-    <script src="{{ mix('js/hljs.js')}}"></script>
     @includeWhen(app()->environment('production'), 'layouts.deps._metrika')
-
-    <!-- Scripts -->
-    {{--    <script src="{{ mix('js/app.js') }}" defer></script>--}}
+</head>
+<body>
+    @include('layouts.bootstrap5._nav')
+    <div class="container-xl mt-n2 mb-3">
+        @include('layouts.bootstrap5._flash')
+        {{-- @include('flash::message')--}}
+        @yield('content')
+        @include('layouts.bootstrap5._footer')
+    </div>
+    @livewireScripts
+    {{--<script src="{{ mix('js/app.js') }}" defer></script>--}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
             crossorigin="anonymous">
     </script>
     <script src="https://cdn.jsdelivr.net/npm/rails-ujs@5.2.6/lib/assets/compiled/rails-ujs.min.js"></script>
+    <script src="{{ mix('js/hljs.js')}}"></script>
     <script src="{{ mix('js/font-awesome.js') }}" defer></script>
     @stack('scripts')
 </body>
