f7d2c8698bf9e2bd8e6fda406c7d64ad900c0a49
Anton Burenkov <buran665@yandex.ru>
2021-08-02 19:29:10 +0700

    color the keywords in 1.3, 1.7

--- resources/lang/en/exercises/1_3.php
+++ resources/lang/en/exercises/1_3.php
@@ -2,5 +2,10 @@
 
 return [
     'title' => 'Sum of the squares',
-    'description' => 'Define a procedure solution that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.',
+    'description' => [
+        '1' =>
+        'Define a procedure ',
+        '2' =>
+        ' that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.',
+    ],
 ];
--- resources/views/exercise/listing/1_3.blade.php
+++ resources/views/exercise/listing/1_3.blade.php
@@ -1,1 +1,3 @@
-<p>{{ __('exercises/1_3.description') }}</p>
+<p>{{ __('exercises/1_3.description.1') }}</p>
+<code>solution</code>
+<p>{{ __('exercises/1_3.description.2') }}</p>
