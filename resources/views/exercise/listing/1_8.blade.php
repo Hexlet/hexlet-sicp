5639654e93b75a7970feef6c39f8a8be655d91a3
Anton Burenkov <buran665@yandex.ru>
2021-08-03 12:47:14 +0700

    color the keywords in 1.8

--- resources/views/exercise/listing/1_8.blade.php
+++ resources/views/exercise/listing/1_8.blade.php
@@ -2,4 +2,7 @@
 <pre><code>x/(y * y) + 2y
 --------------
       3</code></pre>
-<p>{{ __('exercises/1_8.description.2') }}</p>
+<p>{{ __('exercises/1_8.description.2') }}
+<code>cube-root</code>
+{{ __('exercises/1_8.description.3') }}
+</p>
