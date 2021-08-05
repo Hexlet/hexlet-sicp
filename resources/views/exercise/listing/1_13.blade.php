43f24f0198f2fe2da43bb07016e7593040c66268
Anton Burenkov <buran665@yandex.ru>
2021-08-05 14:45:11 +0700

    color 1.13

--- resources/views/exercise/listing/1_13.blade.php
+++ resources/views/exercise/listing/1_13.blade.php
@@ -1,1 +1,12 @@
-<p>{{ __('exercises/1_13.description') }}</p>
+<p>{{ __('exercises/1_13.description.1') }}
+<code>Fib(n)</code>
+{{ __('exercises/1_13.description.2') }}
+<code>φⁿ/√5</code>
+{{ __('exercises/1_13.description.3') }}
+<code>φ = (1 + √5)/2</code>
+{{ __('exercises/1_13.description.4') }}
+<code>ψ = (1 - √5)/2</code>
+{{ __('exercises/1_13.description.5') }}
+<code>Fib(n) = (φⁿ - ψⁿ)/√5</code>
+{{ __('exercises/1_13.description.6') }}
+</p>
