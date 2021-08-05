ce4394ef3c22055e70f60d17a433c73517633ea1
Anton Burenkov <buran665@yandex.ru>
2021-08-05 14:35:10 +0700

    color 1.11

--- resources/views/exercise/listing/1_11.blade.php
+++ resources/views/exercise/listing/1_11.blade.php
@@ -1,1 +1,20 @@
-<p>{{ __('exercises/1_11.description') }}</p>
+<p>{{ __('exercises/1_11.description.1') }}
+<code>f</code>
+{{ __('exercises/1_11.description.2') }}
+<code>f(n) = n</code>
+{{ __('exercises/1_11.description.3') }}
+<code>n < 3</code>
+{{ __('exercises/1_11.description.4') }}
+<code>f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3)</code>
+{{ __('exercises/1_11.description.5') }}
+<code>n ≥ 3</code>
+{{ __('exercises/1_11.description.6') }}
+<code>f</code>
+{{ __('exercises/1_11.description.7') }}
+<code>f</code>
+{{ __('exercises/1_11.description.8') }}
+<code>f-iter</code>
+{{ __('exercises/1_11.description.9') }}
+<code>f</code>
+{{ __('exercises/1_11.description.10') }}
+</p>
