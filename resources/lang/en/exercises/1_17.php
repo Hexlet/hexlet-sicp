d434a442c4cd8daf401c67e789c0731f2e2c3715
Anton Burenkov <buran665@yandex.ru>
2021-08-07 13:03:50 +0700

    color 1.17

--- resources/lang/en/exercises/1_17.php
+++ resources/lang/en/exercises/1_17.php
@@ -6,10 +6,20 @@ return [
         '1' =>
         "The exponentiation algorithms in this section are based on performing exponentiation by means of repeated multiplication. " .
         "In a similar way, one can perform integer multiplication by means of repeated addition. " .
-        "The following multiplication procedure (in which it is assumed that our language can only add, not multiply) is analogous to the expt procedure:",
+        "The following multiplication procedure (in which it is assumed that our language can only add, not multiply) is analogous to the ",
         '2' =>
-        "This algorithm takes a number of steps that is linear in b. " .
-        "Now suppose we include, together with addition, operations double, which doubles an integer, and halve, which divides an (even) integer by 2. " .
-        "Using these, design a multiplication procedure fast-mul analogous to fast-expt that uses a logarithmic number of steps.",
+        " procedure:",
+        '3' =>
+        "This algorithm takes a number of steps that is linear in ",
+        '4' =>
+        ". Now suppose we include, together with addition, operations ",
+        '5' =>
+        ", which doubles an integer, and ",
+        '6' =>
+        ", which divides an (even) integer by 2. Using these, design a multiplication procedure ",
+        '7' =>
+        " analogous to ",
+        '8' =>
+        " that uses a logarithmic number of steps.",
     ],
 ];
