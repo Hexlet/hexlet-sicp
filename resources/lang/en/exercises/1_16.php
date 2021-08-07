3a108931d7c6035deac4644b82cf68bb8b63e8a3
Anton Burenkov <buran665@yandex.ru>
2021-08-07 12:47:13 +0700

    color 1.16

--- resources/lang/en/exercises/1_16.php
+++ resources/lang/en/exercises/1_16.php
@@ -2,9 +2,27 @@
 
 return [
     'title' => 'Fast exponentiation',
-    'description' =>
-        'Design a procedure solution that evolves an iterative exponentiation process that uses successive squaring and uses a logarithmic number of steps, as does fast-expt. ' .
-        '(Hint: Using the observation that (bⁿ୵²)² = (b²)ⁿ୵², keep, along with the exponent n and the base b, an additional state variable a, and define the state transformation in such a way that the product abⁿ is unchanged from state to state. ' .
-        'At the beginning of the process a is taken to be 1, and the answer is given by the value of a at the end of the process. ' .
+    'description' => [
+        '1' =>
+        'Design a procedure ',
+        '2' =>
+        ' that evolves an iterative exponentiation process that uses successive squaring and uses a logarithmic number of steps, as does ',
+        '3' =>
+        '. (Hint: Using the observation that ',
+        '4' =>
+        ', keep, along with the exponent ',
+        '5' =>
+        ' and the base ',
+        '6' =>
+        ', an additional state variable ',
+        '7' =>
+        ', and define the state transformation in such a way that the product ',
+        '8' =>
+        ' is unchanged from state to state. At the beginning of the process ',
+        '9' =>
+        ' is taken to be 1, and the answer is given by the value of ',
+        '10' =>
+        ' at the end of the process. ' .
         'In general, the technique of defining an invariant quantity that remains unchanged from state to state is a powerful way to think about the design of iterative algorithms.)',
+    ],
 ];
