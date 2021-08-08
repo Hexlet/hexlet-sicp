acd7e1adfa79e9a9f4b336fa4075c7ff385c182a
Anton Burenkov <buran665@yandex.ru>
2021-08-08 14:10:44 +0700

    color 1.18

--- resources/views/exercise/listing/1_18.blade.php
+++ resources/views/exercise/listing/1_18.blade.php
@@ -1,3 +1,5 @@
 <p>{{ __('exercises/1_18.description.1') }}<a href="{{ route('exercises.show', getExercise('1.16')) }}">1.16</a>
 {{ __('exercises/1_18.description.2') }}<a href="{{ route('exercises.show', getExercise('1.17')) }}">1.17</a>
-{{ __('exercises/1_18.description.3') }}</p>
+{{ __('exercises/1_18.description.3') }}
+<code>mul-iter</code>
+{{ __('exercises/1_18.description.4') }}</p>
