b99badd68755b3692cdd2592cb4a818e03d84c01
Anton Burenkov <buran665@yandex.ru>
2021-08-09 20:56:23 +0700

    color 1.20

--- resources/lang/en/exercises/1_20.php
+++ resources/lang/en/exercises/1_20.php
@@ -5,12 +5,19 @@ return [
     'description' => [
         '1' =>
         'The process that a procedure generates is of course dependent on the rules used by the interpreter. ' .
-        'As an example, consider the iterative gcd procedure given above. ' .
-        'Suppose we were to interpret this procedure using normal-order evaluation, as discussed in section 1.1.5. ' .
-        '(The normal-order-evaluation rule for if is described in exercise ',
+        'As an example, consider the iterative ',
         '2' =>
-        '.) Using the substitution method (for normal order), illustrate the process generated in evaluating (gcd 206 40) and indicate the remainder operations that are actually performed. ' .
-        'How many remainder operations are actually performed in the normal-order evaluation of (gcd 206 40)? ' .
-        'In the applicative-order evaluation?',
+        ' procedure given above. Suppose we were to interpret this procedure using normal-order evaluation, as discussed in section 1.1.5. ' .
+        '(The normal-order-evaluation rule for ',
+        '3' =>
+        ' is described in exercise ',
+        '4' =>
+        '.) Using the substitution method (for normal order), illustrate the process generated in evaluating ',
+        '5' =>
+        ' and indicate the remainder operations that are actually performed. How many ',
+        '6' =>
+        ' operations are actually performed in the normal-order evaluation of ',
+        '7' =>
+        '? In the applicative-order evaluation?',
     ],
 ];
