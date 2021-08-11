241dbe5bd9d8fba10a19ff8c8fcb5c591b9609f5
Anton Burenkov <buran665@yandex.ru>
2021-08-11 21:14:48 +0700

    color 1.25

--- resources/lang/en/exercises/1_25.php
+++ resources/lang/en/exercises/1_25.php
@@ -4,9 +4,10 @@ return [
     'title' => "Extra work",
     'description' => [
         '1' =>
-        "Alyssa P. Hacker complains that we went to a lot of extra work in writing expmod. " .
-        "After all, she says, since we already know how to compute exponentials, we could have simply written",
+        "Alyssa P. Hacker complains that we went to a lot of extra work in writing ",
         '2' =>
+        ". After all, she says, since we already know how to compute exponentials, we could have simply written",
+        '3' =>
         "Is she correct? Would this procedure serve as well for our fast prime tester? Explain.",
     ],
 ];
--- resources/views/exercise/listing/1_25.blade.php
+++ resources/views/exercise/listing/1_25.blade.php
@@ -1,5 +1,8 @@
-<p>{{ __('exercises/1_25.description.1') }}</p>
+<p>{{ __('exercises/1_25.description.1') }}
+<code>expmod</code>
+{{ __('exercises/1_25.description.2') }}
+</p>
 <pre><code>(define (expmod base exp m)
   (remainder (fast-expt base exp) m))
 </code></pre>
-<p>{{ __('exercises/1_25.description.2') }}</p>
+<p>{{ __('exercises/1_25.description.3') }}</p>
