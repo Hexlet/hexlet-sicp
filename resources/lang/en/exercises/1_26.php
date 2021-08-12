e22e6523e9289bea94a4c4a34817ee5316ee1e61
Anton Burenkov <buran665@yandex.ru>
2021-08-12 21:11:44 +0700

    color 1.26

--- resources/lang/en/exercises/1_26.php
+++ resources/lang/en/exercises/1_26.php
@@ -6,10 +6,17 @@ return [
         '1' =>
         "Louis Reasoner is having great difficulty doing exercise ",
         '2' =>
-        ". His fast-prime? test seems to run more slowly than his prime? test. " .
-        "Louis calls his friend Eva Lu Ator over to help. When they examine Louis's code, " .
-        "they find that he has rewritten the expmod procedure to use an explicit multiplication, rather than calling square:",
+        ". His ",
         '3' =>
+        " test seems to run more slowly than his ",
+        '4' =>
+        " test. Louis calls his friend Eva Lu Ator over to help. When they examine Louis's code, " .
+        "they find that he has rewritten the ",
+        '5' =>
+        " procedure to use an explicit multiplication, rather than calling ",
+        '6' =>
+        ":",
+        '7' =>
         "``I don't see what difference that could make,'' says Louis. ``I do.'' says Eva. ``By writing the procedure like that, " .
         "you have transformed the Θ(log n) process into a Θ(n) process.'' Explain. ",
     ],
