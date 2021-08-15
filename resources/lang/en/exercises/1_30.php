f192b9f064f231bb7694cefee3e1b1dce5932708
Anton Burenkov <buran665@yandex.ru>
2021-08-15 19:53:50 +0700

    color 1.30

--- resources/lang/en/exercises/1_30.php
+++ resources/lang/en/exercises/1_30.php
@@ -4,7 +4,9 @@ return [
     'title' => 'Iteration',
     'description' => [
         '1' =>
-        "The sum procedure above generates a linear recursion. The procedure can be rewritten so that the sum is performed iteratively. " .
+        "The ",
+        '2' =>
+        " procedure above generates a linear recursion. The procedure can be rewritten so that the sum is performed iteratively. " .
         "Show how to do this by filling in the missing expressions in the following definition:",
     ],
 ];
