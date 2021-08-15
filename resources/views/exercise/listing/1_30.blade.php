4d843d26fc56a974d9259bbdf87f373a40f694dd
Anton Burenkov <buran665@yandex.ru>
2021-08-15 19:53:50 +0700

    color 1.30

--- resources/views/exercise/listing/1_30.blade.php
+++ resources/views/exercise/listing/1_30.blade.php
@@ -1,4 +1,7 @@
-<p>{{ __('exercises/1_30.description.1') }}</p>
+<p>{{ __('exercises/1_30.description.1') }}
+<code>sum</code>
+{{ __('exercises/1_30.description.2') }}
+</p>
 <pre><code>(define (sum term a next b)
   (define (iter a result)
     (if &lt;??&gt;
