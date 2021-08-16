2c8718eec2a2654cd8f775bfa469d83de7a08256
Anton Burenkov <buran665@yandex.ru>
2021-08-16 22:52:39 +0700

    color 1.32

--- resources/views/exercise/listing/1_32.blade.php
+++ resources/views/exercise/listing/1_32.blade.php
@@ -1,6 +1,33 @@
-<p>{{ __('exercises/1_32.description.1') }}<a href="{{ route('exercises.show', getExercise('1.31') )}}">1.31</a>
-{{ __('exercises/1_32.description.2') }}</p>
+<p>{{ __('exercises/1_32.description.1') }}
+<code>sum</code>
+{{ __('exercises/1_32.description.2') }}
+<code>product</code>
+{{ __('exercises/1_32.description.3') }}
+<a href="{{ route('exercises.show', getExercise('1.31') )}}">1.31</a>
+{{ __('exercises/1_32.description.4') }}</p>
 <pre><code>(accumulate combiner null-value term a next b)
 </code></pre>
-<p>{{ __('exercises/1_32.description.3') }}</p>
-<p>{{ __('exercises/1_32.description.4') }}</p>
+<p>
+<code>Accumulate</code>
+{{ __('exercises/1_32.description.5') }}
+<code>sum</code>
+{{ __('exercises/1_32.description.6') }}
+<code>product</code>
+{{ __('exercises/1_32.description.7') }}
+<code>combiner</code>
+{{ __('exercises/1_32.description.8') }}
+<code>null-value</code>
+{{ __('exercises/1_32.description.9') }}
+<code>accumulate</code>
+{{ __('exercises/1_32.description.10') }}
+<code>sum</code>
+{{ __('exercises/1_32.description.11') }}
+<code>product</code>
+{{ __('exercises/1_32.description.12') }}
+<code>accumulate</code>
+{{ __('exercises/1_32.description.13') }}
+</p>
+<p>{{ __('exercises/1_32.description.14') }}
+<code>accumulate</code>
+{{ __('exercises/1_32.description.15') }}
+</p>
