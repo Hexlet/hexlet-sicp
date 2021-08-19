522539a4f68edc4666a35a88d22688d8c3e88ca3
Anton Burenkov <buran665@yandex.ru>
2021-08-19 21:42:32 +0700

    color 1.39

--- resources/views/exercise/listing/1_39.blade.php
+++ resources/views/exercise/listing/1_39.blade.php
@@ -7,5 +7,12 @@
                  -------
                  5 - ...
 </code></pre>
-<p>{{ __('exercises/1_39.description.2') }}<a href="{{ route('exercises.show', getExercise('1.37')) }}">1.37</a>
-{{ __('exercises/1_39.description.3') }}</p>
+<p>{{ __('exercises/1_39.description.2') }}
+<code>x</code>
+{{ __('exercises/1_39.description.3') }}
+<code>(tan-cf x k)</code>
+{{ __('exercises/1_39.description.4') }}
+<code>K</code>
+{{ __('exercises/1_39.description.5') }}
+<a href="{{ route('exercises.show', getExercise('1.37')) }}">1.37</a>
+{{ __('exercises/1_39.description.6') }}</p>
