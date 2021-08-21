18ef3390b24564b6df96e089dacfc1746affdc93
Anton Burenkov <buran665@yandex.ru>
2021-08-21 16:59:53 +0700

    color 1.43

--- resources/views/exercise/listing/1_43.blade.php
+++ resources/views/exercise/listing/1_43.blade.php
@@ -1,6 +1,48 @@
-<p>{{ __('exercises/1_43.description.1') }}</p>
+<p>{{ __('exercises/1_43.description.1') }}
+<code>f</code>
+{{ __('exercises/1_43.description.2') }}
+<code>n</code>
+{{ __('exercises/1_43.description.3') }}
+<code>n</code>
+{{ __('exercises/1_43.description.4') }}
+<code>f</code>
+{{ __('exercises/1_43.description.5') }}
+<code>x</code>
+{{ __('exercises/1_43.description.6') }}
+<code>f(f(...(f(x))...))</code>
+{{ __('exercises/1_43.description.7') }}
+<code>f</code>
+{{ __('exercises/1_43.description.8') }}
+<code>x → x + 1</code>
+{{ __('exercises/1_43.description.9') }}
+<code>n</code>
+{{ __('exercises/1_43.description.10') }}
+<code>f</code>
+{{ __('exercises/1_43.description.11') }}
+<code>x → x + n</code>
+{{ __('exercises/1_43.description.12') }}
+<code>f</code>
+{{ __('exercises/1_43.description.13') }}
+<code>n</code>
+{{ __('exercises/1_43.description.14') }}
+<code>f</code>
+{{ __('exercises/1_43.description.15') }}
+<code>2ⁿ</code>
+{{ __('exercises/1_43.description.16') }}
+<code>f</code>
+{{ __('exercises/1_43.description.17') }}
+<code>n</code>
+{{ __('exercises/1_43.description.18') }}
+<code>n</code>
+{{ __('exercises/1_43.description.19') }}
+<code>f</code>
+{{ __('exercises/1_43.description.20') }}
+</p>
 <pre><code>((repeated square 2) 5)
 <i>625</i>
 </code></pre>
-<p>{{ __('exercises/1_43.description.2') }}<a href="{{ route('exercises.show', getExercise('1.42')) }}">1.42</a>
-{{ __('exercises/1_43.description.3') }}</p>
+<p>{{ __('exercises/1_43.description.21') }}
+<code>compose</code>
+{{ __('exercises/1_43.description.22') }}
+<a href="{{ route('exercises.show', getExercise('1.42')) }}">1.42</a>
+{{ __('exercises/1_43.description.23') }}</p>
