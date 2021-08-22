83e041b8a3415c11db64c4c3713d32f6f3dc9653
Anton Burenkov <buran665@yandex.ru>
2021-08-22 15:36:46 +0700

    color 1.45

--- resources/lang/en/exercises/1_45.php
+++ resources/lang/en/exercises/1_45.php
@@ -4,15 +4,28 @@ return [
     'title' => 'Computing nth roots',
     'description' => [
         '1' =>
-        " We saw in section 1.3.3 that attempting to compute square roots by naively finding a fixed point of y → x/y does not converge, " .
-        "and that this can be fixed by average damping. The same method works for finding cube roots " .
-        "as fixed points of the average-damped y → x/y². Unfortunately, the process does not work for fourth roots " .
-        "-- a single average damp is not enough to make a fixed-point search for y → x/y³ converge. " .
-        "On the other hand, if we average damp twice (i.e., use the average damp of the average damp of y → x/y³) the fixed-point search does converge. " .
-        "Do some experiments to determine how many average damps are required " .
-        "to compute nth roots as a fixed-point search based upon repeated average damping of y → x/yⁿ⁻¹. " .
-        "Use this to implement a simple procedure for computing nth roots using fixed-point, average-damp, and the repeated procedure of exercise ",
+        "We saw in section 1.3.3 that attempting to compute square roots by naively finding a fixed point of ",
         '2' =>
+        " does not converge, and that this can be fixed by average damping. The same method works for finding cube roots as fixed points of the average-damped ",
+        '3' =>
+        ". Unfortunately, the process does not work for fourth roots -- a single average damp is not enough to make a fixed-point search for ",
+        '4' =>
+        " converge. On the other hand, if we average damp twice (i.e., use the average damp of the average damp of ",
+        '5' =>
+        ") the fixed-point search does converge. Do some experiments to determine how many average damps are required to compute ",
+        '6' =>
+        "th roots as a fixed-point search based upon repeated average damping of ",
+        '7' =>
+        ". Use this to implement a simple procedure for computing ",
+        '8' =>
+        "th roots using ",
+        '9' =>
+        ", ",
+        '10' =>
+        ", and the ",
+        '11' =>
+        " procedure of exercise ",
+        '12' =>
         ". Assume that any arithmetic operations you need are available as primitives.",
     ],
 ];
--- resources/views/exercise/listing/1_45.blade.php
+++ resources/views/exercise/listing/1_45.blade.php
@@ -1,2 +1,23 @@
-<p>{{ __('exercises/1_45.description.1') }}<a href="{{ route('exercises.show', getExercise('1.43')) }}">1.43</a>
-{{ __('exercises/1_45.description.2') }}</p>
+<p>{{ __('exercises/1_45.description.1') }}
+<code>y → x/y</code>
+{{ __('exercises/1_45.description.2') }}
+<code>y → x/y²</code>
+{{ __('exercises/1_45.description.3') }}
+<code>y → x/y³</code>
+{{ __('exercises/1_45.description.4') }}
+<code>y → x/y³</code>
+{{ __('exercises/1_45.description.5') }}
+<code>n</code>
+{{ __('exercises/1_45.description.6') }}
+<code>y → x/yⁿ⁻¹</code>
+{{ __('exercises/1_45.description.7') }}
+<code>n</code>
+{{ __('exercises/1_45.description.8') }}
+<code>fixed-point</code>
+{{ __('exercises/1_45.description.9') }}
+<code>average-damp</code>
+{{ __('exercises/1_45.description.10') }}
+<code>repeated</code>
+{{ __('exercises/1_45.description.11') }}
+<a href="{{ route('exercises.show', getExercise('1.43')) }}">1.43</a>
+{{ __('exercises/1_45.description.12') }}</p>
