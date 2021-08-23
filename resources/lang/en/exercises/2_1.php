e7890776b772d3d1df40e839162ca79143a6fa5a
Anton Burenkov <buran665@yandex.ru>
2021-08-23 17:17:45 +0700

    color 2.1

--- resources/lang/en/exercises/2_1.php
+++ resources/lang/en/exercises/2_1.php
@@ -2,7 +2,12 @@
 
 return [
     'title' => 'Modify the make-rat',
-    'description' =>
-        'Define a better version of make-rat that handles both positive and negative arguments. ' .
-        'Make-rat should normalize the sign so that if the rational number is positive, both the numerator and denominator are positive, and if the rational number is negative, only the numerator is negative.',
+    'description' => [
+        '1' =>
+        "Define a better version of ",
+        '2' =>
+        " that handles both positive and negative arguments. ",
+        '3' =>
+        " should normalize the sign so that if the rational number is positive, both the numerator and denominator are positive, and if the rational number is negative, only the numerator is negative.",
+    ],
 ];
