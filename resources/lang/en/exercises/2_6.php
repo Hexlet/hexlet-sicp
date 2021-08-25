c66246bc59abbfea18b10e14dde3844bc8e369a3
Anton Burenkov <buran665@yandex.ru>
2021-08-25 21:09:40 +0700

    color 2.6

--- resources/lang/en/exercises/2_6.php
+++ resources/lang/en/exercises/2_6.php
@@ -4,12 +4,28 @@ return [
     'title' => 'Church numerals',
     'description' => [
         '1' =>
-        "In case representing pairs as procedures wasn't mind-boggling enough, consider that, in a language that can manipulate procedures, we can get by without numbers (at least insofar as nonnegative integers are concerned) by implementing 0 and the operation of adding 1 as",
+        "In case representing pairs as procedures wasn't mind-boggling enough, consider that, in a language that can manipulate procedures, we can get by without numbers (at least insofar as nonnegative integers are concerned) by implementing ",
         '2' =>
-        "This representation is known as Church numerals, after its inventor, Alonzo Church, the logician who invented the λ calculus.",
+        " and the operation of adding ",
         '3' =>
-        "Define one and two directly (not in terms of zero and add-1). " .
-        "(Hint: Use substitution to evaluate (add-1 zero)). " .
-        "Give a direct definition of the addition procedure + (add) (not in terms of repeated application of add-1).",
+        " as",
+        '4' =>
+        "This representation is known as Church numerals, after its inventor, Alonzo Church, the logician who invented the λ calculus.",
+        '5' =>
+        "Define ",
+        '6' =>
+        " and ",
+        '7' =>
+        " directly (not in terms of ",
+        '8' =>
+        " and ",
+        '9' =>
+        "). (Hint: Use substitution to evaluate ",
+        '10' =>
+        "). Give a direct definition of the addition procedure + (",
+        '11' =>
+        ") (not in terms of repeated application of ",
+        '12' =>
+        ").",
     ],
 ];
