a4cb0c85f0482cf9f44c524fa11f3e99148341da
Anton Burenkov <buran665@yandex.ru>
2021-09-06 20:54:28 +0700

    color 2.24

--- resources/lang/en/exercises/2_24.php
+++ resources/lang/en/exercises/2_24.php
@@ -4,9 +4,10 @@ return [
     'title' => 'Box-and-pointer structure',
     'description' => [
         '1' =>
-        "Suppose we evaluate the expression (list 1 (list 2 (list 3 4))). " .
-        "Give the result printed by the interpreter, the corresponding box-and-pointer structure, and the interpretation of this as a tree (as in figure 2.6).",
+        "Suppose we evaluate the expression ",
         '2' =>
+        ". Give the result printed by the interpreter, the corresponding box-and-pointer structure, and the interpretation of this as a tree (as in figure 2.6).",
+        '3' =>
         "Figure 2.6:  The list structure in figure 2.5 viewed as a tree.",
     ],
 ];
--- resources/views/exercise/listing/2_24.blade.php
+++ resources/views/exercise/listing/2_24.blade.php
@@ -1,3 +1,6 @@
-<p>{{ __('exercises/2_24.description.1') }}</p>
+<p>{{ __('exercises/2_24.description.1') }}
+<code>(list 1 (list 2 (list 3 4)))</code>
+{{ __('exercises/2_24.description.2') }}
+</p>
 <img class="img-fluid" src="{{ mix('img/exercises/2_24.gif') }}" alt="2.24">
-<p>{{ __('exercises/2_24.description.2') }}</p>
+<p>{{ __('exercises/2_24.description.3') }}</p>
