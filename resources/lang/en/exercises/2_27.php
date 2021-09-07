201b07137957c4c33dfd1ce27bb599d5b8c513c3
Anton Burenkov <buran665@yandex.ru>
2021-09-07 14:36:57 +0700

    color 2.27

--- resources/lang/en/exercises/2_27.php
+++ resources/lang/en/exercises/2_27.php
@@ -4,9 +4,13 @@ return [
     'title' => 'Deep reverse',
     'description' => [
         '1' =>
-        "Modify your reverse procedure of exercise ",
+        "Modify your ",
         '2' =>
-        " to produce a deep-reverse procedure that takes a list as argument and returns as its value the list with its elements reversed and with all sublists deep-reversed as well. " .
+        " procedure of exercise ",
+        '3' =>
+        " to produce a ",
+        '4' =>
+        " procedure that takes a list as argument and returns as its value the list with its elements reversed and with all sublists deep-reversed as well. " .
         "For example,",
     ],
 ];
