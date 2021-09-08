f2aefba400727fc9ec4b57f66220694e2e1a62cc
Anton Burenkov <buran665@yandex.ru>
2021-09-08 21:31:28 +0700

    color 2.28

--- resources/lang/en/exercises/2_28.php
+++ resources/lang/en/exercises/2_28.php
@@ -2,7 +2,10 @@
 
 return [
     'title' => 'List of tree leaves',
-    'description' =>
-        "Write a procedure fringe that takes as argument a tree (represented as a list) and returns a list whose elements are all the leaves of the tree arranged in left-to-right order. " .
-        "For example,",
+    'description' => [
+        '1' =>
+        "Write a procedure ",
+        '2' =>
+        " that takes as argument a tree (represented as a list) and returns a list whose elements are all the leaves of the tree arranged in left-to-right order. For example,",
+    ],
 ];
