86884727b5ad428ecb1c3a1fb7bb7fe4e06695c7
Anton Burenkov <buran665@yandex.ru>
2021-09-08 21:42:43 +0700

    color 2.29

--- resources/lang/en/exercises/2_29.php
+++ resources/lang/en/exercises/2_29.php
@@ -6,19 +6,35 @@ return [
         '1' =>
         "A binary mobile consists of two branches, a left branch and a right branch. " .
         "Each branch is a rod of a certain length, from which hangs either a weight or another binary mobile. " .
-        "We can represent a binary mobile using compound data by constructing it from two branches (for example, using list):",
+        "We can represent a binary mobile using compound data by constructing it from two branches (for example, using ",
         '2' =>
-        "A branch is constructed from a length (which must be a number) together with a structure, which may be either a number (representing a simple weight) or another mobile:",
+        "):",
         '3' =>
-        "a.  Write the corresponding selectors left-branch and right-branch, which return the branches of a mobile, and branch-length and branch-structure, which return the components of a branch.",
+        "A branch is constructed from a ",
         '4' =>
-        "b.  Using your selectors, define a procedure total-weight that returns the total weight of a mobile.",
+        " (which must be a number) together with a ",
         '5' =>
+        ", which may be either a number (representing a simple weight) or another mobile:",
+        '6' =>
+        "a.  Write the corresponding selectors ",
+        '7' =>
+        " and ",
+        '8' =>
+        ", which return the branches of a mobile, and ",
+        '9' =>
+        " and ",
+        '10' =>
+        ", which return the components of a branch.",
+        '11' =>
+        "b.  Using your selectors, define a procedure ",
+        '12' =>
+        " that returns the total weight of a mobile.",
+        '13' =>
         "c.  A mobile is said to be balanced if the torque applied by its top-left branch is equal to that applied by its top-right branch (that is, if the length of the left rod multiplied by the weight hanging from that rod is equal to the corresponding product for the right side) and if each of the submobiles hanging off its branches is balanced. " .
         "Design a predicate that tests whether a binary mobile is balanced.",
-        '6' =>
+        '14' =>
         "d.  Suppose we change the representation of mobiles so that the constructors are",
-        '7' =>
+        '15' =>
         "How much do you need to change your programs to convert to the new representation?",
     ],
 ];
--- resources/views/exercise/listing/2_29.blade.php
+++ resources/views/exercise/listing/2_29.blade.php
@@ -1,18 +1,38 @@
-<p>{{ __('exercises/2_29.description.1') }}</p>
+<p>{{ __('exercises/2_29.description.1') }}
+<code>list</code>
+{{ __('exercises/2_29.description.2') }}
+</p>
 <pre><code>(define (make-mobile left right)
   (list left right))
 </code></pre>
-<p>{{ __('exercises/2_29.description.2') }}</p>
+<p>{{ __('exercises/2_29.description.3') }}
+<code>length</code>
+{{ __('exercises/2_29.description.4') }}
+<code>structure</code>
+{{ __('exercises/2_29.description.5') }}
+</p>
 <pre><code>(define (make-branch length structure)
   (list length structure))
 </code></pre>
-<p>{{ __('exercises/2_29.description.3') }}</p>
-<p>{{ __('exercises/2_29.description.4') }}</p>
-<p>{{ __('exercises/2_29.description.5') }}</p>
-<p>{{ __('exercises/2_29.description.6') }}</p>
+<p>{{ __('exercises/2_29.description.6') }}
+<code>left-branch</code>
+{{ __('exercises/2_29.description.7') }}
+<code>right-branch</code>
+{{ __('exercises/2_29.description.8') }}
+<code>branch-length</code>
+{{ __('exercises/2_29.description.9') }}
+<code>branch-structure</code>
+{{ __('exercises/2_29.description.10') }}
+</p>
+<p>{{ __('exercises/2_29.description.11') }}
+<code>total-weight</code>
+{{ __('exercises/2_29.description.12') }}
+</p>
+<p>{{ __('exercises/2_29.description.13') }}</p>
+<p>{{ __('exercises/2_29.description.14') }}</p>
 <pre><code>(define (make-mobile left right)
   (cons left right))
 (define (make-branch length structure)
   (cons length structure))
 </code></pre>
-<p>{{ __('exercises/2_29.description.7') }}</p>
+<p>{{ __('exercises/2_29.description.15') }}</p>
