bf682f63fe1dc0900946a19bbc2aaec7c105d57d
Anton Burenkov <buran665@yandex.ru>
2021-09-09 21:08:26 +0700

    color 2.30

--- resources/views/exercise/listing/2_30.blade.php
+++ resources/views/exercise/listing/2_30.blade.php
@@ -1,9 +1,22 @@
-<p>{{ __('exercises/2_30.description.1') }}<a href="{{ route('exercises.show', getExercise('2.21')) }}">2.21</a>
-{{ __('exercises/2_30.description.2') }}</p>
+<p>{{ __('exercises/2_30.description.1') }}
+<code>square-tree</code>
+{{ __('exercises/2_30.description.2') }}
+<code>square-list</code>
+{{ __('exercises/2_30.description.3') }}
+<a href="{{ route('exercises.show', getExercise('2.21')) }}">2.21</a>
+{{ __('exercises/2_30.description.4') }}
+<code>square-tree</code>
+{{ __('exercises/2_30.description.5') }}
+</p>
 <pre><code>(square-tree
  (list 1
        (list 2 (list 3 4) 5)
        (list 6 7)))
 <i>(1 (4 (9 16) 25) (36 49))</i>
 </code></pre>
-<p>{{ __('exercises/2_30.description.3') }}</p>
+<p>{{ __('exercises/2_30.description.6') }}
+<code>square-tree</code>
+{{ __('exercises/2_30.description.7') }}
+<code>map</code>
+{{ __('exercises/2_30.description.8') }}
+</p>
