75760161d803c94552c1721e727a6fd298a74083
Anton Burenkov <buran665@yandex.ru>
2021-09-11 12:41:46 +0700

    color 2.36

--- resources/views/exercise/listing/2_36.blade.php
+++ resources/views/exercise/listing/2_36.blade.php
@@ -1,4 +1,19 @@
-<p>{{ __('exercises/2_36.description') }}</p>
+<p>{{ __('exercises/2_36.description.1') }}
+<code>accumulate-n</code>
+{{ __('exercises/2_36.description.2') }}
+<code>accumulate</code>
+{{ __('exercises/2_36.description.3') }}
+<code>s</code>
+{{ __('exercises/2_36.description.4') }}
+<code>((1 2 3) (4 5 6) (7 8 9) (10 11 12))</code>
+{{ __('exercises/2_36.description.5') }}
+<code>(accumulate-n + 0 s)</code>
+{{ __('exercises/2_36.description.6') }}
+<code>(22 26 30)</code>
+{{ __('exercises/2_36.description.7') }}
+<code>accumulate-n</code>
+{{ __('exercises/2_36.description.8') }}
+</p>
 <pre><code>(define (accumulate-n op init seqs)
   (if (null? (car seqs))
       nil
