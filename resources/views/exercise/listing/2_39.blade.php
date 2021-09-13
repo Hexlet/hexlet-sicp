d6b1fd75ee2d4778076ed9b1fbf0f693cc617b4c
Anton Burenkov <buran665@yandex.ru>
2021-09-13 22:04:38 +0700

    color 2.39

--- resources/views/exercise/listing/2_39.blade.php
+++ resources/views/exercise/listing/2_39.blade.php
@@ -1,6 +1,14 @@
-<p>{{ __('exercises/2_39.description.1') }}<a href="{{ route('exercises.show', getExercise('2.18')) }}">2.18</a>
-{{ __('exercises/2_39.description.2') }}<a href="{{ route('exercises.show', getExercise('2.38')) }}">2.38</a>
-{{ __('exercises/2_39.description.3') }}</p>
+<p>{{ __('exercises/2_39.description.1') }}
+<code>reverse</code>
+{{ __('exercises/2_39.description.2') }}
+<a href="{{ route('exercises.show', getExercise('2.18')) }}">2.18</a>
+{{ __('exercises/2_39.description.3') }}
+<code>fold-right</code>
+{{ __('exercises/2_39.description.4') }}
+<code>fold-left</code>
+{{ __('exercises/2_39.description.5') }}
+<a href="{{ route('exercises.show', getExercise('2.38')) }}">2.38</a>
+{{ __('exercises/2_39.description.6') }}</p>
 <pre><code>(define (reverse-right sequence)
   (fold-right (lambda (x y) &lt;??&gt;) nil sequence))
 
