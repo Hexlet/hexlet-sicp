0cd40c554564c07ea05095e00e8df4a760254a2a
Anton Burenkov <buran665@yandex.ru>
2021-09-13 22:13:22 +0700

    color 2.40

--- resources/views/exercise/listing/2_40.blade.php
+++ resources/views/exercise/listing/2_40.blade.php
@@ -1,1 +1,14 @@
-<p>{{ __('exercises/2_40.description') }}</p>
+<p>{{ __('exercises/2_40.description.1') }}
+<code>unique-pairs</code>
+{{ __('exercises/2_40.description.2') }}
+<code>n</code>
+{{ __('exercises/2_40.description.3') }}
+<code>(i,j)</code>
+{{ __('exercises/2_40.description.4') }}
+<code>1 ≤ j < i ≤ n</code>
+{{ __('exercises/2_40.description.5') }}
+<code>unique-pairs</code>
+{{ __('exercises/2_40.description.6') }}
+<code>prime-sum-pairs</code>
+{{ __('exercises/2_40.description.7') }}
+</p>
