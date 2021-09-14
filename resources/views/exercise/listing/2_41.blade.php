5e0a7bad07049f0db3f61b61b8eb98e324cf906e
Anton Burenkov <buran665@yandex.ru>
2021-09-14 22:20:32 +0700

    color 2.41

--- resources/views/exercise/listing/2_41.blade.php
+++ resources/views/exercise/listing/2_41.blade.php
@@ -1,1 +1,12 @@
-<p>{{ __('exercises/2_41.description') }}</p>
+<p>{{ __('exercises/2_41.description.1') }}
+<code>i</code>
+{{ __('exercises/2_41.description.2') }}
+<code>j</code>
+{{ __('exercises/2_41.description.3') }}
+<code>k</code>
+{{ __('exercises/2_41.description.4') }}
+<code>n</code>
+{{ __('exercises/2_41.description.5') }}
+<code>s</code>
+{{ __('exercises/2_41.description.6') }}
+</p>
