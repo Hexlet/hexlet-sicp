66ed3879c5276e0b469286287dc740c4e48f1f29
Anton Burenkov <buran665@yandex.ru>
2021-09-16 21:09:43 +0700

    color 2.43

--- resources/lang/en/exercises/2_43.php
+++ resources/lang/en/exercises/2_43.php
@@ -6,13 +6,19 @@ return [
         '1' =>
         "Louis Reasoner is having a terrible time doing exercise ",
         '2' =>
-        ". His queens procedure seems to work, but it runs extremely slowly. " .
-        "(Louis never does manage to wait long enough for it to solve even the 6 × 6 case.) " .
-        "When Louis asks Eva Lu Ator for help, she points out that he has interchanged the order of the nested mappings in the flatmap, writing it as",
+        ". His ",
         '3' =>
+        " procedure seems to work, but it runs extremely slowly. (Louis never does manage to wait long enough for it to solve even the ",
+        '4' =>
+        " case.) When Louis asks Eva Lu Ator for help, she points out that he has interchanged the order of the nested mappings in the ",
+        '5' =>
+        ", writing it as",
+        '6' =>
         "Explain why this interchange makes the program run slowly. " .
         "Estimate how long it will take Louis's program to solve the eight-queens puzzle, assuming that the program in exercise ",
-        '4' =>
-        " solves the puzzle in time T.",
+        '7' =>
+        " solves the puzzle in time ",
+        '8' =>
+        ".",
     ],
 ];
--- resources/views/exercise/listing/2_43.blade.php
+++ resources/views/exercise/listing/2_43.blade.php
@@ -1,5 +1,12 @@
 <p>{{ __('exercises/2_43.description.1') }}<a href="{{ route('exercises.show', getExercise('2.42')) }}">2.42</a>
-{{ __('exercises/2_43.description.2') }}</p>
+{{ __('exercises/2_43.description.2') }}
+<code>queens</code>
+{{ __('exercises/2_43.description.3') }}
+<code>6 × 6</code>
+{{ __('exercises/2_43.description.4') }}
+<code>flatmap</code>
+{{ __('exercises/2_43.description.5') }}
+</p>
 <pre><code>(flatmap
  (lambda (new-row)
    (map (lambda (rest-of-queens)
@@ -7,5 +14,8 @@
         (queen-cols (- k 1))))
  (enumerate-interval 1 board-size))
 </code></pre>
-<p>{{ __('exercises/2_43.description.3') }}<a href="{{ route('exercises.show', getExercise('2.42')) }}">2.42</a>
-{{ __('exercises/2_43.description.4') }}</p>
+<p>{{ __('exercises/2_43.description.6') }}<a href="{{ route('exercises.show', getExercise('2.42')) }}">2.42</a>
+{{ __('exercises/2_43.description.7') }}
+<code>T</code>
+{{ __('exercises/2_43.description.8') }}
+</p>
