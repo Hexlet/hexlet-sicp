5d8aeec30a116485f48143dfe710428f78919196
Anton Burenkov <buran665@yandex.ru>
2021-09-18 16:09:57 +0700

    color 2.48

--- resources/lang/en/exercises/2_48.php
+++ resources/lang/en/exercises/2_48.php
@@ -7,6 +7,12 @@ return [
         "A directed line segment in the plane can be represented as a pair of vectors -- the vector running from the origin to the start-point of the segment, and the vector running from the origin to the end-point of the segment. " .
         "Use your vector representation from exercise ",
         '2' =>
-        " to define a representation for segments with a constructor make-segment and selectors start-segment and end-segment.",
+        " to define a representation for segments with a constructor ",
+        '3' =>
+        " and selectors ",
+        '4' =>
+        " and ",
+        '5' =>
+        ".",
     ],
 ];
