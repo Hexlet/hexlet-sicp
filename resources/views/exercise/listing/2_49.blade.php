a782b97b6227084ed5b5e03c402499d2b2d779a6
Anton Burenkov <buran665@yandex.ru>
2021-09-18 16:13:53 +0700

    color 2.49

--- resources/views/exercise/listing/2_49.blade.php
+++ resources/views/exercise/listing/2_49.blade.php
@@ -1,5 +1,11 @@
-<p>{{ __('exercises/2_49.description.1') }}</p>
-<p>{{ __('exercises/2_49.description.2') }}</p>
+<p>{{ __('exercises/2_49.description.1') }}
+<code>segments->painter</code>
+{{ __('exercises/2_49.description.2') }}
+</p>
 <p>{{ __('exercises/2_49.description.3') }}</p>
 <p>{{ __('exercises/2_49.description.4') }}</p>
 <p>{{ __('exercises/2_49.description.5') }}</p>
+<p>{{ __('exercises/2_49.description.6') }}
+<code>wave</code>
+{{ __('exercises/2_49.description.7') }}
+</p>
