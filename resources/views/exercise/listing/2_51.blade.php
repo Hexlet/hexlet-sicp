33a889e7a8dfe2c6e24083e7e3431e5c3a0fd85c
Anton Burenkov <buran665@yandex.ru>
2021-09-19 19:06:31 +0700

    color 2.51

--- resources/views/exercise/listing/2_51.blade.php
+++ resources/views/exercise/listing/2_51.blade.php
@@ -1,2 +1,13 @@
-<p>{{ __('exercises/2_51.description.1') }}<a href="{{ route('exercises.show', getExercise('2.50')) }}">2.50</a>
-{{ __('exercises/2_51.description.2') }}</p>
+<p>{{ __('exercises/2_51.description.1') }}
+<code>below</code>
+{{ __('exercises/2_51.description.2') }}
+<code>Below</code>
+{{ __('exercises/2_51.description.3') }}
+<code>below</code>
+{{ __('exercises/2_51.description.4') }}
+<code>beside</code>
+{{ __('exercises/2_51.description.5') }}
+<code>beside</code>
+{{ __('exercises/2_51.description.6') }}
+<a href="{{ route('exercises.show', getExercise('2.50')) }}">2.50</a>
+{{ __('exercises/2_51.description.7') }}</p>
