d25766d4005707cc298cc519be41b9dae6063b1a
Anton Burenkov <buran665@yandex.ru>
2021-09-22 21:38:02 +0700

    color 2.57

--- resources/lang/en/exercises/2_57.php
+++ resources/lang/en/exercises/2_57.php
@@ -6,7 +6,12 @@ return [
         '1' =>
         "Extend the differentiation program to handle sums and products of arbitrary numbers of (two or more) terms. Then the last example above could be expressed as",
         '2' =>
-        "Try to do this by changing only the representation for sums and products, without changing the deriv procedure at all. " .
-        "For example, the addend of a sum would be the first term, and the augend would be the sum of the rest of the terms.",
+        "Try to do this by changing only the representation for sums and products, without changing the ",
+        '3' =>
+        " procedure at all. For example, the ",
+        '4' =>
+        " of a sum would be the first term, and the ",
+        '5' =>
+        " would be the sum of the rest of the terms.",
     ],
 ];
