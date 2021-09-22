7b5631564e4c60b70bd9155267f0d08dc345b0c1
Anton Burenkov <buran665@yandex.ru>
2021-09-22 21:45:54 +0700

    color 2.58

--- resources/views/exercise/listing/2_58.blade.php
+++ resources/views/exercise/listing/2_58.blade.php
@@ -1,3 +1,18 @@
-<p>{{ __('exercises/2_58.description.1') }}</p>
-<p>{{ __('exercises/2_58.description.2') }}</p>
-<p>{{ __('exercises/2_58.description.3') }}</p>
+<p>{{ __('exercises/2_58.description.1') }}
+<code>+</code>
+{{ __('exercises/2_58.description.2') }}
+<code>*</code>
+{{ __('exercises/2_58.description.3') }}
+</p>
+<p>{{ __('exercises/2_58.description.4') }}
+<code>(x + (3 * (x + (y + 2))))</code>
+{{ __('exercises/2_58.description.5') }}
+<code>+</code>
+{{ __('exercises/2_58.description.6') }}
+<code>*</code>
+{{ __('exercises/2_58.description.7') }}
+</p>
+<p>{{ __('exercises/2_58.description.8') }}
+<code>(x + 3 * (x + y + 2))</code>
+{{ __('exercises/2_58.description.9') }}
+</p>
