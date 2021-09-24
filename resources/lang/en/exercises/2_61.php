1a58cd6a7611c6dbe891390a7d873e9320b08f2c
Anton Burenkov <buran665@yandex.ru>
2021-09-24 19:59:21 +0700

    color 2.61

--- resources/lang/en/exercises/2_61.php
+++ resources/lang/en/exercises/2_61.php
@@ -2,7 +2,12 @@
 
 return [
     'title' => 'Adjoin-set for the ordered representation',
-    'description' =>
-        "Give an implementation of adjoin-set using the ordered representation. " .
-        "By analogy with element-of-set? show how to take advantage of the ordering to produce a procedure that requires on the average about half as many steps as with the unordered representation.",
+    'description' => [
+        '1' =>
+        "Give an implementation of ",
+        '2' =>
+        " using the ordered representation. By analogy with ",
+        '3' =>
+        " show how to take advantage of the ordering to produce a procedure that requires on the average about half as many steps as with the unordered representation.",
+    ],
 ];
