d2924b29f48b54d103293602f65983934c4d4fff
Anton Burenkov <buran665@yandex.ru>
2021-09-24 20:03:11 +0700

    color 2.62

--- resources/lang/en/exercises/2_62.php
+++ resources/lang/en/exercises/2_62.php
@@ -2,6 +2,12 @@
 
 return [
     'title' => 'Union-set for for sets represented as ordered lists',
-    'description' =>
-        "Give a Θ(n) implementation of union-set for sets represented as ordered lists.",
+    'description' => [
+        '1' =>
+        "Give a ",
+        '2' =>
+        " implementation of ",
+        '3' =>
+        " for sets represented as ordered lists.",
+    ],
 ];
--- resources/views/exercise/listing/2_62.blade.php
+++ resources/views/exercise/listing/2_62.blade.php
@@ -1,1 +1,6 @@
-<p>{{ __('exercises/2_62.description') }}</p>
+<p>{{ __('exercises/2_62.description.1') }}
+<code>Θ(n)</code>
+{{ __('exercises/2_62.description.2') }}
+<code>union-set</code>
+{{ __('exercises/2_62.description.3') }}
+</p>
