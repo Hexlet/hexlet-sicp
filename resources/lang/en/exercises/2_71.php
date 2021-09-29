67f10534eeb6404cc0147f56106b9d8cb83ae041
Anton Burenkov <buran665@yandex.ru>
2021-09-29 11:46:25 +0700

    color 2.71

--- resources/lang/en/exercises/2_71.php
+++ resources/lang/en/exercises/2_71.php
@@ -2,8 +2,18 @@
 
 return [
     'title' => 'Frequencies of the symbols',
-    'description' =>
-        "Suppose we have a Huffman tree for an alphabet of n symbols, and that the relative frequencies of the symbols are 1, 2, 4, ..., 2ⁿ⁻¹. " .
-        "Sketch the tree for n=5; for n=10. " .
-        "In such a tree (for general n) how many bits are required to encode the most frequent symbol? the least frequent symbol?",
+    'description' => [
+        '1' =>
+        "Suppose we have a Huffman tree for an alphabet of ",
+        '2' =>
+        " symbols, and that the relative frequencies of the symbols are ",
+        '3' =>
+        ". Sketch the tree for ",
+        '4' =>
+        "; for ",
+        '5' =>
+        ". In such a tree (for general ",
+        '6' =>
+        ") how many bits are required to encode the most frequent symbol? the least frequent symbol?",
+    ],
 ];
