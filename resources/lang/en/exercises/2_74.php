131d625439f2b5529590b749ac3525d757ac7198
Anton Burenkov <buran665@yandex.ru>
2021-09-30 18:49:37 +0700

    color 2.74

--- resources/lang/en/exercises/2_74.php
+++ resources/lang/en/exercises/2_74.php
@@ -11,14 +11,24 @@ return [
         '2' =>
         "Show how such a strategy can be implemented with data-directed programming. " .
         "As an example, suppose that each division's personnel records consist of a single file, which contains a set of records keyed on employees' names. The structure of the set varies from division to division. " .
-        "Furthermore, each employee's record is itself a set (structured differently from division to division) that contains information keyed under identifiers such as address and salary. In particular:",
+        "Furthermore, each employee's record is itself a set (structured differently from division to division) that contains information keyed under identifiers such as ",
         '3' =>
-        "a.  Implement for headquarters a get-record procedure that retrieves a specified employee's record from a specified personnel file. The procedure should be applicable to any division's file. Explain how the individual divisions' files should be structured. In particular, what type information must be supplied?",
+        " and ",
         '4' =>
-        "b.  Implement for headquarters a get-salary procedure that returns the salary information from a given employee's record from any division's personnel file. How should the record be structured in order to make this operation work?",
+        ". In particular:",
         '5' =>
-        "c.  Implement for headquarters a find-employee-record procedure. This should search all the divisions' files for the record of a given employee and return the record. Assume that this procedure takes as arguments an employee's name and a list of all the divisions' files.",
+        "a.  Implement for headquarters a ",
         '6' =>
+        " procedure that retrieves a specified employee's record from a specified personnel file. The procedure should be applicable to any division's file. Explain how the individual divisions' files should be structured. In particular, what type information must be supplied?",
+        '7' =>
+        "b.  Implement for headquarters a ",
+        '8' =>
+        " procedure that returns the salary information from a given employee's record from any division's personnel file. How should the record be structured in order to make this operation work?",
+        '9' =>
+        "c.  Implement for headquarters a ",
+        '10' =>
+        " procedure. This should search all the divisions' files for the record of a given employee and return the record. Assume that this procedure takes as arguments an employee's name and a list of all the divisions' files.",
+        '11' =>
         "d.  When Insatiable takes over a new company, what changes must be made in order to incorporate the new personnel information into the central system?",
     ],
 ];
