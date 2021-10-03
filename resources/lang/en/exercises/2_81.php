fd70d23646b5222fae496686d2220784485682c5
Anton Burenkov <buran665@yandex.ru>
2021-10-03 12:27:12 +0700

    color 2.81

--- resources/lang/en/exercises/2_81.php
+++ resources/lang/en/exercises/2_81.php
@@ -4,21 +4,39 @@ return [
     'title' => "Coercion of the arguments with same type",
     'description' => [
         '1' =>
-        "Louis Reasoner has noticed that apply-generic may try to coerce the arguments to each other's type even if they already have the same type. " .
-        "Therefore, he reasons, we need to put procedures in the coercion table to 'coerce' arguments of each type to their own type. " .
-        "For example, in addition to the scheme-number->complex coercion shown above, he would do:",
+        "Louis Reasoner has noticed that ",
         '2' =>
-        "a. With Louis's coercion procedures installed, what happens if apply-generic is called with two arguments of type scheme-number or two arguments of type complex for an operation that is not found in the table for those types? " .
-        "For example, assume that we've defined a generic exponentiation operation:",
+        " may try to coerce the arguments to each other's type even if they already have the same type. " .
+        "Therefore, he reasons, we need to put procedures in the coercion table to 'coerce' arguments of each type to their own type. " .
+        "For example, in addition to the ",
         '3' =>
-        "and have put a procedure for exponentiation in the Scheme-number package but not in any other package:",
+        " coercion shown above, he would do:",
         '4' =>
-        ";; following added to Scheme-number package",
+        "a. With Louis's coercion procedures installed, what happens if ",
         '5' =>
-        "What happens if we call exp with two complex numbers as arguments?",
+        " is called with two arguments of type ",
         '6' =>
-        "b. Is Louis correct that something had to be done about coercion with arguments of the same type, or does apply-generic work correctly as is?",
+        " or two arguments of type ",
         '7' =>
-        "c. Modify apply-generic so that it doesn't try coercion if the two arguments have the same type.",
+        " for an operation that is not found in the table for those types? " .
+        "For example, assume that we've defined a generic exponentiation operation:",
+        '8' =>
+        "and have put a procedure for exponentiation in the ",
+        '9' =>
+        " package but not in any other package:",
+        '10' =>
+        ";; following added to ",
+        '11' =>
+        " package",
+        '12' =>
+        "What happens if we call ",
+        '13' =>
+        " with two complex numbers as arguments?",
+        '14' =>
+        "b. Is Louis correct that something had to be done about coercion with arguments of the same type, or does all work correctly as is?",
+        '15' =>
+        "c. Modify ",
+        '16' =>
+        " so that it doesn't try coercion if the two arguments have the same type.",
     ],
 ];
