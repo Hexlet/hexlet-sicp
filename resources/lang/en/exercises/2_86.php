74543a23155de2a39c2ce1b9a956ddd082cb6005
Anton Burenkov <buran665@yandex.ru>
2021-10-06 09:51:24 +0700

    color 2.86

--- resources/lang/en/exercises/2_86.php
+++ resources/lang/en/exercises/2_86.php
@@ -2,8 +2,14 @@
 
 return [
     'title' => 'Generic complex numbers',
-    'description' =>
+    'description' => [
+        '1' =>
         "Suppose we want to handle complex numbers whose real parts, imaginary parts, magnitudes, and angles can be either ordinary numbers, rational numbers, or other numbers we might wish to add to the system. " .
         "Describe and implement the changes to the system needed to accommodate this. " .
-        "You will have to define operations such as sine and cosine that are generic over ordinary numbers and rational numbers.",
+        "You will have to define operations such as ",
+        '2' =>
+        " and ",
+        '3' =>
+        " that are generic over ordinary numbers and rational numbers.",
+    ],
 ];
--- resources/views/exercise/listing/2_86.blade.php
+++ resources/views/exercise/listing/2_86.blade.php
@@ -1,1 +1,6 @@
-<p>{{ __('exercises/2_86.description') }}</p>
+<p>{{ __('exercises/2_86.description.1') }}
+<code>sine</code>
+{{ __('exercises/2_86.description.2') }}
+<code>cosine</code>
+{{ __('exercises/2_86.description.3') }}
+</p>
