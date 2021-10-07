1e6f1d135e2f6c54b0957b748b0c5ddee0b6102c
Anton Burenkov <buran665@yandex.ru>
2021-10-07 10:29:57 +0700

    color 2.91

--- resources/lang/en/exercises/2_91.php
+++ resources/lang/en/exercises/2_91.php
@@ -13,12 +13,35 @@ return [
         "Stop when the order of the divisor exceeds the order of the dividend and declare the dividend to be the remainder. " .
         "Also, if the dividend ever becomes zero, return zero as both quotient and remainder.",
         '3' =>
-        "We can design a div-poly procedure on the model of add-poly and mul-poly. The procedure checks to see if the two polys have the same variable. " .
-        "If so, div-poly strips off the variable and passes the problem to div-terms, which performs the division operation on term lists. " .
-        "Div-poly finally reattaches the variable to the result supplied by div-terms. " .
-        "It is convenient to design div-terms to compute both the quotient and the remainder of a division. " .
-        "Div-terms can take two term lists as arguments and return a list of the quotient term list and the remainder term list.",
+        "We can design a ",
         '4' =>
-        "Complete the following definition of div-terms by filling in the missing expressions. Use this to implement div-poly, which takes two polys as arguments and returns a list of the quotient and remainder polys.",
+        " procedure on the model of ",
+        '5' =>
+        " and ",
+        '6' =>
+        ". The procedure checks to see if the two polys have the same variable. If so, ",
+        '7' =>
+        " strips off the variable and passes the problem to ",
+        '8' =>
+        ", which performs the division operation on term lists. ",
+        '9' =>
+        " finally reattaches the variable to the result supplied by ",
+        '10' =>
+        ". It is convenient to design ",
+        '11' =>
+        " to compute both the quotient and the remainder of a division. " .
+        "Div-terms can take two term lists as arguments and return a list of the quotient term list and the remainder term list.",
+        '12' =>
+        "Complete the following definition of ",
+        '13' =>
+        " by filling in the missing expressions. Use this to implement ",
+        '14' =>
+        ", which takes two ",
+        '15' =>
+        " as arguments and returns a list of the ",
+        '16' =>
+        " quotient and ",
+        '17' =>
+        " remainder.",
     ],
 ];
