b7dc06edf5f946d21776585c541383ba0908feb7
Anton Burenkov <buran665@yandex.ru>
2021-10-08 12:38:22 +0700

    color 2.94

--- resources/lang/en/exercises/2_94.php
+++ resources/lang/en/exercises/2_94.php
@@ -4,10 +4,23 @@ return [
     'title' => "Generic operation",
     'description' => [
         '1' =>
-        "Using div-terms, implement the procedure remainder-terms and use this to define gcd-terms as above. " .
-        "Now write a procedure gcd-poly that computes the polynomial GCD of two polys. (The procedure should signal an error if the two polys are not in the same variable.) " .
-        "Install in the system a generic operation greatest-common-divisor that reduces to gcd-poly for polynomials and to ordinary gcd for ordinary numbers. As a test, try",
+        "Using ",
         '2' =>
+        ", implement the procedure ",
+        '3' =>
+        " and use this to define ",
+        '4' =>
+        " as above. Now write a procedure ",
+        '5' =>
+        " that computes the polynomial GCD of two polys. (The procedure should signal an error if the two polys are not in the same variable.) " .
+        "Install in the system a generic operation ",
+        '6' =>
+        " that reduces to ",
+        '7' =>
+        " for polynomials and to ordinary ",
+        '8' =>
+        " for ordinary numbers. As a test, try",
+        '9' =>
         "and check your result by hand.",
     ],
 ];
