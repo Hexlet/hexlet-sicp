8ccb087a52d7c68c34d92d72f0b32c057265e127
Anton Burenkov <buran665@yandex.ru>
2021-10-13 12:01:01 +0700

    color 3.44

--- resources/views/exercise/listing/3_44.blade.php
+++ resources/views/exercise/listing/3_44.blade.php
@@ -1,6 +1,14 @@
-<p>{{ __('exercises/3_44.description.1') }}</p>
+<p>{{ __('exercises/3_44.description.1') }}
+<code>make-account</code>
+{{ __('exercises/3_44.description.2') }}
+</p>
 <pre><code>(define (transfer from-account to-account amount)
   ((from-account 'withdraw) amount)
   ((to-account 'deposit) amount))
 </code></pre>
-<p>{{ __('exercises/3_44.description.2') }}</p>
+<p>{{ __('exercises/3_44.description.3') }}
+<code>from-account</code>
+{{ __('exercises/3_44.description.4') }}
+<code>amount</code>
+{{ __('exercises/3_44.description.5') }}
+</p>
