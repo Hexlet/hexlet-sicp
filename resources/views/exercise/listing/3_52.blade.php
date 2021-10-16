a6e5ed82a2445fbd4ed53fec9ab1772ccf0d52aa
Anton Burenkov <buran665@yandex.ru>
2021-10-16 10:37:30 +0700

    color 3.52

--- resources/views/exercise/listing/3_52.blade.php
+++ resources/views/exercise/listing/3_52.blade.php
@@ -15,4 +15,17 @@
 
 (display-stream z)
 </code></pre>
-<p>{{ __('exercises/3_52.description.2') }}</p>
+<p>{{ __('exercises/3_52.description.2') }}
+<code>sum</code>
+{{ __('exercises/3_52.description.3') }}
+<code>stream-ref</code>
+{{ __('exercises/3_52.description.4') }}
+<code>display-stream</code>
+{{ __('exercises/3_52.description.5') }}
+<code>(delay &lt;exp&gt;)</code>
+{{ __('exercises/3_52.description.6') }}
+<code>(lambda () &lt;exp&gt;)</code>
+{{ __('exercises/3_52.description.7') }}
+<code>memo-proc</code>
+{{ __('exercises/3_52.description.8') }}
+</p>
