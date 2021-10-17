4627f559af6660df6624f8ca217d5ef2b8b4a7a5
Anton Burenkov <buran665@yandex.ru>
2021-10-17 12:17:19 +0700

    color 3.55

--- resources/views/exercise/listing/3_55.blade.php
+++ resources/views/exercise/listing/3_55.blade.php
@@ -1,1 +1,12 @@
-<p>{{ __('exercises/3_55.description') }}</p>
+<p>{{ __('exercises/3_55.description.1') }}
+<code>partial-sums</code>
+{{ __('exercises/3_55.description.2') }}
+<code>S</code>
+{{ __('exercises/3_55.description.3') }}
+<code>S₀, S₀ + S₁, S₀ + S₁ + S₂, ...</code>
+{{ __('exercises/3_55.description.4') }}
+<code>(partial-sums integers)</code>
+{{ __('exercises/3_55.description.5') }}
+<code>1, 3, 6, 10, 15 ...</code>
+{{ __('exercises/3_55.description.6') }}
+</p>
