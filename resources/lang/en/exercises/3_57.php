803de72247fb1f5ac7e1c68258ad47596d54b31a
Anton Burenkov <buran665@yandex.ru>
2021-10-18 10:09:14 +0700

    color 3.57

--- resources/lang/en/exercises/3_57.php
+++ resources/lang/en/exercises/3_57.php
@@ -2,7 +2,20 @@
 
 return [
     'title' => 'Additions in computing Fibonacci numbers.',
-    'description' =>
-        "How many additions are performed when we compute the nth Fibonacci number using the definition of fibs based on the add-streams procedure? " .
-        "Show that the number of additions would be exponentially greater if we had implemented (delay <exp>) simply as (lambda () <exp>), without using the optimization provided by the memo-proc procedure described in section 3.5.1.",
+    'description' => [
+        '1' =>
+        "How many additions are performed when we compute the ",
+        '2' =>
+        "th Fibonacci number using the definition of ",
+        '3' =>
+        " based on the ",
+        '4' =>
+        " procedure? Show that the number of additions would be exponentially greater if we had implemented ",
+        '5' =>
+        " simply as ",
+        '6' =>
+        ", without using the optimization provided by the ",
+        '7' =>
+        " procedure described in section 3.5.1.",
+    ],
 ];
