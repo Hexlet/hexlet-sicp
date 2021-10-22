2f6627eec93dbc7e1a95b6388583e75997055315
Anton Burenkov <buran665@yandex.ru>
2021-10-22 15:21:15 +0700

    color 3.66

--- resources/lang/en/exercises/3_66.php
+++ resources/lang/en/exercises/3_66.php
@@ -2,8 +2,16 @@
 
 return [
     'title' => 'Order of pairs in the stream',
-    'description' =>
-        "Examine the stream (pairs integers integers). Can you make any general comments about the order in which the pairs are placed into the stream? " .
-        "For example, about how many pairs precede the pair (1,100)? the pair (99,100)? the pair (100,100)? " .
-        "(If you can make precise mathematical statements here, all the better. But feel free to give more qualitative answers if you find yourself getting bogged down.)",
+    'description' => [
+        '1' =>
+        "Examine the stream ",
+        '2' =>
+        ". Can you make any general comments about the order in which the pairs are placed into the stream? For example, about how many pairs precede the pair ",
+        '3' =>
+        "? the pair ",
+        '4' =>
+        "? the pair ",
+        '5' =>
+        "? (If you can make precise mathematical statements here, all the better. But feel free to give more qualitative answers if you find yourself getting bogged down.)",
+    ],
 ];
