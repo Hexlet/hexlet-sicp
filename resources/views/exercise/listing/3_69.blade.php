198fba3517151924df11f5821ffe0a41f80ccd46
Anton Burenkov <buran665@yandex.ru>
2021-10-24 09:48:00 +0700

    color 3.69

--- resources/views/exercise/listing/3_69.blade.php
+++ resources/views/exercise/listing/3_69.blade.php
@@ -1,1 +1,22 @@
-<p>{{ __('exercises/3_69.description') }}</p>
+<p>{{ __('exercises/3_69.description.1') }}
+<code>triples</code>
+{{ __('exercises/3_69.description.2') }}
+<code>S</code>
+{{ __('exercises/3_69.description.3') }}
+<code>T</code>
+{{ __('exercises/3_69.description.4') }}
+<code>U</code>
+{{ __('exercises/3_69.description.5') }}
+<code>(Si, Tj, Uk)</code>
+{{ __('exercises/3_69.description.6') }}
+<code>i ≤ j ≤ k</code>
+{{ __('exercises/3_69.description.7') }}
+<code>triples</code>
+{{ __('exercises/3_69.description.8') }}
+<code>(i, j, k)</code>
+{{ __('exercises/3_69.description.9') }}
+<code>i ≤ j</code>
+{{ __('exercises/3_69.description.10') }}
+<code>i² + j² = k²</code>
+{{ __('exercises/3_69.description.11') }}
+</p>
