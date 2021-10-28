2eedf089dee30e5e1b6445c79f941939caa31fee
Anton Burenkov <buran665@yandex.ru>
2021-10-28 19:51:27 +0700

    color 3.76

--- resources/views/exercise/listing/3_76.blade.php
+++ resources/views/exercise/listing/3_76.blade.php
@@ -1,2 +1,7 @@
 <p>{{ __('exercises/3_76.description.1') }}<a href="{{ route('exercises.show', getExercise('3.75')) }}">3.75</a>
-{{ __('exercises/3_76.description.2') }}</p>
+{{ __('exercises/3_76.description.2') }}
+<code>smooth</code>
+{{ __('exercises/3_76.description.3') }}
+<code>smooth</code>
+{{ __('exercises/3_76.description.4') }}
+</p>
