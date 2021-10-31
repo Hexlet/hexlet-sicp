4a07b9405addd0e4f72de7fab6e1f5c2d080a5b8
Anton Burenkov <buran665@yandex.ru>
2021-10-31 13:57:24 +0700

    color 4.2

--- resources/views/exercise/listing/4_2.blade.php
+++ resources/views/exercise/listing/4_2.blade.php
@@ -1,3 +1,22 @@
-<p>{{ __('exercises/4_2.description.1') }}</p>
-<p>{{ __('exercises/4_2.description.2') }}</p>
-<p>{{ __('exercises/4_2.description.3') }}</p>
+<p>{{ __('exercises/4_2.description.1') }}
+<code>cond</code>
+{{ __('exercises/4_2.description.2') }}
+<code>eval</code>
+{{ __('exercises/4_2.description.3') }}
+</p>
+<p>{{ __('exercises/4_2.description.4') }}
+<code>(define x 3)</code>
+{{ __('exercises/4_2.description.5') }}
+</p>
+<p>{{ __('exercises/4_2.description.6') }}
+<code>call</code>
+{{ __('exercises/4_2.description.7') }}
+<code>(factorial 3)</code>
+{{ __('exercises/4_2.description.8') }}
+<code>(call factorial 3)</code>
+{{ __('exercises/4_2.description.9') }}
+<code>(+ 1 2)</code>
+{{ __('exercises/4_2.description.10') }}
+<code>(call + 1 2)</code>
+{{ __('exercises/4_2.description.11') }}
+</p>
