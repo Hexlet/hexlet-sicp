4365dac296565d9e399f9aba0941dda4710c43b1
Anton Burenkov <buran665@yandex.ru>
2021-11-02 16:00:09 +0700

    color 4.5

--- resources/lang/en/exercises/4_5.php
+++ resources/lang/en/exercises/4_5.php
@@ -3,10 +3,25 @@
 return [
     'title' => 'Modify the handling of cond',
     'description' => [
-        '1' => "Scheme allows an additional syntax for cond clauses, (<test> => <recipient>). If <test> evaluates " .
-        "to a true value, then <recipient> is evaluated. Its value must be a procedure of one argument; this " .
-        "procedure is then invoked on the value of the <test>, and the result is returned as the value of the cond " .
-        "expression. For example",
-        '2' => "returns 2. Modify the handling of cond so that it supports this extended syntax.",
+        '1' =>
+        "Scheme allows an additional syntax for ",
+        '2' =>
+        " clauses, ",
+        '3' =>
+        ". If ",
+        '4' =>
+        " evaluates to a true value, then ",
+        '5' =>
+        " is evaluated. Its value must be a procedure of one argument; this procedure is then invoked on the value of the ",
+        '6' =>
+        ", and the result is returned as the value of the ",
+        '7' =>
+        " expression. For example",
+        '8' =>
+        "returns ",
+        '9' =>
+        ". Modify the handling of ",
+        '10' =>
+        " so that it supports this extended syntax.",
     ],
 ];
--- resources/views/exercise/listing/4_5.blade.php
+++ resources/views/exercise/listing/4_5.blade.php
@@ -1,4 +1,22 @@
-<p>{{ __('exercises/4_5.description.1') }}</p>
+<p>{{ __('exercises/4_5.description.1') }}
+<code>cond</code>
+{{ __('exercises/4_5.description.2') }}
+<code>(&lt;test&gt; => &lt;recipient&gt;)</code>
+{{ __('exercises/4_5.description.3') }}
+<code>&lt;test&gt;</code>
+{{ __('exercises/4_5.description.4') }}
+<code>&lt;recipient&gt;</code>
+{{ __('exercises/4_5.description.5') }}
+<code>&lt;test&gt;</code>
+{{ __('exercises/4_5.description.6') }}
+<code>cond</code>
+{{ __('exercises/4_5.description.7') }}
+</p>
 <pre><code>(cond ((assoc ’b ’((a 1) (b 2))) => cadr)
       (else false))</code></pre>
-<p>{{ __('exercises/4_5.description.2') }}</p>
+<p>{{ __('exercises/4_5.description.8') }}
+<code>2</code>
+{{ __('exercises/4_5.description.9') }}
+<code>cond</code>
+{{ __('exercises/4_5.description.10') }}
+</p>
