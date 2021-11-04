0c3d2282c3200deccf48312cd82e9567f0fb880b
Anton Burenkov <buran665@yandex.ru>
2021-11-04 12:18:45 +0700

    color 4.10

--- resources/lang/en/exercises/4_10.php
+++ resources/lang/en/exercises/4_10.php
@@ -2,7 +2,15 @@
 
 return [
     'title' => 'Implement a new syntax for Scheme',
-    'description' => "By using data abstraction, we were able to write an eval procedure that is independent of " .
-    "the particular syntax of the language to be evaluated. To illustrate this, design and implement a new syntax " .
-    "for Scheme by modifying the procedures in this section, without changing eval or apply.",
+    'description' => [
+        '1' =>
+        "By using data abstraction, we were able to write an ",
+        '2' =>
+        " procedure that is independent of the particular syntax of the language to be evaluated. To illustrate this, design and implement a new syntax " .
+        "for Scheme by modifying the procedures in this section, without changing ",
+        '3' =>
+        " or ",
+        '4' =>
+        ".",
+    ],
 ];
--- resources/views/exercise/listing/4_10.blade.php
+++ resources/views/exercise/listing/4_10.blade.php
@@ -1,1 +1,8 @@
-<p>{{ __('exercises/4_10.description') }}</p>
+<p>{{ __('exercises/4_10.description.1') }}
+<code>eval</code>
+{{ __('exercises/4_10.description.2') }}
+<code>eval</code>
+{{ __('exercises/4_10.description.3') }}
+<code>apply</code>
+{{ __('exercises/4_10.description.4') }}
+</p>
