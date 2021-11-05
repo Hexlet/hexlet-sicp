33db28bf2c8dabbc88705040f7275f056a943a69
Anton Burenkov <buran665@yandex.ru>
2021-11-05 11:30:37 +0700

    color 4.13

--- resources/lang/en/exercises/4_13.php
+++ resources/lang/en/exercises/4_13.php
@@ -2,9 +2,15 @@
 
 return [
     'title' => 'Implement a special form make-unbound!',
-    'description' => "Scheme allows us to create new bindings for variables by means of define, but provides no " .
-    "way to get rid of bindings. Implement for the evaluator a special form make-unbound! that removes the binding " .
-    "of a given symbol from the environment in which the make-unbound! expression is evaluated. This problem is " .
-    "not completely specified. For example, should we remove only the binding in the first frame of the " .
-    "environment? Complete the specification and justify any choices you make.",
+    'description' => [
+        '1' =>
+        "Scheme allows us to create new bindings for variables by means of ",
+        '2' =>
+        ", but provides no way to get rid of bindings. Implement for the evaluator a special form ",
+        '3' =>
+        " that removes the binding of a given symbol from the environment in which the ",
+        '4' =>
+        " expression is evaluated. This problem is not completely specified. For example, should we remove only the binding in the first frame of the " .
+        "environment? Complete the specification and justify any choices you make.",
+    ],
 ];
--- resources/views/exercise/listing/4_13.blade.php
+++ resources/views/exercise/listing/4_13.blade.php
@@ -1,1 +1,8 @@
-<p>{{ __('exercises/4_13.description') }}</p>
+<p>{{ __('exercises/4_13.description.1') }}
+<code>define</code>
+{{ __('exercises/4_13.description.2') }}
+<code>make-unbound!</code>
+{{ __('exercises/4_13.description.3') }}
+<code>make-unbound!</code>
+{{ __('exercises/4_13.description.4') }}
+</p>
