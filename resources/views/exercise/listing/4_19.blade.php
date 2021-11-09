41f076b3263660f29fd716cbf8d318dd8b841c57
Anton Burenkov <buran665@yandex.ru>
2021-11-09 22:54:07 +0700

    color 4.19

--- resources/views/exercise/listing/4_19.blade.php
+++ resources/views/exercise/listing/4_19.blade.php
@@ -5,5 +5,39 @@
     (define a 5)
     (+ a b))
   (f 10))</code></pre>
-<p>{{ __('exercises/4_19.description.2') }}<a href="{{ route('exercises.show', getExercise('4.16')) }}">4.16</a>
-{{ __('exercises/4_19.description.3') }}</p>
+<p>{{ __('exercises/4_19.description.2') }}
+<code>define</code>
+{{ __('exercises/4_19.description.3') }}
+<code>b</code>
+{{ __('exercises/4_19.description.4') }}
+<code>11</code>
+{{ __('exercises/4_19.description.5') }}
+<code>a</code>
+{{ __('exercises/4_19.description.6') }}
+<code>5</code>
+{{ __('exercises/4_19.description.7') }}
+<code>16</code>
+{{ __('exercises/4_19.description.8') }}
+<a href="{{ route('exercises.show', getExercise('4.16')) }}">4.16</a>
+{{ __('exercises/4_19.description.9') }}
+<code>a</code>
+{{ __('exercises/4_19.description.10') }}
+<code>b</code>
+{{ __('exercises/4_19.description.11') }}
+<code>5</code>
+{{ __('exercises/4_19.description.12') }}
+<code>a</code>
+{{ __('exercises/4_19.description.13') }}
+<code>b</code>
+{{ __('exercises/4_19.description.14') }}
+<code>a</code>
+{{ __('exercises/4_19.description.15') }}
+<code>5</code>
+{{ __('exercises/4_19.description.16') }}
+<code>b</code>
+{{ __('exercises/4_19.description.17') }}
+<code>15</code>
+{{ __('exercises/4_19.description.18') }}
+<code>20</code>
+{{ __('exercises/4_19.description.19') }}
+</p>
