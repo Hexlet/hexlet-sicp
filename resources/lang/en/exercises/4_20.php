67da532a86c4b222aa7b736e67bf9a62236cf0be
Anton Burenkov <buran665@yandex.ru>
2021-11-10 20:54:27 +0700

    color 4.20

--- resources/lang/en/exercises/4_20.php
+++ resources/lang/en/exercises/4_20.php
@@ -3,24 +3,72 @@
 return [
     'title' => 'Special form of letrec',
     'description' => [
-        '1' => "Because internal definitions look sequential but are actually simultaneous, some people " .
-        "prefer to avoid them entirely, and use the special form letrec instead. Letrec looks like let, so " .
-        "it is not surprising that the variables it binds are bound simultaneously and have the same scope " .
-        "as each other. The sample procedure f above can be written without internal definitions, but with " .
-        "exactly the same meaning, as",
-        '2' => "Letrec expressions, which have the form",
-        '3' => "are a variation on let in which the expressions <expk> that provide the initial values for " .
-        "the variables <vark> are evaluated in an environment that includes all the letrec bindings. This " .
-        "permits recursion in the bindings, such as the mutual recursion of even? and odd? in the example " .
-        "above, or the evaluation of 10 factorial with",
-        '4' => "a. Implement letrec as a derived expression, by transforming a letrec expression into a let " .
-        "expression as shown in the text above or in exercise ",
-        '5' => ". That is, the letrec variables should be created with a let and then be assigned their values with set!.",
-        '6' => "b. Louis Reasoner is confused by all this fuss about internal definitions. The way he sees " .
-        "it, if you don't like to use define inside a procedure, you can just use let. Illustrate what is " .
-        "loose about his reasoning by drawing an environment diagram that shows the environment in which the " .
-        "<rest of body of f> is evaluated during evaluation of the expression (f 5), with f defined as in " .
-        "this exercise. Draw an environment diagram for the same evaluation, but with let in place of letrec " .
-        "in the definition of f.",
+        '1' =>
+        "Because internal definitions look sequential but are actually simultaneous, some people " .
+        "prefer to avoid them entirely, and use the special form ",
+        '2' =>
+        " instead. ",
+        '3' =>
+        " looks like ",
+        '4' =>
+        ", so it is not surprising that the variables it binds are bound simultaneously and have the same scope " .
+        "as each other. The sample procedure ",
+        '5' =>
+        " above can be written without internal definitions, but with exactly the same meaning, as",
+        '6' =>
+        "",
+        '7' =>
+        " expressions, which have the form",
+        '8' =>
+        "are a variation on ",
+        '9' =>
+        " in which the expressions ",
+        '10' =>
+        " that provide the initial values for the variables ",
+        '11' =>
+        " are evaluated in an environment that includes all the ",
+        '12' =>
+        " bindings. This permits recursion in the bindings, such as the mutual recursion of ",
+        '13' =>
+        " and ",
+        '14' =>
+        " in the example above, or the evaluation of ",
+        '15' =>
+        " factorial with",
+        '16' =>
+        "a. Implement ",
+        '17' =>
+        " as a derived expression, by transforming a ",
+        '18' =>
+        " expression into a ",
+        '19' =>
+        " expression as shown in the text above or in exercise ",
+        '20' =>
+        ". That is, the ",
+        '21' =>
+        " variables should be created with a ",
+        '22' =>
+        " and then be assigned their values with ",
+        '23' =>
+        ".",
+        '24' =>
+        "b. Louis Reasoner is confused by all this fuss about internal definitions. The way he sees " .
+        "it, if you don't like to use ",
+        '25' =>
+        " inside a procedure, you can just use ",
+        '26' =>
+        ". Illustrate what is loose about his reasoning by drawing an environment diagram that shows the environment in which the ",
+        '27' =>
+        " is evaluated during evaluation of the expression ",
+        '28' =>
+        ", with ",
+        '29' =>
+        " defined as in this exercise. Draw an environment diagram for the same evaluation, but with ",
+        '30' =>
+        " in place of ",
+        '31' =>
+        " in the definition of ",
+        '32' =>
+        ".",
     ],
 ];
