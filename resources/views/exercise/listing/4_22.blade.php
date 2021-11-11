642bce1a25be2c5055be72ebcc95f1de80acc7cf
Anton Burenkov <buran665@yandex.ru>
2021-11-11 13:26:29 +0700

    color 4.22

--- resources/views/exercise/listing/4_22.blade.php
+++ resources/views/exercise/listing/4_22.blade.php
@@ -1,1 +1,4 @@
-<p>{{ __('exercises/4_22.description') }}<a href="{{ route('exercises.show', getExercise('4.6')) }}">4.6</a>.</p>
+<p>{{ __('exercises/4_22.description.1') }}
+<code>let</code>
+{{ __('exercises/4_22.description.2') }}
+<a href="{{ route('exercises.show', getExercise('4.6')) }}">4.6</a>.</p>
