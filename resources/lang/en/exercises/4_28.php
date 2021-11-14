5d7dbcc8e2a483a980518aaf409410e4b6ce99e2
Anton Burenkov <buran665@yandex.ru>
2021-11-14 15:22:59 +0700

    color 4.28

--- resources/lang/en/exercises/4_28.php
+++ resources/lang/en/exercises/4_28.php
@@ -2,6 +2,14 @@
 
 return [
     'title' => 'Evaluate the operator using actual-value',
-    'description' => "Eval uses actual-value rather than eval to evaluate the operator before passing it to apply, " .
-    "in order to force the value of the operator. Give an example that demonstrates the need for this forcing.",
+    'description' => [
+        '1' =>
+        "Eval uses ",
+        '2' =>
+        " rather than ",
+        '3' =>
+        " to evaluate the operator before passing it to ",
+        '4' =>
+        ", in order to force the value of the operator. Give an example that demonstrates the need for this forcing.",
+    ],
 ];
--- resources/views/exercise/listing/4_28.blade.php
+++ resources/views/exercise/listing/4_28.blade.php
@@ -1,1 +1,10 @@
-<p>{{ __('exercises/4_28.description') }}</p>
+<p>
+<code>Eval</code>
+{{ __('exercises/4_28.description.1') }}
+<code>actual-value</code>
+{{ __('exercises/4_28.description.2') }}
+<code>eval</code>
+{{ __('exercises/4_28.description.3') }}
+<code>apply</code>
+{{ __('exercises/4_28.description.4') }}
+</p>
