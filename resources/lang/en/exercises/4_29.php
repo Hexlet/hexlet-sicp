1c67986d238ff01730c842dc883bb3198aab05e1
Anton Burenkov <buran665@yandex.ru>
2021-11-15 19:36:48 +0700

    color 4.29

--- resources/lang/en/exercises/4_29.php
+++ resources/lang/en/exercises/4_29.php
@@ -3,9 +3,18 @@
 return [
     'title' => 'Demonstrate the benefits of memoization',
     'description' => [
-        '1' => "Exhibit a program that you would expect to run much more slowly without memoization than with " .
-        "memoization. Also, consider the following interaction, where the id procedure is defined as in exercise ",
-        '2' => " and count starts at 0:",
-        '3' => "Give the responses both when the evaluator memoizes and when it does not.",
+        '1' =>
+        "Exhibit a program that you would expect to run much more slowly without memoization than with " .
+        "memoization. Also, consider the following interaction, where the ",
+        '2' =>
+        " procedure is defined as in exercise ",
+        '3' =>
+        " and ",
+        '4' =>
+        " starts at ",
+        '5' =>
+        ":",
+        '6' =>
+        "Give the responses both when the evaluator memoizes and when it does not.",
     ],
 ];
