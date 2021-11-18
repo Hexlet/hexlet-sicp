1e6c9e0cc2a708d3b8add0f5e51681a7b9de2ba5
Anton Burenkov <buran665@yandex.ru>
2021-11-18 12:06:46 +0700

    color 4.36

--- resources/lang/en/exercises/4_36.php
+++ resources/lang/en/exercises/4_36.php
@@ -3,12 +3,19 @@
 return [
     'title' => 'Generate arbitrary Pythagorean triples',
     'description' => [
-        '1' => "Exercise ",
-        '2' => " discussed how to generate the stream of all Pythagorean triples, with no upper bound on the size " .
-        "of the integers to be searched. Explain why simply replacing an-integer-between by " .
-        "an-integer-starting-from in the procedure in exercise ",
-        '3' => " is not an adequate way to generate arbitrary Pythagorean triples. Write a procedure that actually " .
-        "will accomplish this. (That is, write a procedure for which repeatedly typing try-again would in " .
-        "principle eventually generate all Pythagorean triples.)",
+        '1' =>
+        "Exercise ",
+        '2' =>
+        " discussed how to generate the stream of all Pythagorean triples, with no upper bound on the size " .
+        "of the integers to be searched. Explain why simply replacing ",
+        '3' =>
+        " by ",
+        '4' =>
+        " in the procedure in exercise ",
+        '5' =>
+        " is not an adequate way to generate arbitrary Pythagorean triples. Write a procedure that actually " .
+        "will accomplish this. (That is, write a procedure for which repeatedly typing ",
+        '6' =>
+        " would in principle eventually generate all Pythagorean triples.)",
     ],
 ];
--- resources/views/exercise/listing/4_36.blade.php
+++ resources/views/exercise/listing/4_36.blade.php
@@ -1,3 +1,11 @@
 <p>{{ __('exercises/4_36.description.1') }}<a href="{{ route('exercises.show', getExercise('3.69')) }}">3.69</a>
-{{ __('exercises/4_36.description.2') }}<a href="{{ route('exercises.show', getExercise('4.35')) }}">4.35</a>
-{{ __('exercises/4_36.description.3') }}</p>
+{{ __('exercises/4_36.description.2') }}
+<code>an-integer-between</code>
+{{ __('exercises/4_36.description.3') }}
+<code>an-integer-starting-from</code>
+{{ __('exercises/4_36.description.4') }}
+<a href="{{ route('exercises.show', getExercise('4.35')) }}">4.35</a>
+{{ __('exercises/4_36.description.5') }}
+<code>try-again</code>
+{{ __('exercises/4_36.description.6') }}
+</p>
