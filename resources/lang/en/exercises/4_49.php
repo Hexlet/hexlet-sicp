25615c95decee8c3721df416157bddd199d06f2e
Anton Burenkov <buran665@yandex.ru>
2021-11-22 17:10:54 +0700

    color 4.49

--- resources/lang/en/exercises/4_49.php
+++ resources/lang/en/exercises/4_49.php
@@ -2,8 +2,11 @@
 
 return [
     'title' => "Sentences generating",
-    'description' =>
-        "Alyssa P. Hacker is more interested in generating interesting sentences than in parsing them. " .
-        "She reasons that by simply changing the procedure parse-word so that it ignores the ''input sentence'' and instead always succeeds and generates an appropriate word, we can use the programs we had built for parsing to do generation instead. " .
+    'description' => [
+        '1' =>
+        "Alyssa P. Hacker is more interested in generating interesting sentences than in parsing them. She reasons that by simply changing the procedure ",
+        '2' =>
+        " so that it ignores the ''input sentence'' and instead always succeeds and generates an appropriate word, we can use the programs we had built for parsing to do generation instead. " .
         "Implement Alyssa's idea, and show the first half-dozen or so sentences generated.",
+    ],
 ];
