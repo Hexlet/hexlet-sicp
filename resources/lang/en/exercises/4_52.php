e1c52f6f1768eecaf9247f2f562f813bc318c0d6
Anton Burenkov <buran665@yandex.ru>
2021-11-23 14:26:12 +0700

    color 4.52

--- resources/lang/en/exercises/4_52.php
+++ resources/lang/en/exercises/4_52.php
@@ -2,7 +2,13 @@
 
 return [
     'title' => "If-fail construct",
-    'description' =>
-        "Implement a new construct called if-fail that permits the user to catch the failure of an expression. If-fail takes two expressions. " .
+    'description' => [
+        '1' =>
+        "Implement a new construct called ",
+        '2' =>
+        " that permits the user to catch the failure of an expression. ",
+        '3' =>
+        " takes two expressions. " .
         "It evaluates the first expression as usual and returns as usual if the evaluation succeeds. If the evaluation fails, however, the value of the second expression is returned, as in the following example:",
+    ],
 ];
--- resources/views/exercise/listing/4_52.blade.php
+++ resources/views/exercise/listing/4_52.blade.php
@@ -1,4 +1,9 @@
-<p>{{ __('exercises/4_52.description') }}</p>
+<p>{{ __('exercises/4_52.description.1') }}
+<code>if-fail</code>
+{{ __('exercises/4_52.description.2') }}
+<code>If-fail</code>
+{{ __('exercises/4_52.description.3') }}
+</p>
 <pre><code>;;; Amb-Eval input:
 (if-fail (let ((x (an-element-of '(1 3 5))))
            (require (even? x))
