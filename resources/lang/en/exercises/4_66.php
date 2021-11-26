a24efecb0f2c1f2b0b90bcf7db056b693772dbd8
Anton Burenkov <buran665@yandex.ru>
2021-11-26 17:55:46 +0700

    color 4.66

--- resources/lang/en/exercises/4_66.php
+++ resources/lang/en/exercises/4_66.php
@@ -8,13 +8,22 @@ return [
         '2' =>
         "In general, Ben's new system allows expressions of the form",
         '3' =>
-        "where accumulation-function can be things like sum, average, or maximum. Ben reasons that it should be a cinch to implement this. " .
-        "He will simply feed the query pattern to qeval. This will produce a stream of frames. " .
+        "where ",
+        '4' =>
+        " can be things like ",
+        '5' =>
+        ", ",
+        '6' =>
+        ", or ",
+        '7' =>
+        ". Ben reasons that it should be a cinch to implement this. He will simply feed the query pattern to ",
+        '8' =>
+        ". This will produce a stream of frames. " .
         "He will then pass this stream through a mapping function that extracts the value of the designated variable from each frame in the stream and feed the resulting stream of values to the accumulation function. " .
         "Just as Ben completes the implementation and is about to try it out, Cy walks by, still puzzling over the wheel query result in exercise ",
-        '4' =>
+        '9' =>
         ". When Cy shows Ben the system's response, Ben groans, ''Oh, no, my simple accumulation scheme won't work!''",
-        '5' =>
+        '10' =>
         "What has Ben just realized? Outline a method he can use to salvage the situation.",
     ],
 ];
