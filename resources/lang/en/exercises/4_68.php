2f7a0f491536498a8e7fabdfa3682d920e702365
Anton Burenkov <buran665@yandex.ru>
2021-11-27 12:18:46 +0700

    color 4.68

--- resources/lang/en/exercises/4_68.php
+++ resources/lang/en/exercises/4_68.php
@@ -4,8 +4,16 @@ return [
     'title' => "Rules for reverse operation",
     'description' => [
         '1' =>
-        'Define rules to implement the reverse operation of exercise ',
+        "Define rules to implement the ",
         '2' =>
-        ", which returns a list containing the same elements as a given list in reverse order. (Hint: Use append-to-form.) Can your rules answer both (reverse (1 2 3) ?x) and (reverse ?x (1 2 3)) ?",
+        " operation of exercise ",
+        '3' =>
+        ", which returns a list containing the same elements as a given list in reverse order. (Hint: Use ",
+        '4' =>
+        ".) Can your rules answer both ",
+        '5' =>
+        " and ",
+        '6' =>
+        "?",
     ],
 ];
