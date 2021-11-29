3d3f382e3ab46d83670c87457c06b89cc6847225
Anton Burenkov <buran665@yandex.ru>
2021-11-29 20:21:33 +0700

    color 4.73

--- resources/views/exercise/listing/4_73.blade.php
+++ resources/views/exercise/listing/4_73.blade.php
@@ -1,4 +1,9 @@
-<p>{{ __('exercises/4_73.description') }}</p>
+<p>{{ __('exercises/4_73.description.1') }}
+<code>flatten-stream</code>
+{{ __('exercises/4_73.description.2') }}
+<code>delay</code>
+{{ __('exercises/4_73.description.3') }}
+</p>
 <pre><code>(define (flatten-stream stream)
   (if (stream-null? stream)
       the-empty-stream
