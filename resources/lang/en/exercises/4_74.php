77018294828ce67fa58913ffaef3a8c3fccf994e
Anton Burenkov <buran665@yandex.ru>
2021-11-30 15:07:58 +0700

    color 4.74

--- resources/lang/en/exercises/4_74.php
+++ resources/lang/en/exercises/4_74.php
@@ -4,10 +4,18 @@ return [
     'title' => "A simpler version of stream-flatmap",
     'description' => [
         '1' =>
-        "Alyssa P. Hacker proposes to use a simpler version of stream-flatmap in negate, lisp-value, and find-assertions. She observes that the procedure that is mapped over the frame stream in these cases always produces either the empty stream or a singleton stream, so no interleaving is needed when combining these streams.",
+        "Alyssa P. Hacker proposes to use a simpler version of ",
         '2' =>
-        "a. Fill in the missing expressions in Alyssa's program.",
+        " in ",
         '3' =>
+        ", ",
+        '4' =>
+        ", and ",
+        '5' =>
+        ". She observes that the procedure that is mapped over the frame stream in these cases always produces either the empty stream or a singleton stream, so no interleaving is needed when combining these streams.",
+        '6' =>
+        "a. Fill in the missing expressions in Alyssa's program.",
+        '7' =>
         "b. Does the query system's behavior change if we change it in this way?",
     ],
 ];
