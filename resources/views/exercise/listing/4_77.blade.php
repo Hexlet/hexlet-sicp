4cfa7ae6e7f382ab1a901ce3cd07a50a84d09db7
Anton Burenkov <buran665@yandex.ru>
2021-12-01 13:50:51 +0700

    color 4.77

--- resources/views/exercise/listing/4_77.blade.php
+++ resources/views/exercise/listing/4_77.blade.php
@@ -1,1 +1,6 @@
-<p>{{ __('exercises/4_77.description') }}</p>
+<p>{{ __('exercises/4_77.description.1') }}
+<code>not</code>
+{{ __('exercises/4_77.description.2') }}
+<code>lisp-value</code>
+{{ __('exercises/4_77.description.3') }}
+</p>
