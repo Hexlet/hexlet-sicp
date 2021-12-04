0c2f84112e4a0382551d6551a9939241931180d7
Anton Burenkov <buran665@yandex.ru>
2021-12-04 12:52:51 +0700

    color 5.11

--- resources/views/exercise/listing/5_11.blade.php
+++ resources/views/exercise/listing/5_11.blade.php
@@ -1,11 +1,44 @@
-<p>{{ __('exercises/5_11.description.1') }}</p>
+<p>{{ __('exercises/5_11.description.1') }}
+<code>save</code>
+{{ __('exercises/5_11.description.2') }}
+<code>restore</code>
+{{ __('exercises/5_11.description.3') }}
+</p>
 <pre><code>(save y)
 (save x)
 (restore y)</code></pre>
-<p>{{ __('exercises/5_11.description.2') }}</p>
-<p>{{ __('exercises/5_11.description.3') }}</p>
-<p>{{ __('exercises/5_11.description.4') }}</p>
-<p>{{ __('exercises/5_11.description.5') }}</p>
+<p>{{ __('exercises/5_11.description.4') }}
+<code>restore</code>
+{{ __('exercises/5_11.description.5') }}
+</p>
+<p>{{ __('exercises/5_11.description.6') }}
+<code>(restore y)</code>
+{{ __('exercises/5_11.description.7') }}
+<code>y</code>
+{{ __('exercises/5_11.description.8') }}
+</p>
+<p>{{ __('exercises/5_11.description.9') }}
+<code>(restore y)</code>
+{{ __('exercises/5_11.description.10') }}
+<code>y</code>
+{{ __('exercises/5_11.description.11') }}
+<code>y</code>
+{{ __('exercises/5_11.description.12') }}
+<code>save</code>
+{{ __('exercises/5_11.description.13') }}
+</p>
+<p>{{ __('exercises/5_11.description.14') }}
+<code>(restore y)</code>
+{{ __('exercises/5_11.description.15') }}
+<code>y</code>
+{{ __('exercises/5_11.description.16') }}
+<code>y</code>
+{{ __('exercises/5_11.description.17') }}
+<code>y</code>
+{{ __('exercises/5_11.description.18') }}
+<code>initialize-stack</code>
+{{ __('exercises/5_11.description.19') }}
+</p>
 <pre><code>(controller
    (assign continue (label fib-done))
  fib-loop
@@ -38,4 +71,4 @@
    (goto (reg continue))
  fib-done)
 </code></pre>
-<p>{{ __('exercises/5_11.description.6') }}</p>
+<p>{{ __('exercises/5_11.description.20') }}</p>
