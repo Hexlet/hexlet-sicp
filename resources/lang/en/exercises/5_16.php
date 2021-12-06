9dd822c6af71b09adffd3edf313fbbb41eb6f387
Anton Burenkov <buran665@yandex.ru>
2021-12-06 14:00:33 +0700

    color 5.16

--- resources/lang/en/exercises/5_16.php
+++ resources/lang/en/exercises/5_16.php
@@ -2,7 +2,13 @@
 
 return [
     'title' => "Instruction tracing",
-    'description' =>
+    'description' => [
+        '1' =>
         "Augment the simulator to provide for instruction tracing. That is, before each instruction is executed, the simulator should print the text of the instruction. " .
-        "Make the machine model accept trace-on and trace-off messages to turn tracing on and off.",
+        "Make the machine model accept ",
+        '2' =>
+        " and ",
+        '3' =>
+        " messages to turn tracing on and off.",
+    ],
 ];
