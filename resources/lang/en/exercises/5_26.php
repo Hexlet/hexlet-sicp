aded5e85c7974420d8b8768db41182b77576d8ae
Anton Burenkov <buran665@yandex.ru>
2021-12-10 10:04:53 +0700

    color 5.26

--- resources/lang/en/exercises/5_26.php
+++ resources/lang/en/exercises/5_26.php
@@ -4,12 +4,30 @@ return [
     'title' => "Exploration of the tail-recursive property of the evaluator",
     'description' => [
         '1' =>
-        "Use the monitored stack to explore the tail-recursive property of the evaluator (section 5.4.2). Start the evaluator and define the iterative factorial procedure from section 1.2.1:",
+        "Use the monitored stack to explore the tail-recursive property of the evaluator (section 5.4.2). Start the evaluator and define the iterative ",
         '2' =>
-        "Run the procedure with some small values of n. Record the maximum stack depth and the number of pushes required to compute n! for each of these values.",
+        " procedure from section 1.2.1:",
         '3' =>
-        "a. You will find that the maximum depth required to evaluate n! is independent of n. What is that depth?",
+        "Run the procedure with some small values of ",
         '4' =>
-        "b. Determine from your data a formula in terms of n for the total number of push operations used in evaluating n! for any n ≥ 1. Note that the number of operations used is a linear function of n and is thus determined by two constants.",
+        ". Record the maximum stack depth and the number of pushes required to compute ",
+        '5' =>
+        " for each of these values.",
+        '6' =>
+        "a. You will find that the maximum depth required to evaluate ",
+        '7' =>
+        " is independent of ",
+        '8' =>
+        ". What is that depth?",
+        '9' =>
+        "b. Determine from your data a formula in terms of ",
+        '10' =>
+        " for the total number of push operations used in evaluating ",
+        '11' =>
+        " for any ",
+        '12' =>
+        ". Note that the number of operations used is a linear function of ",
+        '13' =>
+        " and is thus determined by two constants.",
     ],
 ];
--- resources/views/exercise/listing/5_26.blade.php
+++ resources/views/exercise/listing/5_26.blade.php
@@ -1,4 +1,7 @@
-<p>{{ __('exercises/5_26.description.1') }}</p>
+<p>{{ __('exercises/5_26.description.1') }}
+<code>factorial</code>
+{{ __('exercises/5_26.description.2') }}
+</p>
 <pre><code>(define (factorial n)
   (define (iter product counter)
     (if (> counter n)
@@ -7,6 +10,25 @@
               (+ counter 1))))
   (iter 1 1))
 </code></pre>
-<p>{{ __('exercises/5_26.description.2') }}</p>
-<p>{{ __('exercises/5_26.description.3') }}</p>
-<p>{{ __('exercises/5_26.description.4') }}</p>
+<p>{{ __('exercises/5_26.description.3') }}
+<code>n</code>
+{{ __('exercises/5_26.description.4') }}
+<code>n!</code>
+{{ __('exercises/5_26.description.5') }}
+</p>
+<p>{{ __('exercises/5_26.description.6') }}
+<code>n!</code>
+{{ __('exercises/5_26.description.7') }}
+<code>n</code>
+{{ __('exercises/5_26.description.8') }}
+</p>
+<p>{{ __('exercises/5_26.description.9') }}
+<code>n</code>
+{{ __('exercises/5_26.description.10') }}
+<code>n!</code>
+{{ __('exercises/5_26.description.11') }}
+<code>n ≥ 1</code>
+{{ __('exercises/5_26.description.12') }}
+<code>n</code>
+{{ __('exercises/5_26.description.13') }}
+</p>
