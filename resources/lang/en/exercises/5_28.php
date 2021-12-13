63e738d0119571462488ef1cc4e671effd443390
Anton Burenkov <buran665@yandex.ru>
2021-12-13 13:33:23 +0700

    color 5.28

--- resources/lang/en/exercises/5_28.php
+++ resources/lang/en/exercises/5_28.php
@@ -4,10 +4,14 @@ return [
     'title' => "The evaluator without tail-recursion",
     'description' => [
         '1' =>
-        "Modify the definition of the evaluator by changing eval-sequence as described in section 5.4.2 so that the evaluator is no longer tail-recursive. Rerun your experiments from exercises ",
+        "Modify the definition of the evaluator by changing ",
         '2' =>
-        " and ",
+        " as described in section 5.4.2 so that the evaluator is no longer tail-recursive. Rerun your experiments from exercises ",
         '3' =>
-        " to demonstrate that both versions of the factorial procedure now require space that grows linearly with their input.",
+        " and ",
+        '4' =>
+        " to demonstrate that both versions of the ",
+        '5' =>
+        " procedure now require space that grows linearly with their input.",
     ],
 ];
