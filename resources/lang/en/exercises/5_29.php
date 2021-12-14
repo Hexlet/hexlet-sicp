2fe5ea84cdb955c70baee4707f612d328a8a3bd3
Anton Burenkov <buran665@yandex.ru>
2021-12-14 13:23:12 +0700

    color 5.29

--- resources/lang/en/exercises/5_29.php
+++ resources/lang/en/exercises/5_29.php
@@ -6,10 +6,46 @@ return [
         '1' =>
         "Monitor the stack operations in the tree-recursive Fibonacci computation:",
         '2' =>
-        "a. Give a formula in terms of n for the maximum depth of the stack required to compute Fib(n) for n ≥ 2. Hint: In section 1.2.2 we argued that the space used by this process grows linearly with n.",
+        "a. Give a formula in terms of ",
         '3' =>
-        "b. Give a formula for the total number of pushes used to compute Fib(n) for n ≥ 2. You should find that the number of pushes (which correlates well with the time used) grows exponentially with n. " .
-        "Hint: Let S(n) be the number of pushes used in computing Fib(n). You should be able to argue that there is a formula that expresses S(n) in terms of S(n - 1), S(n - 2), and some fixed ''overhead'' constant k that is independent of n. " .
-        "Give the formula, and say what k is. Then show that S(n) can be expressed as a Fib(n + 1) + b and give the values of a and b.",
+        " for the maximum depth of the stack required to compute ",
+        '4' =>
+        " for ",
+        '5' =>
+        ". Hint: In section 1.2.2 we argued that the space used by this process grows linearly with ",
+        '6' =>
+        ".",
+        '7' =>
+        "b. Give a formula for the total number of pushes used to compute ",
+        '8' =>
+        " for ",
+        '9' =>
+        ". You should find that the number of pushes (which correlates well with the time used) grows exponentially with ",
+        '10' =>
+        ". Hint: Let ",
+        '11' =>
+        " be the number of pushes used in computing ",
+        '12' =>
+        ". You should be able to argue that there is a formula that expresses ",
+        '13' =>
+        " in terms of ",
+        '14' =>
+        ", ",
+        '15' =>
+        ", and some fixed ''overhead'' constant ",
+        '16' =>
+        " that is independent of ",
+        '17' =>
+        ". Give the formula, and say what ",
+        '18' =>
+        " is. Then show that ",
+        '19' =>
+        " can be expressed as ",
+        '20' =>
+        " and give the values of ",
+        '21' =>
+        " and ",
+        '22' =>
+        ".",
     ],
 ];
