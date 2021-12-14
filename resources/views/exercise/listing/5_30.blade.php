d1189c21c7fb45fa4f63f9124b2ca31c7d5679a6
Anton Burenkov <buran665@yandex.ru>
2021-12-14 13:32:56 +0700

    color 5.30

--- resources/views/exercise/listing/5_30.blade.php
+++ resources/views/exercise/listing/5_30.blade.php
@@ -1,3 +1,15 @@
 <p>{{ __('exercises/5_30.description.1') }}</p>
-<p>{{ __('exercises/5_30.description.2') }}</p>
-<p>{{ __('exercises/5_30.description.3') }}</p>
+<p>{{ __('exercises/5_30.description.2') }}
+<code>signal-error</code>
+{{ __('exercises/5_30.description.3') }}
+</p>
+<p>{{ __('exercises/5_30.description.4') }}
+<code>car</code>
+{{ __('exercises/5_30.description.5') }}
+<code>car</code>
+{{ __('exercises/5_30.description.6') }}
+<code>primitive-apply</code>
+{{ __('exercises/5_30.description.7') }}
+<code>signal-error</code>
+{{ __('exercises/5_30.description.8') }}
+</p>
