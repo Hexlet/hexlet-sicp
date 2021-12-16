7f4597cc1028561c7717b0eb749c43d03ff4004d
Anton Burenkov <buran665@yandex.ru>
2021-12-16 10:35:51 +0700

    color 5.34

--- resources/lang/en/exercises/5_34.php
+++ resources/lang/en/exercises/5_34.php
@@ -6,6 +6,8 @@ return [
         '1' =>
         "Compile the iterative factorial procedure",
         '2' =>
-        "Annotate the resulting code, showing the essential difference between the code for iterative and recursive versions of factorial that makes one process build up stack space and the other run in constant stack space.",
+        "Annotate the resulting code, showing the essential difference between the code for iterative and recursive versions of ",
+        '3' =>
+        " that makes one process build up stack space and the other run in constant stack space.",
     ],
 ];
