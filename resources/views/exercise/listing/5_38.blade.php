5332a548ff18fabe06dee2ffdfb2456056f9d22b
Anton Burenkov <buran665@yandex.ru>
2021-12-17 13:41:35 +0700

    color 5.38

--- resources/views/exercise/listing/5_38.blade.php
+++ resources/views/exercise/listing/5_38.blade.php
@@ -1,10 +1,63 @@
-<p>{{ __('exercises/5_38.description.1') }}</p>
+<p>{{ __('exercises/5_38.description.1') }}
+<code>(+ a 1)</code>
+{{ __('exercises/5_38.description.2') }}
+<code>argl</code>
+{{ __('exercises/5_38.description.3') }}
+<code>+</code>
+{{ __('exercises/5_38.description.4') }}
+<code>proc</code>
+{{ __('exercises/5_38.description.5') }}
+<code>(+ a 1)</code>
+{{ __('exercises/5_38.description.6') }}
+</p>
 <pre><code>(assign val (op lookup-variable-value) (const a) (reg env))
 (assign val (op +) (reg val) (const 1))
 </code></pre>
-<p>{{ __('exercises/5_38.description.2') }}</p>
-<p>{{ __('exercises/5_38.description.3') }}</p>
-<p>{{ __('exercises/5_38.description.4') }}</p>
-<p>{{ __('exercises/5_38.description.5') }}</p>
-<p>{{ __('exercises/5_38.description.6') }}</p>
-<p>{{ __('exercises/5_38.description.7') }}</p>
+<p>{{ __('exercises/5_38.description.7') }}
+<code>arg1</code>
+{{ __('exercises/5_38.description.8') }}
+<code>arg2</code>
+{{ __('exercises/5_38.description.9') }}
+<code>arg1</code>
+{{ __('exercises/5_38.description.10') }}
+<code>arg2</code>
+{{ __('exercises/5_38.description.11') }}
+<code>val</code>
+{{ __('exercises/5_38.description.12') }}
+<code>arg1</code>
+{{ __('exercises/5_38.description.13') }}
+<code>arg2</code>
+{{ __('exercises/5_38.description.14') }}
+</p>
+<p>{{ __('exercises/5_38.description.15') }}
+<code>compile</code>
+{{ __('exercises/5_38.description.16') }}
+</p>
+<p>{{ __('exercises/5_38.description.17') }}
+<code>spread-arguments</code>
+{{ __('exercises/5_38.description.18') }}
+<code>Spread-arguments</code>
+{{ __('exercises/5_38.description.19') }}
+</p>
+<p>{{ __('exercises/5_38.description.20') }}
+<code>=</code>
+{{ __('exercises/5_38.description.21') }}
+<code>*</code>
+{{ __('exercises/5_38.description.22') }}
+<code>-</code>
+{{ __('exercises/5_38.description.23') }}
+<code>+</code>
+{{ __('exercises/5_38.description.24') }}
+<code>compile</code>
+{{ __('exercises/5_38.description.25') }}
+</p>
+<p>{{ __('exercises/5_38.description.26') }}
+<code>factorial</code>
+{{ __('exercises/5_38.description.27') }}
+</p>
+<p>{{ __('exercises/5_38.description.28') }}
+<code>+</code>
+{{ __('exercises/5_38.description.29') }}
+<code>*</code>
+{{ __('exercises/5_38.description.30') }}
+</p>
