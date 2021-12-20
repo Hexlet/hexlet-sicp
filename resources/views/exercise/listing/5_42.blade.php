312cc951a631a69e4a6679e2c0dc0aa1a06fc7b4
Anton Burenkov <buran665@yandex.ru>
2021-12-20 13:50:49 +0700

    color 5.42

--- resources/views/exercise/listing/5_42.blade.php
+++ resources/views/exercise/listing/5_42.blade.php
@@ -1,2 +1,20 @@
-<p>{{ __('exercises/5_42.description.1') }}<a href="{{ route('exercises.show', getExercise('5.41')) }}">5.41</a>
-{{ __('exercises/5_42.description.2') }}</p>
+<p>{{ __('exercises/5_42.description.1') }}
+<code>find-variable</code>
+{{ __('exercises/5_42.description.2') }}
+<a href="{{ route('exercises.show', getExercise('5.41')) }}">5.41</a>
+{{ __('exercises/5_42.description.3') }}
+<code>compile-variable</code>
+{{ __('exercises/5_42.description.4') }}
+<code>compile-assignment</code>
+{{ __('exercises/5_42.description.5') }}
+<code>find-variable</code>
+{{ __('exercises/5_42.description.6') }}
+<code>not-found</code>
+{{ __('exercises/5_42.description.7') }}
+<code>(op get-global-environment)</code>
+{{ __('exercises/5_42.description.8') }}
+<code>env</code>
+{{ __('exercises/5_42.description.9') }}
+<code>lambda</code>
+{{ __('exercises/5_42.description.10') }}
+</p>
