bfcabb7bea14b4b2fd1b93ae58d4c239ccdf4ef0
Anton Burenkov <buran665@yandex.ru>
2021-12-21 15:11:51 +0700

    color 5.43

--- resources/views/exercise/listing/5_43.blade.php
+++ resources/views/exercise/listing/5_43.blade.php
@@ -1,2 +1,11 @@
-<p>{{ __('exercises/5_43.description.1') }}<a href="{{ route('exercises.show', getExercise('4.16')) }}">4.16</a>
-{{ __('exercises/5_43.description.2') }}</p>
+<p>{{ __('exercises/5_43.description.1') }}
+<code>define</code>
+{{ __('exercises/5_43.description.2') }}
+<code>define</code>
+{{ __('exercises/5_43.description.3') }}
+<code>lambda</code>
+{{ __('exercises/5_43.description.4') }}
+<code>set!</code>
+{{ __('exercises/5_43.description.5') }}
+<a href="{{ route('exercises.show', getExercise('4.16')) }}">4.16</a>
+{{ __('exercises/5_43.description.6') }}</p>
