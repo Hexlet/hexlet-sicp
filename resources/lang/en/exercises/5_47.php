209928ea0aab1803b78b47659ae28d87e4624f54
Anton Burenkov <buran665@yandex.ru>
2021-12-23 13:07:12 +0700

    color 5.47

--- resources/lang/en/exercises/5_47.php
+++ resources/lang/en/exercises/5_47.php
@@ -6,13 +6,32 @@ return [
         '1' =>
         "This section described how to modify the explicit-control evaluator so that interpreted code can call compiled procedures. " .
         "Show how to modify the compiler so that compiled procedures can call not only primitive procedures and compiled procedures, but interpreted procedures as well. " .
-        "This requires modifying compile-procedure-call to handle the case of compound (interpreted) procedures. " .
-        "Be sure to handle all the same target and linkage combinations as in compile-proc-appl. " .
-        "To do the actual procedure application, the code needs to jump to the evaluator’s compound-apply entry point. " .
-        "This label cannot be directly referenced in object code (since the assembler requires that all labels referenced by the code it is assembling be defined there), so we will add a register called compapp to the evaluator machine to hold this entry point, and add an instruction to initialize it:",
+        "This requires modifying ",
         '2' =>
-        "To test your code, start by defining a procedure f that calls a procedure g. " .
-        "Use compile-and-go to compile the definition of f and start the evaluator. " .
-        "Now, typing at the evaluator, define g and try to call f.",
+        " to handle the case of compound (interpreted) procedures. Be sure to handle all the same ",
+        '3' =>
+        " and ",
+        '4' =>
+        " combinations as in ",
+        '5' =>
+        ". To do the actual procedure application, the code needs to jump to the evaluator’s ",
+        '6' =>
+        " entry point. This label cannot be directly referenced in object code (since the assembler requires that all labels referenced by the code it is assembling be defined there), so we will add a register called ",
+        '7' =>
+        " to the evaluator machine to hold this entry point, and add an instruction to initialize it:",
+        '8' =>
+        "To test your code, start by defining a procedure ",
+        '9' =>
+        " that calls a procedure ",
+        '10' =>
+        ". Use ",
+        '11' =>
+        " to compile the definition of ",
+        '12' =>
+        " and start the evaluator. Now, typing at the evaluator, define ",
+        '13' =>
+        " and try to call ",
+        '14' =>
+        ".",
     ],
 ];
