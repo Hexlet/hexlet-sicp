ac123b0a1c0eca7331e3193392fb037f8bdd7ea3
Anton Burenkov <buran665@yandex.ru>
2021-12-24 11:45:19 +0700

    color 5.49

--- resources/views/exercise/listing/5_49.blade.php
+++ resources/views/exercise/listing/5_49.blade.php
@@ -1,1 +1,6 @@
-<p>{{ __('exercises/5_49.description') }}</p>
+<p>{{ __('exercises/5_49.description.1') }}
+<code>compile</code>
+{{ __('exercises/5_49.description.2') }}
+<code>assemble</code>
+{{ __('exercises/5_49.description.3') }}
+</p>
