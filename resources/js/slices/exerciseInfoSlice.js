50145550154908502735524c0b2f6f449c226610
Sergei Kalikin <sskalikin@gmail.com>
2022-03-18 21:45:54 +0500

    fixed new line in hashLocationMap, returning state in exerciseInfiSlice

--- resources/js/slices/exerciseInfoSlice.js
+++ resources/js/slices/exerciseInfoSlice.js
@@ -1,43 +1,43 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-import routes from '../common/routes.js';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import routes from "../common/routes.js";
 
-const loadExerciseInfo = createAsyncThunk('loadExerciseInfo', async ({ exerciseId }) => {
-  const url = routes.exerciseInfoPath(exerciseId);
-  const response = await axios.get(url);
-  const { prepared_code, test_code, has_tests } = response.data.exercise;
-  return ({
-    preparedCode: prepared_code,
-    hasTests: has_tests,
-    testCode: test_code,
-  });
-});
+const loadExerciseInfo = createAsyncThunk(
+  "loadExerciseInfo",
+  async ({ exerciseId }) => {
+    const url = routes.exerciseInfoPath(exerciseId);
+    const response = await axios.get(url);
+    const { prepared_code, test_code, has_tests } = response.data.exercise;
+    return {
+      preparedCode: prepared_code,
+      hasTests: has_tests,
+      testCode: test_code,
+    };
+  }
+);
 
 const slice = createSlice({
-  name: 'exerciseInfo',
+  name: "exerciseInfo",
   initialState: {
-    loadingState: 'idle',
-    preparedCode: '',
+    loadingState: "idle",
+    preparedCode: "",
     hasTests: false,
     testCode: null,
   },
-  reducers: {
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(loadExerciseInfo.pending, (state) => {
-        state.loadingState = 'loading';
+        state.loadingState = "loading";
       })
-      .addCase(loadExerciseInfo.fulfilled, (state, { payload }) => {
-        state.loadingState = 'loaded';
-        state.preparedCode = payload.preparedCode;
-        state.hasTests = payload.hasTests;
-        state.testCode = payload.testCode;
-      })
+      .addCase(loadExerciseInfo.fulfilled, (_state, { payload }) => ({
+        loadingState: "loaded",
+        ...payload,
+      }))
       .addCase(loadExerciseInfo.rejected, (state) => {
-        state.loadingState = 'error';
-      })
-  }
+        state.loadingState = "error";
+      });
+  },
 });
 
 export const actions = {
