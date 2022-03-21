5a60df7d0f78eaa4e188698d9a847b21f7e8e103
Sergei Kalikin <sskalikin@gmail.com>
2022-03-21 13:50:40 +0500

    fixed haslocationmap

--- resources/js/common/hashLocationMap.js
+++ resources/js/common/hashLocationMap.js
@@ -2,12 +2,4 @@ export default {
   '#editor': 'editor',
   '#output': 'output',
   '#tests': 'tests',
-<<<<<<< HEAD
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> added Tests component
-=======
-};
->>>>>>> fixed new line in hashLocationMap, returning state in exerciseInfiSlice
