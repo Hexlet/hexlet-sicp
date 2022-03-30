b40e830c4a4f806a5efb43c07bf7bb3b5e72d90e
Sergei Kalikin <sskalikin@gmail.com>
2022-03-30 13:59:07 +0500

    added line numbers

--- resources/js/components/Highlight.jsx
+++ resources/js/components/Highlight.jsx
@@ -3,11 +3,21 @@ import scheme from 'highlight.js/lib/languages/scheme';
 import vbnet from 'highlight.js/lib/languages/vbnet';
 import sql from 'highlight.js/lib/languages/sql';
 import 'highlight.js/styles/github.css';
+import './highlight.css';
 import React, { useCallback, useEffect, useRef } from 'react';
 
 hljs.registerLanguage('scheme', scheme);
 hljs.registerLanguage('vbnet', vbnet);
 hljs.registerLanguage('sql', sql);
+hljs.addPlugin({
+  'after:highlightElement': ({ el, result }) => {
+    const lines = result.value.split('\n');
+    const buildLine = (line, lineNumber) => `<tr><td class="hljs-ln"><span>${lineNumber}</span></td><td class="hljs-line">${line}</td></tr>`;
+    const linedCode = lines.map((line, index) => buildLine(line, index + 1)).join('\n');
+    // eslint-disable-next-line no-param-reassign
+    el.innerHTML = `<table><tbody>${linedCode}</tbody></table>`;
+  },
+});
 
 const Highlight = (props) => {
   const codeBlock = useRef(null);
