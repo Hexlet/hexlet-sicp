c34fdd77b0b2a4c8dc55dcf40cd9abfcba954141
feycot <feycot@gmail.com>
2022-04-09 20:53:32 +0500

    fix page layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.bootstrap5.app')
 @section('description'){{ __('about.description') }}@endsection
 @section('content')
 <h1 class="my-4">{{ __('about.title') }}</h1>
