352e99a38c1f712d249dc237253616270ffbb89d
Anton Burenkov <buran665@yandex.ru>
2022-08-06 15:38:19 +0700

    add solution for 1.33

---
+++ resources/views/exercise/solution_stub/1_33_solution.blade.php
@@ -0,0 +1,6 @@
+(define (filtered-accumulate combiner null-value term a next b filter) 
+   (define (iter a result) 
+     (cond ((> a b) result) 
+           ((filter a) (iter (next a) (combiner result (term a)))) 
+           (else (iter (next a) result)))) 
+   (iter a null-value))
