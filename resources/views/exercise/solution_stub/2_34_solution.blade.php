e27abcf788026e46e84324ce9735087e21e200b9
Anton Burenkov <buran665@yandex.ru>
2022-08-16 10:50:45 +0700

    fix typo in solution 2.34

--- resources/views/exercise/solution_stub/2_34_solution.blade.php
+++ resources/views/exercise/solution_stub/2_34_solution.blade.php
@@ -4,7 +4,7 @@
         (op (car sequence)
             (accumulate op initial (cdr sequence)))))
 
-(define (horner-eval x coefficitent-sequences)
+(define (horner-eval x coefficient-sequences)
   (accumulate (lambda (this-coeff higher-terms) (+ this-coeff (* x higher-terms)))
               0
-              coefficitent-sequences))
+              coefficient-sequences))
