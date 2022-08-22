222333143c3ed9c2dbcf8979781672cd0a9c11b0
Anton Burenkov <buran665@yandex.ru>
2022-08-22 12:46:36 +0700

    add solution for 2.47

---
+++ resources/views/exercise/solution_stub/2_47_solution.blade.php
@@ -0,0 +1,7 @@
+(define (origin-list f) (car f))
+(define (edge1-list f) (cadr f))
+(define (edge2-list f) (caddr f))
+
+(define (origin-cons f) (car f))
+(define (edge1-cons f) (cadr f))
+(define (edge2-cons f) (cddr f))
