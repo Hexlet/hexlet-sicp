6d67d83365dc20f4ad685a653b1eca7581806786
Anton Burenkov <buran665@yandex.ru>
2022-08-25 09:56:07 +0700

    add solution for 2.66

---
+++ resources/views/exercise/solution_stub/2_66_solution.blade.php
@@ -0,0 +1,10 @@
+(define (lookup given-key tree-of-records)
+    (if (null? tree-of-records)
+        #f
+        (let ((entry-key (key (entry tree-of-records))))
+            (cond ((= given-key entry-key)
+                    (entry tree-of-records))
+                  ((> given-key entry-key)
+                    (lookup given-key (right-branch tree-of-records)))
+                  ((< given-key entry-key)
+                    (lookup given-key (left-branch tree-of-records)))))))
