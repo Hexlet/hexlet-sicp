8f104bade7b19ff7422b9b893a2704dfb1720fc8
Anton Burenkov <buran665@yandex.ru>
2022-08-28 10:50:24 +0700

    add solution for 2.79

---
+++ resources/views/exercise/solution_stub/2_79_solution.blade.php
@@ -0,0 +1,19 @@
+(define (equ? x y)
+    (apply-generic 'equ? x y))
+
+(define (update-scheme-number-package)
+  (put 'equ? '(scheme-number scheme-number) equal?))
+
+(define (update-rational-package)
+  (put 'equ? '(rational rational)
+       (lambda (x y) (and (equal? (car x) (car y))
+                          (equal? (cdr x) (cdr y))))))
+
+(define (update-complex-package)
+  (put 'equ? '(complex complex)
+       (lambda (c1 c2) (and (equal? (real-part c1) (real-part c2))
+                            (equal? (imag-part c1) (imag-part c2))))))
+
+(update-scheme-number-package)
+(update-rational-package)
+(update-complex-package)
