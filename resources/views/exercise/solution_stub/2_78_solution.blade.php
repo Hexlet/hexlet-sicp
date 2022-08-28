322b2943376ab3c35f7659090a926f0c37dce30f
Anton Burenkov <buran665@yandex.ru>
2022-08-28 10:23:47 +0700

    add solution for 2.78

---
+++ resources/views/exercise/solution_stub/2_78_solution.blade.php
@@ -0,0 +1,14 @@
+ (define (attach-tag type-tag contents)
+   (if (number? contents)
+       contents
+       (cons type-tag contents)))
+
+ (define (type-tag datum)
+   (cond ((number? datum) 'scheme-number)
+         ((pair? datum) (car datum))
+         (else (error "Wrong datum -- TYPE-TAG" datum))))
+
+ (define (contents datum)
+   (cond ((number? datum) datum)
+         ((pair? datum) (cdr datum))
+         (else (error "Wrong datum -- CONTENTS" datum))))
