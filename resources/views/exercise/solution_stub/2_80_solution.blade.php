e7c140aefb2befc8af0ec09a834351c2d4da1883
Anton Burenkov <buran665@yandex.ru>
2022-08-29 10:59:20 +0700

    add solution for 2.80

---
+++ resources/views/exercise/solution_stub/2_80_solution.blade.php
@@ -0,0 +1,16 @@
+(define (=zero? x) (apply-generic '=zero? x))
+
+(define (update-scheme-number-package)
+  (put '=zero? '(scheme-number) (lambda (x) (= 0 x))))
+
+(define (update-rational-package)
+  (put '=zero? '(rational)
+       (lambda (x) (= 0 (car x)))))
+
+(define (update-complex-package)
+  (put '=zero? '(complex)
+       (lambda (c1) (and (= 0 (real-part c1)) (= 0 (imag-part c1))))))
+
+(update-scheme-number-package)
+(update-rational-package)
+(update-complex-package)
