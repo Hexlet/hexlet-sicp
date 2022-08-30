f74666700da50368cdb4095e2c301474f8e0db07
Anton Burenkov <buran665@yandex.ru>
2022-08-30 10:08:18 +0700

    add solutionf for 3.4

---
+++ resources/views/exercise/solution_stub/3_4_solution.blade.php
@@ -0,0 +1,27 @@
+(define (inc x) (+ x 1))
+
+(define (make-account balance secret)
+  (let ((attempts 0))
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch key m)
+    (cond ((eq? key secret)
+           (begin (set! attempts 0)
+                  (cond ((eq? m 'withdraw) withdraw)
+                        ((eq? m 'deposit) deposit)
+                        (else (error "Unknown request -- MAKE-ACCOUNT"
+                                     m)))))
+          (else (if (>= attempts 7)
+                           (call-the-cops)
+                           (begin
+                             (set! attempts (inc attempts))
+                             (error "Wrong password"))))))
+    dispatch))
+
+(define (call-the-cops) (error "Cops called"))
