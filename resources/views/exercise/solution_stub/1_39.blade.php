897330cf33bbe9fccf058ce3d455deebbb5fe2ac
Anton Burenkov <buran665@yandex.ru>
2022-08-31 11:26:05 +0700

    add pi to tests because lang is racket/base instead of racket

--- resources/views/exercise/solution_stub/1_39.blade.php
+++ resources/views/exercise/solution_stub/1_39.blade.php
@@ -1,9 +1,11 @@
-#lang racket
+#lang racket/base
 (require rackunit)
 
 ;;; BEGIN
 {!! $solution !!}
 ;;; END
+
+(define pi 3.141592653589793)
 
 (check-equal? (tan-cf (/ pi 4) 100) 1.0)
 (check-equal? (tan-cf 0 10) 0)
