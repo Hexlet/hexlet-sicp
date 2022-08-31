8da65b7b377e2b1c38830f4e8e67183627cbc289
Anton Burenkov <buran665@yandex.ru>
2022-08-31 11:28:38 +0700

    add solutionf for 1.39

---
+++ resources/views/exercise/solution_stub/1_39_solution.blade.php
@@ -0,0 +1,16 @@
+(define (cont-frac N D k)
+    (define (iter i result)
+        (if (= i 0)
+            result
+            (iter (- i 1)
+                  (/ (N i)
+                     (+ (D i) result)))))
+    (iter (- k 1)
+          (/ (N k) (D k))))
+
+ (define (tan-cf x k)
+   (cont-frac (lambda (i)
+                (if (= i 1) x (- (* x x))))
+              (lambda (i)
+                (- (* i 2) 1))
+              k))
