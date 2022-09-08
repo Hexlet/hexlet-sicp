1b24579d567ac6be77f37814365faf25ba43a55e
Roman Ashikov <rmn.shkv@gmail.com>
2022-09-08 14:40:43 +0700

    change webpack to vite update

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,20 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel([
-            'resources/css/app.css',
             'resources/js/app.js',
             'resources/js/hljs.js',
             'resources/js/editor.js',
-            
+
         ]),
         react(),
     ],
+    resolve: {
+        alias: [
+            {
+                // this is required for the SCSS modules
+                find: /^~(.*)$/,
+                replacement: '$1',
+            },
+        ],
+    },
 });
