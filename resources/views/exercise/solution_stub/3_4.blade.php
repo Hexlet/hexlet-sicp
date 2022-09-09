21da46bc59b18c3d30dc2535c587504a0729f493
Anton Burenkov <buran665@yandex.ru>
2022-09-09 15:26:03 +0700

    fix extra output checking in exercise 3.4

--- resources/views/exercise/solution_stub/3_4.blade.php
+++ resources/views/exercise/solution_stub/3_4.blade.php
@@ -9,9 +9,9 @@
 
 (define acc (make-account 100 'secret-password))
 
-(define wrong-message "Wrong password\n")
+(define wrong-message "Wrong password")
 
-(define cops-message "Cops called\n")
+(define cops-message "Cops called")
 
 (define (attempt)
   (with-handlers ([exn:fail?
@@ -20,11 +20,12 @@
 
 (check-equal? ((acc 'secret-password 'withdraw) 40) 60)
 (check-equal? ((acc 'secret-password 'deposit) 40) 100)
-(check-equal? (attempt) wrong-message)
-(check-equal? (attempt) wrong-message)
-(check-equal? (attempt) wrong-message)
-(check-equal? (attempt) wrong-message)
-(check-equal? (attempt) wrong-message)
-(check-equal? (attempt) wrong-message)
-(check-equal? (attempt) wrong-message)
-(check-equal? (attempt) cops-message)
+; substring used because checking system has extra output
+(check-equal? (substring (attempt) 0 14) wrong-message)
+(check-equal? (substring (attempt) 0 14) wrong-message)
+(check-equal? (substring (attempt) 0 14) wrong-message)
+(check-equal? (substring (attempt) 0 14) wrong-message)
+(check-equal? (substring (attempt) 0 14) wrong-message)
+(check-equal? (substring (attempt) 0 14) wrong-message)
+(check-equal? (substring (attempt) 0 14) wrong-message)
+(check-equal? (substring (attempt) 0 11) cops-message)
