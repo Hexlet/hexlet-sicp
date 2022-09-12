c5e4d05bcf7425aeedbde99172dc7b269d06eb62
Anton Burenkov <buran665@yandex.ru>
2022-09-12 19:58:04 +0700

    solution 3.61 with mul-series instead of mul-streams

--- resources/views/exercise/solution_stub/3_61_solution.blade.php
+++ resources/views/exercise/solution_stub/3_61_solution.blade.php
@@ -1,6 +1,15 @@
- (define (invert-unit-series series)
-   (define inverted-unit-series
-     (cons-stream 1 (scale-stream (mul-streams (stream-cdr series)
-                                               inverted-unit-series)
-                                  -1)))
-   inverted-unit-series)
+(define (invert-unit-series stream)
+  (cons-stream 1
+               (negate-series (mul-series (stream-cdr stream)
+                                          (invert-unit-series stream)))))
+
+(define (negate-series series)
+  (stream-map (lambda (x) (- x))
+              series))
+
+(define (mul-series s1 s2)
+  (cons-stream
+    (* (stream-car s1) (stream-car s2))
+    (add-streams (add-streams (scale-stream (stream-cdr s1) (stream-car s2))
+                              (scale-stream (stream-cdr s2) (stream-car s1)))
+                 (cons-stream 0 (mul-series (stream-cdr s1) (stream-cdr s2))))))
