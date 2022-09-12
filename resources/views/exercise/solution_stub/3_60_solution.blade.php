1ae5e6573b44944859d8df82f80e316d5bf651da
Anton Burenkov <buran665@yandex.ru>
2022-09-12 19:43:40 +0700

    add solution for 3.60

---
+++ resources/views/exercise/solution_stub/3_60_solution.blade.php
@@ -0,0 +1,15 @@
+(define (mul-series s1 s2)
+  (cons-stream
+    (* (stream-car s1) (stream-car s2))
+    (add-streams (add-streams (scale-stream (stream-cdr s1) (stream-car s2))
+                              (scale-stream (stream-cdr s2) (stream-car s1)))
+                 (cons-stream 0 (mul-series (stream-cdr s1) (stream-cdr s2))))))
+
+(define (integrate-series s)
+              (stream-map /  s integers))
+
+(define sine-series
+  (cons-stream 0 (integrate-series cosine-series)))
+
+(define cosine-series
+  (cons-stream 1 (integrate-series (scale-stream sine-series -1))))
