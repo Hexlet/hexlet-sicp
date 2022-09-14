eb9280e9c82c0c95c8ccce1c6e68ab66fde16229
Anton Burenkov <buran665@yandex.ru>
2022-09-14 17:40:41 +0700

    add solution for 3.69

--- resources/views/exercise/solution_stub/3_69.blade.php
+++ resources/views/exercise/solution_stub/3_69.blade.php
@@ -1,69 +1,16 @@
-#lang sicp
-(#%require rackunit)
+#lang racket
+(require rackunit)
+(require sicp)
 
 
 ;;; BEGIN
 {!! $solution !!}
 ;;; END
 
-(define (stream-car stream) (car stream))
-
-(define (stream-cdr stream) (force (cdr stream)))
-
-(define (stream-ref s n)
-  (if (= n 0)
-      (stream-car s)
-      (stream-ref (stream-cdr s) (- n 1))))
-
-(define (interleave s1 s2)
-  (if (stream-null? s1)
-      s2
-      (cons-stream (stream-car s1)
-                   (interleave s2 (stream-cdr s1)))))
-
-(define (stream-filter pred stream)
-  (cond ((stream-null? stream) the-empty-stream)
-        ((pred (stream-car stream))
-         (cons-stream (stream-car stream)
-                      (stream-filter pred
-                                     (stream-cdr stream))))
-        (else (stream-filter pred (stream-cdr stream)))))
-
-(define (stream-map proc . list-of-stream)
-    (if (null? (car list-of-stream))
-        '()
-        (cons-stream
-            (apply proc 
-                   (map (lambda (s)
-                            (stream-car s))
-                        list-of-stream))
-            (apply stream-map 
-                   (cons proc (map (lambda (s)
-                                       (stream-cdr s))
-                                   list-of-stream))))))
-
-(define (add-streams s1 s2)
-  (stream-map + s1 s2))
-
-(define (pairs s t)
-  (cons-stream
-   (list (stream-car s) (stream-car t))
-   (interleave
-    (stream-map (lambda (x) (list (stream-car s) x))
-                (stream-cdr t))
-    (pairs (stream-cdr s) (stream-cdr t)))))
-
-(define ones (cons-stream 1 ones))
-
-(define integers (cons-stream 1 (add-streams ones integers)))
-
-
-(define s (triples integers integers integers))
-
 (check-equal? (stream-ref s 0) '(1 1 1))
 (check-equal? (stream-ref s 1) '(1 1 2))
 (check-equal? (stream-ref s 2) '(2 2 2))
 (check-equal? (stream-ref s 3) '(1 1 3))
 
-(check-equal? (stream-ref pythagorian-triples 0) '(3 4 5))
-(check-equal? (stream-ref pythagorian-triples 1) '(6 8 10))
+(check-equal? (stream-ref pythagorean-triples 0) '(3 4 5))
+(check-equal? (stream-ref pythagorean-triples 1) '(6 8 10))
---
+++ resources/views/exercise/solution_stub/3_69_solution.blade.php
@@ -0,0 +1,69 @@
+(define (square x) (* x x))
+
+(define (stream-car stream) (car stream))
+
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-map proc . list-of-stream)
+    (if (null? (car list-of-stream))
+        '()
+        (cons-stream
+            (apply proc 
+                   (map (lambda (s)
+                            (stream-car s))
+                        list-of-stream))
+            (apply stream-map 
+                   (cons proc (map (lambda (s)
+                                       (stream-cdr s))
+                                   list-of-stream))))))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define (pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+    (stream-map (lambda (x) (list (stream-car s) x))
+                (stream-cdr t))
+    (pairs (stream-cdr s) (stream-cdr t)))))
+
+(define ones (cons-stream 1 ones))
+
+(define integers (cons-stream 1 (add-streams ones integers)))
+
+(define (triples s t u)
+  (cons-stream (list (stream-car s) (stream-car t) (stream-car u))
+    (interleave
+      (stream-map 
+        (lambda (x) (cons (stream-car s) x))
+        (pairs t (stream-cdr u)))
+      (triples (stream-cdr s) (stream-cdr t) (stream-cdr u)))))
+
+(define s (triples integers integers integers))
+
+(define pythagorean-triples
+  (stream-filter
+    (lambda (triple) (= (+ (square (car triple))
+                           (square (cadr triple)))
+                        (square (caddr triple))))
+    s))
