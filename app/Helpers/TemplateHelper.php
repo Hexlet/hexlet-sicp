e052c9aaef41b2fe3cb55e8205c5c16220852913
Roman Ashikov <rmn.shkv@gmail.com>
2022-09-28 17:29:26 +0700

    fix linter

--- app/Helpers/TemplateHelper.php
+++ app/Helpers/TemplateHelper.php
@@ -22,7 +22,7 @@ class TemplateHelper
 
     public static function getBookLink(string $locale): string
     {
-        return match($locale) {
+        return match ($locale) {
             'ru' => 'https://mirror.yandex.ru/mirrors/ftp.linux.kiev.ua/docs/developer/general/sicp-ru/sicp-ru-screen.pdf',
             'en' => 'https://web.mit.edu/6.001/6.037/sicp.pdf'
         };
