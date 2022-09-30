ca20f8e08d206a85ec5fba9ab05e3391b76a0bb9
Anton Burenkov <buran665@yandex.ru>
2022-10-01 00:10:38 +0700

    add compatibility/mlist in 3.48

--- resources/views/exercise/solution_stub/3_48.blade.php
+++ resources/views/exercise/solution_stub/3_48.blade.php
@@ -1,11 +1,12 @@
 #lang racket/base
 (require rackunit)
-(require sicp)
 
 
 ;;; BEGIN
 {!! $solution !!}
 ;;; END
+
+(require compatibility/mlist)
 
 (define (make-serializer)
   (let ((mutex (make-mutex)))
@@ -18,21 +19,21 @@
       serialized-p)))
 
 (define (make-mutex)
-  (let ((cell (list false)))            
+  (let ((cell (mlist #f)))
     (define (the-mutex m)
       (cond ((eq? m 'acquire)
-             (if (test-and-set! cell)
-                 (the-mutex 'acquire))) ; retry
+             (when (test-and-set! cell)
+                 (the-mutex 'acquire)))
             ((eq? m 'release) (clear! cell))))
     the-mutex))
 (define (clear! cell)
-  (set-car! cell false))
+  (set-mcar! cell #f))
 
 (define (test-and-set! cell)
-  (if (car cell)
-      true
-      (begin (set-car! cell true)
-             false)))
+  (if (mcar cell)
+      #t
+      (begin (set-mcar! cell #t)
+             #f)))
 
 (define (exchange account1 account2)
   (let ((difference (- (account1 'balance)
