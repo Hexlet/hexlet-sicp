ff5b0ffb773e54a5f5e3b0e49809de36837af039
Anton Burenkov <buran665@yandex.ru>
2022-10-14 10:11:48 +0700

    use lazy in exercise 3.76

--- resources/views/exercise/solution_stub/3_76.blade.php
+++ resources/views/exercise/solution_stub/3_76.blade.php
@@ -1,15 +1,19 @@
 #lang racket/base
 (require rackunit)
-(require sicp)
 
 
 ;;; BEGIN
 {!! $solution !!}
 ;;; END
 
+(require lazy)
+(require lazy/force)
+
+(define (cons-stream x s) (cons x s))
+
 (define (stream-car stream) (car stream))
 
-(define (stream-cdr stream) (force (cdr stream)))
+(define (stream-cdr stream) (! (cdr stream)))
 
 (define (stream-ref s n)
   (if (= n 0)
@@ -37,7 +41,7 @@
 
 (define s (smooth sense-data))
 
-(check-equal? (stream-ref s 0) 3/2)
-(check-equal? (stream-ref s 1) 1.75)
-(check-equal? (stream-ref s 2) 1.25)
-(check-equal? (stream-ref s 3) 0.75)
+(check-equal? (! (stream-ref s 0)) 3/2)
+(check-equal? (! (stream-ref s 1)) 1.75)
+(check-equal? (! (stream-ref s 2)) 1.25)
+(check-equal? (! (stream-ref s 3)) 0.75)
