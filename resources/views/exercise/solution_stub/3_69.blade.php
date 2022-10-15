07dab76a3494c509366f66499ce7c6783718f46e
Anton Burenkov <buran665@yandex.ru>
2022-10-16 00:04:18 +0700

    se lazy in exercise 3.69

--- resources/views/exercise/solution_stub/3_69.blade.php
+++ resources/views/exercise/solution_stub/3_69.blade.php
@@ -1,11 +1,67 @@
-#lang racket
+#lang racket/base
 (require rackunit)
-(require sicp)
 
 
 ;;; BEGIN
 {!! $solution !!}
 ;;; END
+
+(require lazy)
+(require lazy/force)
+
+(define (square x) (* x x))
+
+(define (cons-stream x s) (cons x s))
+
+(define (stream-car stream) (car stream))
+
+(define (stream-cdr stream) (! (cdr stream)))
+
+(define the-empty-stream '())
+
+(define (stream-null? x) (null? x))
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-map proc . list-of-stream)
+    (if (null? (car list-of-stream))
+        '()
+        (cons-stream
+            (apply proc 
+                   (map (lambda (s)
+                            (stream-car s))
+                        list-of-stream))
+            (apply stream-map 
+                   (cons proc (map (lambda (s)
+                                       (stream-cdr s))
+                                   list-of-stream))))))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define (pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+    (stream-map (lambda (x) (list (stream-car s) x))
+                (stream-cdr t))
+    (pairs (stream-cdr s) (stream-cdr t)))))
+
+(define ones (cons-stream 1 ones))
+
+(define integers (cons-stream 1 (add-streams ones integers)))
 
 (define s (triples integers integers integers))
 
@@ -14,10 +70,10 @@
       (stream-car s)
       (stream-ref (stream-cdr s) (- n 1))))
 
-(check-equal? (stream-ref s 0) '(1 1 1))
-(check-equal? (stream-ref s 1) '(1 1 2))
-(check-equal? (stream-ref s 2) '(2 2 2))
-(check-equal? (stream-ref s 3) '(1 1 3))
+(check-equal? (!! (stream-ref s 0)) '(1 1 1))
+(check-equal? (!! (stream-ref s 1)) '(1 1 2))
+(check-equal? (!! (stream-ref s 2)) '(2 2 2))
+(check-equal? (!! (stream-ref s 3)) '(1 1 3))
 
-(check-equal? (stream-ref pythagorean-triples 0) '(3 4 5))
-(check-equal? (stream-ref pythagorean-triples 1) '(6 8 10))
+(check-equal? (!! (stream-ref pythagorean-triples 0)) '(3 4 5))
+(check-equal? (!! (stream-ref pythagorean-triples 1)) '(6 8 10))
