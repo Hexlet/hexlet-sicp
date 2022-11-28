d97cb96adb33c0ecb2480b78bcd167db82d47985
Roman Ashikov <rmn.shkv@gmail.com>
2022-11-28 21:46:18 +0700

    remove dublicate from compose command

--- make-compose.mk
+++ make-compose.mk
@@ -1,6 +1,6 @@
 BUILD_ARGS:= --build-arg UID=$(shell id -u) --build-arg GID=$(shell id -u)
 
-compose: compose-clear compose-build compose-setup compose-start
+compose: compose-clear compose-setup compose-start
 
 compose-start:
 	docker-compose up --abort-on-container-exit
