f5600daa970ba981f27b72cccbb7b433c4ae1f85
IgBuS <i.b@msulab.ru>
2022-12-08 19:33:48 +0300

    return controller's test

---
+++ tests/Feature/Http/Controllers/SitemapControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use Github\Api\Gists;
+use GrahamCampbell\GitHub\GitHubManager;
+use Tests\TestCase;
+
+class SitemapControllerTest extends TestCase
+{
+    public function testIndex(): void
+    {
+        $gists = $this->createMock(Gists::class);
+        $gists->method('show')->willReturn(
+            $this->getFixture()
+        );
+
+        $github = $this->getMockBuilder(GitHubManager::class)
+            ->disableOriginalConstructor()
+            ->addMethods(['gists'])
+            ->getMock();
+        $github->method('gists')->willReturn($gists);
+
+        $this->app->instance(GitHubManager::class, $github);
+
+        $response = $this->get('/sitemap.xml');
+
+        $response->assertOk();
+    }
+
+    private function getFixture(): array
+    {
+        $sitemapFilepath = base_path('tests/fixtures/sitemap.xml');
+        return [
+            "files" => [
+                "sitemap.xml" => [
+                    "content" => file_get_contents($sitemapFilepath),
+                ],
+            ],
+        ];
+    }
+}
