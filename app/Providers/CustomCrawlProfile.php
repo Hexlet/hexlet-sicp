e2bd93573c7c7916b5d06e0f9335cbadb1d44ac1
IgBuS <i.b@msulab.ru>
2022-12-08 19:34:44 +0300

    crowler update

--- app/Providers/CustomCrawlProfile.php
+++ app/Providers/CustomCrawlProfile.php
@@ -4,15 +4,22 @@ namespace App\Providers;
 
 use Spatie\Crawler\CrawlProfiles\CrawlProfile;
 use Psr\Http\Message\UriInterface;
+use Illuminate\Support\Str;
 
 class CustomCrawlProfile extends CrawlProfile
 {
     public function shouldCrawl(UriInterface $url): bool
     {
-        foreach (config('sitemap.page_exceptions') as $exception) {
-            if (strpos($url->getPath(), $exception) !== false) {
-                return false;
-            }
+        $parsedAppUrl = parse_url(config('app.url'));
+
+        $appUrlHost = $parsedAppUrl['host'];
+
+        if ($url->getHost() !== $appUrlHost) {
+            return false;
+        }
+
+        if (Str::contains($url->getPath(), config('sitemap.url_parts_to_filter'))) {
+            return false;
         }
 
         return true;
