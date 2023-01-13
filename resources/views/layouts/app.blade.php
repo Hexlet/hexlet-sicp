329d297a00d0654b05f189bb06cdd5184db2737b
feycot <feycot@gmail.com>
2023-01-14 01:48:34 +0500

    add dark theme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="h-100" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="h-100" lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-bs-theme="dark">
 
 <head>
   <meta charset="utf-8">
