99749556d6b411cfd107925c948e6616bd6bf8b4
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2023-01-18 16:19:50 +0300

    change semantics of background-color class for ul

--- resources/views/exercise/navigation.blade.php
+++ resources/views/exercise/navigation.blade.php
@@ -1,4 +1,4 @@
-<ul class="nav nav-tabs nav-fill rounded border-bottom-0 x-bg-gray-200 p-2 mb-4">
+<ul class="nav nav-tabs nav-fill rounded border-bottom-0 x-bg-gray p-2 mb-4">
     <li class="nav-item mb-0">
         <a href="{{ route('exercises.index') }}" @class(['nav-link', 'rounded', 'border-0', 'active' => Request::routeIs('exercises.index')]) >
             {{ __('layout.nav.exercises') }}
