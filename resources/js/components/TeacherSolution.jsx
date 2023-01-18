5c41ab001a4170151e0ecf8d5a5c3792d0f58147
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2023-01-18 16:22:49 +0300

    add theme toggling depending on html data-bs-theme attribute

--- resources/js/components/TeacherSolution.jsx
+++ resources/js/components/TeacherSolution.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Highlight from 'react-syntax-highlighter';
-import { vs } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { vs, vs2015 } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import theme from '../common/currentTheme';
 
 const TeacherSolution = () => {
   const { hasTeacherSolution, teacherSolutionCode } = useSelector((state) => state.exerciseInfo);
 
   return hasTeacherSolution ? (
     <div className="d-flex h-100">
-      <Highlight className="h-100" language="scheme" style={vs} showLineNumbers>
+      <Highlight
+        className="h-100"
+        language="scheme"
+        style={theme === 'dark' ? vs2015 : vs}
+        showLineNumbers
+      >
         {teacherSolutionCode}
       </Highlight>
     </div>
