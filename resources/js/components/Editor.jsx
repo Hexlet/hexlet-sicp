d2a69d342cae3e83688eb31888da111cc919d5af
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2023-02-01 19:20:48 +0300

    rewrite editor using code mirror 6

--- resources/js/components/Editor.jsx
+++ resources/js/components/Editor.jsx
@@ -1,65 +1,13 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react';
+import React from 'react';
 import { Spinner } from 'react-bootstrap';
-import { Controlled as CodeMirrorEditor } from 'react-codemirror2-react-17';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { changeContent } from '../slices/editorSlice.js';
-import codeTemplates from '../common/codeTemplates.js';
-import theme from '../common/currentTheme';
 
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/material-darker.css';
-import 'codemirror/theme/neat.css';
-import 'codemirror/addon/scroll/simplescrollbars.css';
-import 'codemirror/addon/scroll/simplescrollbars.js';
-import 'codemirror/addon/edit/closebrackets.js';
-import 'codemirror/addon/edit/matchbrackets.js';
-import 'codemirror/mode/scheme/scheme.js';
-import 'codemirror/keymap/sublime.js';
+import EditorBuilder from './EditorBuilder.jsx';
 
 const Editor = () => {
-  const { content, focusesCount } = useSelector((state) => state.editor);
-  const { hasTests, preparedCode, loadingState } = useSelector((state) => state.exerciseInfo);
-  const [editor, setEditor] = useState(null);
-  const dispatch = useDispatch();
+  const { loadingState } = useSelector((state) => state.exerciseInfo);
   const { t } = useTranslation();
-
-  useEffect(() => {
-    if (hasTests) {
-      dispatch(changeContent({ content: codeTemplates.withTemplate(t('editorContent.withTemplate'), preparedCode) }));
-    } else {
-      dispatch(changeContent({ content: codeTemplates.withoutTemplate(t('editorContent.withoutTemplate')) }));
-    }
-  }, [dispatch, preparedCode, hasTests, t]);
-
-  useLayoutEffect(() => {
-    editor?.refresh();
-    editor?.focus();
-  }, [focusesCount, editor]);
-
-  const onMount = (self) => {
-    self.setSize('100%', '100%');
-    setEditor(self);
-  };
-
-  const onContentChange = (_editor, _data, newContent) => {
-    // setContent(newContent); // это для localStorage. См. позже
-    dispatch(changeContent({ content: newContent }));
-    editor.refresh();
-  };
-
-  const options = {
-    autoCloseBrackets: {
-      override: true,
-    },
-    autofocus: true,
-    keyMap: 'sublime',
-    matchBrackets: true,
-    scrollbarStyle: 'overlay',
-    lineNumbers: true,
-    tabSize: 2,
-    theme: theme === 'dark' ? 'material-darker' : 'neat',
-  };
 
   return loadingState === 'loading'
     ? (
@@ -70,13 +18,7 @@ const Editor = () => {
       </div>
     )
     : (
-      <CodeMirrorEditor
-        value={content}
-        options={options}
-        onBeforeChange={onContentChange}
-        editorDidMount={onMount}
-        className="h-100 w-100"
-      />
+      <EditorBuilder />
     );
 };
 
---
+++ resources/js/components/EditorBuilder.jsx
@@ -0,0 +1,78 @@
+import React, {
+  useRef, useEffect, useLayoutEffect,
+} from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { EditorView, basicSetup } from 'codemirror';
+import { EditorState } from '@codemirror/state';
+import { StreamLanguage } from '@codemirror/language';
+import { scheme } from '@codemirror/legacy-modes/mode/scheme';
+import { basicDark } from 'cm6-theme-basic-dark';
+import { basicLight } from 'cm6-theme-basic-light';
+import { useTranslation } from 'react-i18next';
+import { changeContent } from '../slices/editorSlice.js';
+import codeTemplates from '../common/codeTemplates.js';
+import theme from '../common/currentTheme';
+
+const myTheme = theme === 'dark' ? basicDark : basicLight;
+
+const EditorBuilder = () => {
+  const { focusesCount } = useSelector((state) => state.editor);
+  const { hasTests, preparedCode } = useSelector((state) => state.exerciseInfo);
+
+  const editorRef = useRef();
+  const viewRef = useRef();
+  const contentRef = useRef();
+  const dispatch = useDispatch();
+  const { t } = useTranslation();
+
+  const onChange = EditorView.updateListener.of((v) => {
+    if (v.docChanged) {
+      const value = v.state.doc.toString();
+      dispatch(changeContent({ content: value }));
+    }
+  });
+
+  useLayoutEffect(() => {
+    if (hasTests) {
+      dispatch(changeContent({ content: codeTemplates.withTemplate(t('editorContent.withTemplate'), preparedCode) }));
+      contentRef.current = codeTemplates.withTemplate(t('editorContent.withTemplate'), preparedCode);
+    } else {
+      dispatch(changeContent({ content: codeTemplates.withoutTemplate(t('editorContent.withoutTemplate')) }));
+      contentRef.current = codeTemplates.withoutTemplate(t('editorContent.withoutTemplate'));
+    }
+  }, [dispatch, hasTests, preparedCode, t]);
+
+  useEffect(() => {
+    viewRef.current = new EditorView({
+      state: EditorState.create({
+        doc: contentRef.current,
+        extensions: [
+          basicSetup,
+          StreamLanguage.define(scheme),
+          myTheme,
+          onChange,
+        ],
+      }),
+      parent: editorRef.current,
+    });
+
+    const cursorLocation = hasTests ? contentRef.current.indexOf('\n') + 1 : contentRef.current.length;
+    viewRef.current.dispatch({ selection: { anchor: cursorLocation } });
+
+    viewRef.current.focus();
+
+    return () => {
+      viewRef.current.destroy();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    viewRef?.current.focus();
+  }, [focusesCount]);
+
+  return <div ref={editorRef} />;
+};
+
+export default EditorBuilder;
