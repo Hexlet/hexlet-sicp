1b6d7e0c9114451cb416657575296d56839f5318
vasily.smolin <vasilysmoolin@gmail.com>
2023-03-23 15:52:24 +0400

    #1393 update-laravel-10

--- app/Models/User.php
+++ app/Models/User.php
@@ -77,6 +77,7 @@ class User extends Authenticatable implements MustVerifyEmail
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'deleted_at' => 'date:Y-m-d',
     ];
 
     public function chapters(): BelongsToMany
