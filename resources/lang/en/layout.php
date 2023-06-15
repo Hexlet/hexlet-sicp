2c3b3cc14dcce44550613a18960cc7e5f2ac282b
Ivan Popov <popovbm@gmail.com>
2023-06-15 21:45:04 +0300

    add logo alt text to lang files

--- resources/lang/en/layout.php
+++ resources/lang/en/layout.php
@@ -3,7 +3,7 @@
 return [
     'meta' => [
         'description' =>
-            'Hexlet SICP is a service for those studying the book Structure and Interpretation of Computer Programs. Track your progress and match yourself against others on the global leaderboard.',
+        'Hexlet SICP is a service for those studying the book Structure and Interpretation of Computer Programs. Track your progress and match yourself against others on the global leaderboard.',
     ],
     'title' => [
         'name' => 'Structure and Interpretation of Computer Programs (SICP) - Hexlet SICP',
@@ -21,6 +21,7 @@ return [
         'chapters' => 'Contents',
         'exercises' => 'Exercises',
         'sicp_book' => 'The book',
+        'logo_alt' => 'Hexlet SICP',
     ],
     'login' => [
         'form_header' => 'Login',
