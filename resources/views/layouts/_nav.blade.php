bc4be6bbe71c68bb7f936167b0bd1b0cf2f4eca0
Ivan Popov <popovbm@gmail.com>
2023-06-15 19:37:58 +0300

    add change logo by locale

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -8,10 +8,13 @@ $user = Auth::user();
 
 <header class="navbar navbar-expand-lg">
   <nav class="container py-2 border-bottom">
-    <a href="{{ LaravelLocalization::getLocalizedURL($currentLocale, route('home')) }}" class="navbar-brand"><img
-        src="{{ mix('images/logo.png') }}" height="40px" alt=""></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-responsive"
-      aria-controls="navbar-responsive" aria-expanded="false" aria-label="Toggle navigation">
+    <a href="{{ LaravelLocalization::getLocalizedURL($currentLocale, route('home')) }}" class="navbar-brand">
+      @if (App::isLocale('ru'))
+      <img src="images/logo.svg" alt="Хекслет СИКП" height="25px">
+      @else
+      <img src="images/logo-eng.svg" alt="Hexlet SICP" height="25px">
+      @endif</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-responsive" aria-controls="navbar-responsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
@@ -28,15 +31,13 @@ $user = Auth::user();
         @endif
         <li class="nav-item"><a href="{{ route('top.index') }}" class="nav-link p-2">{{ __('layout.nav.rating') }}</a>
         </li>
-        <li class="nav-item"><a href="{{ TemplateHelper::getBookLink($currentLocale) }}"
-            class="nav-link link-info p-2">{{ __('layout.nav.sicp_book') }}</a></li>
+        <li class="nav-item"><a href="{{ TemplateHelper::getBookLink($currentLocale) }}" class="nav-link link-info p-2">{{ __('layout.nav.sicp_book') }}</a></li>
       </ul>
       <ul class="navbar-nav ms-md-auto">
         @guest
         @if(app()->environment('local'))
         <li>
-          <a href="{{ route('auth.dev-login') }}" class="nav-link px-2" data-method="post"
-            data-confirm="Are you sure you want to submit?"> Dev-login</a>
+          <a href="{{ route('auth.dev-login') }}" class="nav-link px-2" data-method="post" data-confirm="Are you sure you want to submit?"> Dev-login</a>
         </li>
         @endif
         <li class="nav-item"><a href="{{ route('login') }}" class="nav-link p-2">{{ __('layout.nav.login') }}</a></li>
@@ -44,8 +45,7 @@ $user = Auth::user();
         </li>
         @else
         <li class="nav-item dropdown d-md-block">
-          <a class="nav-link dropdown-toggle py-1 px-2 link-secondary" id="dropdownMenuButton" data-bs-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false" href="#">
+          <a class="nav-link dropdown-toggle py-1 px-2 link-secondary" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">
             <i class="bi bi-person align-middle fs-4"></i>
           </a>
           <ul class="dropdown-menu x-z-index-dropdown dropdown-menu-right" aria-labelledby="dropdownMenuButton">
@@ -75,20 +75,16 @@ $user = Auth::user();
         </li>
         @endguest
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle px-2 link-secondary" id="dropdownFlagButton" data-bs-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false" href="#">
-            <img src="{{ LocalizationHelper::getPathToLocaleFlag($currentLocale) }}"
-              alt="{{ LocalizationHelper::getNativeLanguageName($currentLocale) }}" class="me-1" width="24">
+          <a class="nav-link dropdown-toggle px-2 link-secondary" id="dropdownFlagButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">
+            <img src="{{ LocalizationHelper::getPathToLocaleFlag($currentLocale) }}" alt="{{ LocalizationHelper::getNativeLanguageName($currentLocale) }}" class="me-1" width="24">
             <span class="d-md-none">{{ LocalizationHelper::getNativeLanguageName($currentLocale) }}</span>
           </a>
           <ul class="dropdown-menu dropdown-menu-right x-min-w-0" aria-labelledby="dropdownFlagButton">
             @foreach(LocalizationHelper::getOtherLocales($currentLocale, $locales) as $localeCode => ['native' =>
             $language])
             <li>
-              <a href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}" rel="alternate"
-                class="dropdown-item" hreflang="{{ $localeCode }}">
-                <img src="{{ LocalizationHelper::getPathToLocaleFlag($localeCode) }}"
-                  alt="{{ LocalizationHelper::normalizeNativeLanguageName($language) }}" class="mr-1" width="24">
+              <a href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}" rel="alternate" class="dropdown-item" hreflang="{{ $localeCode }}">
+                <img src="{{ LocalizationHelper::getPathToLocaleFlag($localeCode) }}" alt="{{ LocalizationHelper::normalizeNativeLanguageName($language) }}" class="mr-1" width="24">
                 <span class="d-md-none">{{ LocalizationHelper::normalizeNativeLanguageName($language) }}</span>
               </a>
             </li>
