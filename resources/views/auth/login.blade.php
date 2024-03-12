aabcabfe1417ddd0f602d6387d23496ebbb8b62a
Nikolay Gagarinov <feycot@gmail.com>
2024-03-13 00:01:22 +0500

    use spatie/html for login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,19 +7,28 @@
           {{ __('layout.login.form_header') }}
         </h1>
         <div class="card-body">
-          {{ BsForm::open(route('login')) }}
-          {{ BsForm::email('email')->label(__('layout.login.email')) }}
-          {{ BsForm::password('password')->label(__('layout.login.password')) }}
-          {{ BsForm::checkbox('remember')->label(__('layout.login.remember_me')) }}
-          {{ BsForm::submit(__('layout.login.button'))->attribute('class', 'btn btn-primary btn-block') }}
-          <div class="mt-2">
-            @if (Route::has('password.request'))
-              <a href="{{ route('password.request') }}">
-                {{ __('layout.login.reset_password') }}
-              </a>
-            @endif
+          {{ html()->form('POST', route('login'))->open() }}
+          <div class="mb-3">
+            {{ html()->label(__('layout.login.email'))->for('email')->class('form-label') }}
+            {{ html()->email('email')->class(['form-control', 'is-invalid' => $errors->has('email')]) }}
+            @error('email')
+            <span class="invalid-feedback">{{ $errors->first('email') }}</span>
+            @enderror
           </div>
-          {{ BsForm::close() }}
+          <div class="mb-3">
+            {{ html()->label(__('layout.login.password'))->for('password')->class('form-label') }}
+            {{ html()->password('password')->class(['form-control', 'is-invalid' => $errors->has('password')]) }}
+            @error('password')
+            <span class="invalid-feedback">{{ $errors->first('password') }}</span>
+            @enderror
+          </div>
+          <div class="mb-3 form-check">
+            {{ html()->checkbox('remember')->class('form-check-input') }}
+            {{ html()->label(__('layout.login.remember_me'))->for('remember')->class('form-label') }}
+          </div>
+          {{ html()->submit(__('layout.login.button'))->class('btn btn-primary btn-block') }}
+          <a class="mt-2 d-block" href="{{ route('password.request') }}">{{ __('layout.login.reset_password') }}</a>
+          {{ html()->form()->close() }}
         </div>
         <div class="card-footer p-4 text-center bg-transparent">
           @include('components.social_login')
