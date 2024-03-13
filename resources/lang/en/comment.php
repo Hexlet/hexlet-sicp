112c817dc36fa6b391e61475064b1094155356c4
Nikolay Gagarinov <feycot@gmail.com>
2024-03-14 01:17:20 +0500

    spatie/html for comment forms

--- resources/lang/en/comment.php
+++ resources/lang/en/comment.php
@@ -11,7 +11,7 @@ return [
     'none_comments' => 'There are no comments yet.',
     'authentication_required' => 'Authentication required',
     'must_log_in' => 'You must log in to post a comment.',
-    'enter_your_message' => 'Enter your message here',
+    'enter_your_message' => 'Enter your message here (markdown supported)',
     'submit' => 'Submit',
     'edit_comment' => 'Edit Comment',
     'update_comment_here' => 'Update your message here:',
