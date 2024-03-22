72d71b425f2f253fd3122061dd2b36ced7204ab3
Nikolay Gagarinov <feycot@gmail.com>
2024-03-22 21:14:42 +0500

    add migrations

---
+++ database/migrations/2024_03_22_160627_add_to_chapter_member_state_column.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::transaction(function () {
+            Schema::table('chapter_members', function (Blueprint $t) {
+                $t->string('state')->nullable();
+            });
+            DB::table('chapter_members')->update([
+                'state' => 'finished',
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('chapter_members', function (Blueprint $t) {
+            $t->dropColumn('state');
+        });
+    }
+};
