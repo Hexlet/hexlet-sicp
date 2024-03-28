3f6ea0c19893e0195c97dbccbd40cf32f399bf63
Nikolay Gagarinov <feycot@gmail.com>
2024-03-28 16:00:46 +0500

    fix lint

--- database/migrations/2024_03_28_104056_fix_points_of_users.php
+++ database/migrations/2024_03_28_104056_fix_points_of_users.php
@@ -15,11 +15,4 @@ return new class extends Migration
         DB::statement("UPDATE users SET points = ((SELECT count(user_id) FROM exercise_members em WHERE em.user_id = users.id AND state = 'finished') * 3);");
         DB::statement("UPDATE users SET points = points + (SELECT count(user_id) FROM chapter_members em WHERE em.user_id = users.id AND state = 'finished');");
     }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-    }
 };
