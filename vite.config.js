4070c63db6f3750164822148243c3b010350bece
Dmitry <neox56@gmail.com>
2024-06-08 11:40:20 +0400

    update docker configure

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,30 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-              'resources/js/app.js',
-              'resources/js/custom.js',
-              'resources/js/hljs.js',
-              'resources/js/editor.js',
-              'resources/sass/app.scss',
-              'resources/sass/_activity_chart.scss',
-              'resources/sass/_custom.scss',
-            ],
-            refresh: true,
-        }),
-        react(),
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      host: 'localhost',
+    },
+  },
+  plugins: [
+    laravel({
+      input: [
+        'resources/js/app.js',
+        'resources/js/custom.js',
+        'resources/js/hljs.js',
+        'resources/js/editor.js',
+        'resources/sass/app.scss',
+        'resources/sass/_activity_chart.scss',
+        'resources/sass/_custom.scss',
+      ],
+      refresh: true,
+    }),
+    react(),
 
-    ],
+  ],
   build: {
     outDir: 'public/build',
     emptyOutDir: false,
-  }
+  },
 });
