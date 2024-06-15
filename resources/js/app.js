b7b742107b311e249eac5fae3af3d946ba3a38b9
Nikolay Gagarinov <feycot@gmail.com>
2024-06-16 02:07:55 +0500

    fix ujs

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,8 @@ import $ from 'jquery';
 import lodash from 'lodash';
 import * as bootstrap from 'bootstrap';
 import axios from 'axios';
+import ujs from '@rails/ujs';
 import correctionListener from './correction';
-
-import 'jquery-ujs';
 
 import.meta.glob([
   '../assets/images/**',
@@ -16,5 +15,6 @@ window.jQuery = $;
 window._ = lodash;
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+ujs.start();
 
 correctionListener();
