24fbc3ce52270ba5332da2a446db9c8407ecc915
Nikolay Gagarinov <feycot@gmail.com>
2024-11-28 15:57:14 +0500

    Revert "Moved the pdf from gdrive to repository. Closes #1644"

--- app/Helpers/TemplateHelper.php
+++ app/Helpers/TemplateHelper.php
@@ -9,4 +9,12 @@ class TemplateHelper
         $name = __('layout.title.name_SICP');
         return "{$header} - {$name}";
     }
+
+    public static function getBookLink(string $locale): string
+    {
+        return match ($locale) {
+            'ru' => 'https://drive.google.com/file/d/1xc9r6txuTZMZ5lPy9YQuD1Dwv9spg7Nt/view?usp=sharing',
+            default => 'https://drive.google.com/file/d/1t3rlltwuU85ow7g0ZI6Kx7eY5n-Umh8p/view?usp=sharing',
+        };
+    }
 }
