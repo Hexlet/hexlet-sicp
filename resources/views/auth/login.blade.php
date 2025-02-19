6367b9d70a343b39fede9c7d678ef34536cedf9b
artosepyan <artyomosepyan@mail.ru>
2025-02-19 21:56:53 +0300

    Removed login via Github for RU locale. Closes #1658

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,8 +17,10 @@
 
           <a class="mt-2 d-block" href="{{ route('register') }}">{{ __('login.register') }}</a>
           <a class="mt-2 d-block" href="{{ route('password.request') }}">{{ __('login.reset_password') }}</a>
+          @if(app()->getLocale() !== 'ru')
           <a href="{{ route('oauth.github') }}" class="mt-2 d-block"> {{ __('auth.with_github') }}</a>
-
+          @endif
+          
           {{ html()->form()->close() }}
         </div>
       </div>
