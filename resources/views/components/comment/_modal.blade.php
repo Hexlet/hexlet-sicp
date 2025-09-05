5877d4e1bfe7ea15c46d257e19202d053904c981
Nikolay Gagarinov <feycot@gmail.com>
2025-09-05 23:20:44 +0500

    remove unused

--- resources/views/components/comment/_modal.blade.php
+++
@@ -1,9 +0,0 @@
-@php
-  /**
-   * @var \Illuminate\Database\Eloquent\Model $model
-   * @var string $modalTitle
-   * @var string $content
-   * @var string $submitLabel
-   * @var string $method
-   */
-@endphp
