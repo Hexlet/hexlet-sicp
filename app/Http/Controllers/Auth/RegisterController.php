7227d7fd2909fb7d180ceb3d426c21f6fe700de3
GreedVal <137034289+GreedVal@users.noreply.github.com>
2025-09-21 18:27:42 +0300

    Delete use
    fix register controler

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Rules\EmailValidation;
 use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\Validation\Validator as ValidatorContract;
