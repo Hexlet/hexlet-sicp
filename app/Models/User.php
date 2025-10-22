18bbcc2b97bb18166a13d68bd04df926d347d1fb
rnik82 <rnik82@mail.ru>
2025-10-22 18:03:24 +0300

    add is_admin field to users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,7 +57,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var string[]
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'is_admin',
     ];
 
     /**
