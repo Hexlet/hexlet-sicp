c17f435603427dfd7f551c7f72ae9bacc5f99b0d
Maxim <raychukmaxim@gmail.com>
2025-12-11 18:35:49 +0300

    Add max password length validation (128 chars)

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => 'required|string|max:255',
             'email' => 'required|email:rfc,filter|string|max:255|unique:users',
-            'password' => 'required|confirmed|min:8',
+            'password' => 'required|confirmed|min:8|max:128',
             'password_confirmation' => 'required|same:password',
         ]);
     }
