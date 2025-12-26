11f099340c44cef167ac9a70e2d32b2623848775
Ddm14159 <ddm14159@gmail.com>
2025-12-26 18:25:43 +0700

    Add GitHub repository templates

---
+++ resources/templates/github/exercise.rkt
@@ -0,0 +1,15 @@
+#lang racket
+(require rackunit)
+
+;;; Exercise {exercise_path}
+;;; See: {exercise_url}
+
+;;; BEGIN SOLUTION
+;;; Write your solution here
+;;; Example:
+;;; (define (solution ...)
+;;;   ...)
+;;; END SOLUTION
+
+;;; Tests
+{tests}
