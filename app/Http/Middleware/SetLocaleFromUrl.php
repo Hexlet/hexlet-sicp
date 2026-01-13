572f1afa2a08a0ee65b245eb96d307e356ca1841
Ddm14159 <ddm14159@gmail.com>
2026-01-13 22:09:54 +0700

    Remove debug logging from SetLocaleFromUrl middleware

--- app/Http/Middleware/SetLocaleFromUrl.php
+++ app/Http/Middleware/SetLocaleFromUrl.php
@@ -11,15 +11,9 @@ class SetLocaleFromUrl
     public function handle(Request $request, Closure $next)
     {
         $locale = LaravelLocalization::getCurrentLocale();
-        \Log::info('SetLocaleFromUrl middleware', [
-            'current_locale' => $locale,
-            'url' => $request->url(),
-            'app_locale_before' => app()->getLocale(),
-        ]);
 
         if ($locale) {
             app()->setLocale($locale);
-            \Log::info('Locale set to: ' . $locale);
         }
 
         return $next($request);
